4efccae31c288266d23b6acf61f6edf92e5e7362
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-05-30 08:49:25 +0800

    升级到rails3.2.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lianlian::Application.config.secret_token = 'd27b45120b066f8fa02104c75808fc10528d0ace091243fc3c8fe9b8e143b9980076b8a5a84e7cf111f00645f87c1ce749bf2c7792f7ef20d285026df49588ee'
--- vendor/plugins/swfupload_fu/tasks/swfupload_fu_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :swfupload_fu do
-#   # Task goes here
-# end
