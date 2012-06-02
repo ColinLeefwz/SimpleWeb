cf4428ef5d4f81ee540d26ccb523c50144cd7b51
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-06-02 19:40:22 +0800

    修改客户端接口的输出：删除无用的输出、消除多余的嵌套层次

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+  
+  def safe_output
+    self.attributes.slice("id", "name", "wb_uid", "gender", "birthday").merge!( {:logo => '/phone2/images/namei2.gif'} )
+  end
 
 end
