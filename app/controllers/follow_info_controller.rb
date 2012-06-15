a1117b1c76eb43351217d8242841af42d1b01726
pulsen <huang123qwe@126.com>
2012-06-15 09:31:32 +0800

    关注，粉丝名称查询

--- app/controllers/follow_info_controller.rb
+++ app/controllers/follow_info_controller.rb
@@ -2,23 +2,45 @@ class FollowInfoController < ApplicationController
   
   
   def followers
-    output_users Follow.find_all_by_follow_id(params[:id]).map {|x| x.user}
-    
+    output_users Follow.find(:all, :conditions => genCondition(params[:id].to_i), :include => :user)
   end
   
   def friends
-    output_users Follow.find_all_by_user_id(params[:id]).map {|x| x.follow}
+    output_users Follow.find(:all, :conditions => genCondition(params[:id].to_i), :include => :follow)
   end
   
-  private 
+  private
+
+  def genCondition(id)
+
+    if params[:action] == "followers"
+      sql = "follows.follow_id = ?"
+      a = [id]
+    elsif params[:action] == "friends"
+      sql = "follows.user_id = ?"
+      a = [id]
+    end
+
+    unless  params[:name].blank?
+      sql += " and users.name like ? "
+      a << "%#{params[:name]}%"
+    end
+
+    #    puts "------------------|#{a.unshift(sql.join(" and "))}|"
+    a.unshift(sql)
+
+  end
+
   def output_users(fs)
+    puts "------------------------------------|#{fs.length}|"
     users = []
     page = params[:page] || 1
     pcount = params[:pcount] || 20
     page = page.to_i
     pcount = pcount.to_i
+    
     fs2 = fs[(page-1)*pcount,pcount]
-    fs2.each {|f| users << f.safe_output } if fs2
+    fs2.each {|f| users << f.user.safe_output } if fs2
     if params[:hash]
       ret = {:count => fs.size}
       ret.merge!( {:data => users})
