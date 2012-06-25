b2dc58bb3eeebe6e7d09c65bfe8373b9de6f43af
pulsen <huang123qwe@126.com>
2012-06-25 14:20:39 +0800

    临时测试商家查询

--- app/models/mcategory.rb
+++ app/models/mcategory.rb
@@ -20,4 +20,23 @@ class Mcategory < ActiveRecord::Base
   def mcity_mcategory(mcity_id)
     return McityMcategory.find_by_mcity_id_and_mcategory_id(mcity_id, self.id)
   end
+
+  def self.unfold(type_id)
+    categories = []
+    case type_id
+    when 1
+      categories =  self.where(['name like ? or name like ?', '%酒吧%', '%活动%'])
+    when 2
+      categories =  self.where(['name like ? or name like ?', '%咖啡%', '%茶馆%'])
+    when 3
+      categories =  self.where(['name like ? or name like ?', '%餐饮%', '%酒店%'])
+    when 4
+      categories =  self.where(['name like ? or name like ?', '%运动%', '%休闲%'])
+    when 5
+      categories =  self.where(['name like ? or name like ?', '%购物%', '%广场%'])
+    end
+    
+    categories.map{|m| m.id }
+
+  end
 end
