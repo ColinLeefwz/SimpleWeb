bb4311f58f8bf41ec9ef98bb3ac945eb39765eae
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-07-02 17:59:14 +0800

    商家和聊天室同步

--- app/models/of_room.rb
+++ app/models/of_room.rb
@@ -4,12 +4,29 @@ class OfRoom < ActiveRecord::Base
   self.primary_key = "name"
   
   def self.gen_from_shop(mshop)
+    unless OfRoom.find_by_name(mshop.id)
+      gen_from_shop0 mshop
+      return true
+    else
+      return false
+    end
+  end
+
+  def self.gen_from_shop!(mshop)
     raise "shop #{mshop.id} is already exsits." if OfRoom.find_by_name(mshop.id)
+    gen_from_shop0 mshop
+  end
+  
+  def self.gen_from_shop0(mshop)
     room = OfRoom.order(:roomID).last.dup
     room.name = mshop.id
     room.naturalName = mshop.name
     room.roomID += 1
     room.save!
+  end
+    
+  def self.sync
+    Mshop.all.each {|shop| OfRoom.gen_from_shop(shop)}
   end
   
 end
