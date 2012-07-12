89f2ab29820170856b5cbb63d2bfeefe254d61bc
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-07-12 21:02:01 +0800

    首次登录时从新浪获取个人昵称、性别等信息

--- app/controllers/oauth2_controller.rb
+++ app/controllers/oauth2_controller.rb
@@ -37,16 +37,32 @@ class Oauth2Controller < ApplicationController
     data = {:token=> token.token, :expires_in => token.expires_in, :expires_at => token.expires_at, :wb_uid => uid }
     user = User.find_by_wb_uid uid
     if user.nil?
+      sina_info = get_user_info(uid,token.token)
       user = User.new
       user.wb_uid = uid
       user.password = Digest::SHA1.hexdigest(":dface#{user.wb_uid}")[0,16]
+      if sina_info
+        user.name = sina_info["screen_name"]
+        user.gender = 1 if sina_info["gender"]=="m"
+        user.gender = 2 if sina_info["gender"]=="f"
+      end
       user.save!
     end
     session[:user_id] = user.id
     $login_users << user.id
-    data.merge!( {:id => user.id, :password => user.password} )
+    data.merge!( {:id => user.id, :password => user.password, :name => user.name, :gender => user.gender} )
     data.merge!( user.head_logo_hash  )
 	  render :json => data.to_json
+  end
+  
+  def get_user_info(uid,token)
+    require 'open-uri'
+    url = "https://api.weibo.com/2/users/show.json?uid=#{uid}&source=#{$sina_api_key}&access_token=#{token}"
+    
+    open(url) do |f|
+      return ActiveSupport::JSON.decode( f.gets )
+    end
+    return nil
   end
   
 end
