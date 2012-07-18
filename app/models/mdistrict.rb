cb48c08d7cf0eb05c9df5f4b0a34005ed84fe11d
pulsen <huang123qwe@126.com>
2012-07-18 17:08:17 +0800

    定评网抓取调整

--- app/models/mdistrict.rb
+++ app/models/mdistrict.rb
@@ -11,4 +11,21 @@ class Mdistrict < ActiveRecord::Base
   def mcity_mdistrict(mcity_id)
     return McityMdistrict.find_by_mcity_id_and_mdistrict_id(mcity_id, self.id)
   end
+
+  def sub_district
+    return Mdistrict.find_all_by_nest_id(self.id)
+  end
+
+  
+  # 所有尖端节点
+  def sleaf(s=[])
+    sub= sub_district
+    unless sub.blank?
+      sub.each{|sb| sb.sleaf(s)}
+      s
+    else
+      s << self
+    end
+  end
+
 end
