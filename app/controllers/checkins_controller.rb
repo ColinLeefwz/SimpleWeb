8a28f15852e299cd017df5b1f2d05bcb2c73856b
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-07-21 11:53:11 +0800

    将签到数据从mysql迁移到mongodb

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -13,45 +13,26 @@ class CheckinsController < ApplicationController
     end
   end
 
-  # GET /checkins/1
-  # GET /checkins/1.json
   def show
     @checkin = Checkin.find(params[:id])
-
+  
     respond_to do |format|
       format.html # show.html.erb
       format.json { render :json => @checkin }
     end
   end
-
-  # GET /checkins/new
-  # GET /checkins/new.json
-  def new
-    @checkin = Checkin.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render :json => @checkin }
-    end
-  end
-
-  # GET /checkins/1/edit
-  def edit
-    @checkin = Checkin.find(params[:id])
-  end
-
+  
   # POST /checkins
   # POST /checkins.json
   def create
     @checkin = Checkin.new
-    @checkin.lat = params[:lat]
-    @checkin.lng = params[:lng]
+    @checkin.loc = [params[:lat].to_f, params[:lng].to_f]
     @checkin.accuracy = params[:accuracy]
     @checkin.user_id = params[:user_id]
     @checkin.mshop_id = params[:mshop_id]
     @checkin.shop_name = params[:shop_name]
     @checkin.ip = real_ip
-    @checkin.time = Time.now
+    @checkin.cat = Time.now
 
     respond_to do |format|
       if @checkin.save
@@ -59,22 +40,6 @@ class CheckinsController < ApplicationController
         format.json { render :json => @checkin, :status => :created, :location => @checkin }
       else
         format.html { render :action => "new" }
-        format.json { render :json => @checkin.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /checkins/1
-  # PUT /checkins/1.json
-  def update
-    @checkin = Checkin.find(params[:id])
-
-    respond_to do |format|
-      if @checkin.update_attributes(params[:checkin])
-        format.html { redirect_to @checkin, :notice => 'Checkin was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render :action => "edit" }
         format.json { render :json => @checkin.errors, :status => :unprocessable_entity }
       end
     end
