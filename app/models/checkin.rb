c81122ffed75c81a1cedd7f5153d6b47a186da7f
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-07-21 11:53:11 +0800

    将签到数据从mysql迁移到mongodb

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,7 +1,10 @@
 class Checkin < ActiveRecord::Base
-  belongs_to :mshop
-  belongs_to :user
-
-  validates_length_of :id, :maximum => 64
-  validates_length_of :shop_name, :maximum => 64
+  include Mongoid::Document
+  field :mshop_id, type: Integer
+  field :user_id, type: Integer
+  field :loc, type:Array
+  field :ip
+  field :shop_name
+  field :cat, type:DateTime
+  field :accuracy, type:Float
 end
