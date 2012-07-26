95245ad82f66cdd15099ac39a0d9dd06e7795920
dooo <dooo@www.1dooo.com>
2012-07-26 22:56:28 +0800

    删除重复的地名

--- db/mongo/duplicate_remove.js
+++ db/mongo/duplicate_remove.js
@@ -1,8 +1,7 @@
+print(db.mapabc2.count());
+
 db.mapabc2.find().forEach(function(x){
-  var flag=false;
-  db.mapabc2.find({loc : { $near : x.loc , $maxDistance : 0.05 },name:x.name, _id:{$ne : x._id}}).forEach(function(xx){
-    printjson(xx);
-    flag=true;
-  });
-  if(flag) printjson(x);
-})
+  db.mapabc2.remove({loc : { $near : x.loc , $maxDistance : 0.05 },name:x.name, _id:{$ne : x._id}});
+});
+
+print(db.mapabc2.count());
