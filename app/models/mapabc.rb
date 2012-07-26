4a35d11a4093bcfe816eb977a6716d354c69ad97
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-07-26 11:07:51 +0800

    使用mapcabc的数据查询

---
+++ app/models/mapabc.rb
@@ -0,0 +1,33 @@
+class Mapabc
+  include Mongoid::Document
+  store_in collection: "mapabc"
+  field :name
+  field :loc, type:Array
+  field :phone
+  field :city
+  field :addr
+  field :type
+  
+  
+  
+  def safe_output
+    self.attributes.slice("id", "name", "phone").merge!( {"lat"=>self.loc[0], "lng"=>self.loc[1], "address"=>self.addr} )
+  end
+  
+  def safe_output_with_users
+    a,b,c = users_count
+    safe_output.merge!( {"user"=>a, "male"=>b, "female"=>c} )
+  end
+  
+  def users_count
+    us = users.size
+    female = users.where("gender=2").size
+    [us,us-female,female]
+  end
+  
+  def users
+    User.where("name is not null and id<60").order("id asc")
+  end
+  
+  
+end
