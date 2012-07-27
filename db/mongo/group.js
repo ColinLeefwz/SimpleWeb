00d8d1d29dbeb60fdddc9d98c75c8ca9bdd3c79c
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-07-27 17:35:08 +0800

    experiment

--- db/mongo/group.js
+++ db/mongo/group.js
@@ -1,4 +1,20 @@
-db.mapabc2.find({city:"0571"}).forEach(function(x){
-var reg = new RegExp("^"+x.name+".+");
-db.mapabc2.find({loc : { $near : x.loc , $maxDistance : 0.05 },name:reg}).forEach(function(xx){printjson(xx);})
+var num=0;
+db.mapabc2.find().sort({name:1}).forEach(function(x){
+try{
+  num +=1;
+  var flag=false;
+  var reg = new RegExp("^" + x.name + "\\\(" );
+  db.mapabc2.find({loc : { $near : x.loc , $maxDistance : 0.15 },name:reg}).forEach(function(xx){
+    print(xx._id);
+    flag=true;
+  });
+  if(flag){
+	print(x._id);
+	print(".\n");
+  }	
+  print(":"+num);
+}catch(e){
+	print(e);
+}	
+
 })
