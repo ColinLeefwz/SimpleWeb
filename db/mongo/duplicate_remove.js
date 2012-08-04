9f6c46f9afd29ffd78d7b35b7940e47b4dd84416
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-08-04 10:40:13 +0800

    misc

--- db/mongo/duplicate_remove.js
+++ db/mongo/duplicate_remove.js
@@ -1,7 +1,7 @@
 print(db.mapabc2.count());
 
 db.mapabc2.find().forEach(function(x){
-  db.mapabc2.remove({loc : { $near : x.loc , $maxDistance : 0.05 },name:x.name, _id:{$ne : x._id}});
+  db.mapabc2.remove({loc : { $near : x.loc , $maxDistance : 0.001 },name:x.name, _id:{$ne : x._id}});
 });
 
 print(db.mapabc2.count());
