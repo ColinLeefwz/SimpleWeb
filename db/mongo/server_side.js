17caf90fa882f5310e9814a36da10b8555febfa3
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-08-05 09:17:14 +0800

    数据纠偏和server side js查询

--- db/mongo/server_side.js
+++ db/mongo/server_side.js
@@ -23,22 +23,21 @@ db.system.js.save({ "_id" : "offline_size_all", "value" : offline_size_all })
 
 //经纬度纠偏
 
-var get_distance4 = function( lat1,  lng1,  lat2,  lng2){
-    function rad(d){
-        return d * Math.PI / 180.0;
-    }
-    var radLat1 = rad(lat1);
-    var radLat2 = rad(lat2);
-    var a = radLat1 - radLat2;
-    var  b = rad(lng1) - rad(lng2);
-    var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a/2),2) +
-                                    Math.cos(radLat1)*Math.cos(radLat2)*Math.pow(Math.sin(b/2),2)));
-    s = s *6378.137 ;// EARTH_RADIUS;
-    s = Math.round(s * 10000) / 10;
-    return s;
-}
-
 var get_distance = function( loc1,loc2){
+	var get_distance4 = function( lat1,  lng1,  lat2,  lng2){
+	    function rad(d){
+	        return d * Math.PI / 180.0;
+	    }
+	    var radLat1 = rad(lat1);
+	    var radLat2 = rad(lat2);
+	    var a = radLat1 - radLat2;
+	    var  b = rad(lng1) - rad(lng2);
+	    var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a/2),2) +
+	                                    Math.cos(radLat1)*Math.cos(radLat2)*Math.pow(Math.sin(b/2),2)));
+	    s = s *6378.137 ;// EARTH_RADIUS;
+	    s = Math.round(s * 10000) / 10;
+	    return s;
+	}
     return get_distance4(loc1[0],loc1[1],loc2[0],loc2[1]);
 }
 
@@ -54,3 +53,22 @@ var real_to_gcj02_distance = function(loc){
 db.system.js.save({ "_id" : "get_distance", "value" : get_distance });
 db.system.js.save({ "_id" : "real_to_gcj02", "value" : real_to_gcj02 });
 db.system.js.save({ "_id" : "real_to_gcj02_distance", "value" : real_to_gcj02_distance });
+
+var gcj02_to_real = function(loc){
+    var tmp = db.offsets.findOne({loc: {$near : loc} });
+    return [loc[0]-tmp.d[0],loc[1]-tmp.d[1]];
+};
+
+db.system.js.save({ "_id" : "gcj02_to_real", "value" : gcj02_to_real });
+
+var find_shops = function(loc,accuracy,ip){
+	var loc2 = real_to_gcj02(loc);
+    var cursor = db.shops.find({loc:{$within:{$center:[loc2,0.003]}}}).limit(10);
+	var ret = [];
+	while ( cursor.hasNext() ) ret.push(cursor.next());
+    return ret;
+};
+
+db.system.js.save({ "_id" : "find_shops", "value" : find_shops });
+
+
