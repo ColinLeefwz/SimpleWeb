b7fb0bfd7d2eb057e875b1319af8c18240996369
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-08-19 20:24:24 +0800

    fix

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,12 +1,15 @@
 class Checkin
   include Mongoid::Document
   field :shop_id, type: Integer
-  field :user_id, type: Integer
+  field :user_id
   field :loc, type:Array
   field :ip
   field :shop_name
-  field :cat, type:DateTime
   field :accuracy, type:Float
   field :od, type: Integer
+  
+  def cat
+    self._id.generation_time
+  end
   
 end
