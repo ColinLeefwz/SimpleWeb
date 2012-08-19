6fccef2025f0a318bb7e8d34762d274fd88a8216
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-08-19 20:24:24 +0800

    fix

--- app/controllers/mshop_controller.rb
+++ app/controllers/mshop_controller.rb
@@ -4,27 +4,6 @@ class MshopController < ApplicationController
   def index
   end
   
-  def nearby
-    page = params[:page].to_i
-    pcount = params[:pcount].to_i
-    page = 1 if page==0
-    pcount = 20 if pcount==0
-    skip = (page-1)*pcount
-    loc = Offset.offset(params[:lat].to_f , params[:lng].to_f)
-    hash = { loc: { "$within" => { "$center" => [loc, 0.1]} }}
-    hash.merge!( {name: /#{params[:name]}/ }  )  if params[:name]
-    hash.merge!( {t: params[:type].to_i }  )  if params[:type]
-    shops = Shop.where(hash).skip(skip).limit(pcount)
-    render :json =>  shops.map {|s| s.safe_output_with_users}.to_json
-  end
-  
-  def users
-    shop = Shop.find(params[:id].to_i)
-    # TODO: 实际计算最后出现时间
-    render :json => shop.users.map {|u| u.safe_output_with_relation(session[:user_id]).merge!({time:"1 minute"})}.to_json
-  end
-  
-  
   def map1
     @mshop = Mshop.find_by_dp_id(params[:id])
   end
