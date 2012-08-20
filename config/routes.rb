712f5be93f7a9a0b666c8f92efa296e4f3e6e729
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-08-20 19:55:11 +0800

    misc

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,9 @@
 Lianlian::Application.routes.draw do
   resources :shop_notices
   resources :photos
-
-  get "user_info/get"
-
-
   resources :checkins
 
-  get "aroundme/shops"
-  get "aroundme/users"
-
   post "shops/manual"
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
   
   resources :users do
     resources :user_logos
