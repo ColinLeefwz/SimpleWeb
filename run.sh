bd7edcc3ed7e07fbe3cbaeb4bf9731f5736a8a7e
dooo <dooo@www.1dooo.com>
2012-08-27 16:43:07 +0800

    生产环境使用unicorn替代webrick

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-sudo rails s -p 80
+unicorn -D -E production -c unicorn.conf.rb
+#sudo rails s -p 80
 #sudo ruby script/server --port=80 
 #ruby script/server_ssl &
 #ruby script/runner app/check_sms.rb &
