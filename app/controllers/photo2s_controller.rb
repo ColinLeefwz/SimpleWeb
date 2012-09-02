aceb802d0b286cef2272a6555e6c7d489d23c196
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-09-02 11:53:14 +0800

    个人之间聊天时传图单独处理

---
+++ app/controllers/photo2s_controller.rb
@@ -0,0 +1,25 @@
+class Photo2sController < ApplicationController
+  before_filter :user_login_filter
+
+  def create
+    p = Photo2.new(params[:photo])
+    p.user_id = session[:user_id]
+    p.save!
+    render :json => p.output_hash.to_json
+  end
+  
+  def show
+    photo = Photo2.find(params[:id])
+    if params[:size].to_i==0
+      response.headers['IMG_URL'] = photo.img.url
+      redirect_to photo.img.url
+    elsif params[:size].to_i==2
+      response.headers['IMG_URL'] = photo.img.url(:t2)
+      redirect_to photo.img.url(:t2)
+    else
+      response.headers['IMG_URL'] = photo.img.url(:t1)
+      redirect_to photo.img.url(:t1)
+    end
+  end
+
+end
