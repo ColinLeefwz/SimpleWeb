7b09d9da91e9c833bab7b5081f363de010581987
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-09-02 17:45:12 +0800

    refactor

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,10 +3,8 @@ class CouponsController < ApplicationController
     cp = Coupon.find(params[:id])
     
     if params[:size].to_i==0
-      response.headers['IMG_URL'] = cp.img.url
       redirect_to cp.img.url
     else
-      response.headers['IMG_URL'] = cp.img.url(:t1)
       redirect_to cp.img.url(:t1)
     end
   end
