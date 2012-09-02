7ab5802fa87fc53d26a88be9139187ce583792a8
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-09-02 12:47:25 +0800

    优惠券调整

---
+++ app/uploaders/coupon_uploader.rb
@@ -0,0 +1,30 @@
+# encoding: utf-8
+
+class CouponUploader < CarrierWave::Uploader::Base
+
+  include CarrierWave::MiniMagick
+
+  storage :aliyun
+  
+  def store_dir
+    "#{model.id}"
+  end
+  
+  def aliyun_bucket
+    "coupon"
+  end
+  
+  def filename
+    "0.jpg" if original_filename
+  end
+
+  process :resize_to_fit => [610, 306]
+  
+  version :t1 do
+    process :resize_to_fit => [305, 153]
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+end
