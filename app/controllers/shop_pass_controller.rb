d185c3708f516a4392a58e6631ba13baac9776c2
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-09-07 10:01:26 +0800

    misc

--- app/controllers/shop_pass_controller.rb
+++ app/controllers/shop_pass_controller.rb
@@ -3,8 +3,8 @@ class ShopPassController < ApplicationController
   before_filter :shop_authorize
   def index
     if request.post?
-      return flash.now[:notice] = " 原来的密码输入不正确！ " if @shop.password != params[:oldpass]
-      if @shop.update_attributes(params[:shop])
+      return flash.now[:notice] = " 原来的密码输入不正确！ " if session_shop.password != params[:oldpass]
+      if session_shop.update_attributes(params[:shop])
         flash[:notice] = '密码修改成功.'
         return redirect_to '/shop_login/index'
       end
