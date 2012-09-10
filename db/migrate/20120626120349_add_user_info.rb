b929a1eed867142cbcb99045be8a10ce62a51e5d
pulsen <huang123qwe@126.com>
2012-09-10 16:22:33 +0800

    主分支删除迁移文件

--- db/migrate/20120626120349_add_user_info.rb
+++
@@ -1,9 +0,0 @@
-class AddUserInfo < ActiveRecord::Migration
-  def change
-    add_column :users, :signature, :string
-    add_column :users, :job, :string
-    add_column :users, :jobtype, :integer
-    add_column :users, :hobby, :string
-  end
-
-end
