5ceced5d664fe42e6c196040cd82c06399d28305
pulsen <huang123qwe@126.com>
2012-09-10 16:22:33 +0800

    主分支删除迁移文件

--- db/migrate/20120214111118_add_referer_access_log.rb
+++
@@ -1,9 +0,0 @@
-class AddRefererAccessLog < ActiveRecord::Migration
-  def self.up
-	add_column :access_logs, :referer, :string, :length => 64
-  end
-
-  def self.down
-	remove_column :access_logs, :referer
-  end
-end
--- db/migrate/20120214111120_add_agent_access_log.rb
+++
@@ -1,9 +0,0 @@
-class AddAgentAccessLog < ActiveRecord::Migration
-  def self.up
-	add_column :access_logs, :agent, :string, :length => 100
-  end
-
-  def self.down
-	remove_column :access_logs, :agent
-  end
-end
