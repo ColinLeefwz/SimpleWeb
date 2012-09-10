c65240d115facb5950b16aa60c7114545eacea15
pulsen <huang123qwe@126.com>
2012-09-10 16:22:33 +0800

    主分支删除迁移文件

--- db/migrate/20120613034304_manual_mshops.rb
+++
@@ -1,9 +0,0 @@
-class ManualMshops < ActiveRecord::Migration
-  def up
-    add_column :mshops, :manual, :boolean, :default => false
-  end
-
-  def down
-    remove_column :mshops, :manual
-  end
-end
