eddd6ea8f90b7550e07ec03e8efa1e0cfeeae1fb
pulsen <huang123qwe@126.com>
2012-09-11 15:40:07 +0800

    查看签到记录,本周／本月签到统计排行(根据总数排序)

---
+++ app/controllers/shop_checkins_controller.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+
+class ShopCheckinsController < ApplicationController
+  before_filter :shop_authorize
+
+  def index
+    @checkins = Checkin.where({shop_id: session[:shop_id]}).sort({_id: -1})
+  end
+
+  def show
+    @checkin = Checkin.find(params[:id])
+  end
+
+  def bank
+    case params[:flag]
+    when 'week'
+      objectid = 1.weeks.ago.beginning_of_day.to_i.to_s(16).ljust(24,'0')
+    when 'month'
+      objectid = 1.months.ago.beginning_of_day.to_i.to_s(16).ljust(24,'0')
+    end
+    shop_id = session[:shop_id]
+    str = "groupCheckin(#{shop_id}, '#{objectid}')"
+    @banks = Mongoid.default_session.command(eval:str)["retval"]
+    @banks = @banks.sort{|f,s| s['count'] <=> f['count']}
+  end
+
+
+
+end
