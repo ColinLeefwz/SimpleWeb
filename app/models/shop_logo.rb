5ef7c5ee7fa4e8ed4e6b1f6a9cefb1ea99be2929
pulsen <huang123qwe@126.com>
2012-09-12 10:20:17 +0800

    商家logo上传

--- app/models/shop_logo.rb
+++ app/models/shop_logo.rb
@@ -8,4 +8,12 @@ class ShopLogo
   mount_uploader(:img, PhotoUploader) { def aliyun_bucket; "logo"+bucket_suffix ; end }
 
 
+  def self.shop_logo(shop_id)
+    (self.where({shop_id: shop_id}).sort({_id: -1}).entries).first
+  end
+
+  def logo_thumb_hash
+    {:logo => self.img.url, :logo_thumb => self.img.url(:t1), :logo_thumb2 => self.img.url(:t2)  }
+  end
+
 end
