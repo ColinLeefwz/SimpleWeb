c65fdccf54c863f2c265854c06e5179db5e43ed6
pulsen <huang123qwe@126.com>
2012-09-12 10:20:17 +0800

    商家logo上传

---
+++ app/controllers/shop_logos_controller.rb
@@ -0,0 +1,21 @@
+class ShopLogosController < ApplicationController
+  before_filter :shop_authorize
+  
+  def index
+    @shop_logo = ShopLogo.shop_logo(session[:shop_id])
+  end
+
+  def new
+    @shop_logo = ShopLogo.new
+  end
+
+  def create
+    @shop_logo=  ShopLogo.new(params[:shop_logo])
+    @shop_logo.shop_id = session[:shop_id]
+    if @shop_logo.save
+      redirect_to :action => :index
+    else
+      render :action => :new
+    end
+  end
+end
