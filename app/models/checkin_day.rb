0c5e936bd26ff9394a55ba8d8d7882b829ea733a
pulsen <huang123qwe@126.com>
2012-09-19 10:21:25 +0800

    admin 后台看每日签到统计

--- app/models/checkin_day.rb
+++ app/models/checkin_day.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class CheckinDay
   include Mongoid::Document
   field :_id,  type: String
@@ -6,4 +7,8 @@ class CheckinDay
   field :od2, type: Integer
   field :od3, type: Integer
   field :shops, type:Array
+
+  def show_shops
+    self.shops.map{|m| "#{Shop.find_by_id(m[0]).try(:name)}: #{m[1]}"}.join(', ')
+  end
 end
