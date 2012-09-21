feea81da043d72dd6e9cfabb6ebcda6aac072c97
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-09-21 10:36:36 +0800

    misc

--- app/controllers/init_controller.rb
+++ app/controllers/init_controller.rb
@@ -1,11 +1,11 @@
 class InitController < ApplicationController
   def init
-    hash = Digest::SHA1.hexdigest("#{params[:os]}#{params[:mac]}init")[0,32]
+    hash = Digest::SHA1.hexdigest("#{model}#{params[:os]}#{params[:mac]}init")[0,32]
     if hash != params[:hash]
       render :json => {error: "hash error: #{hash}."}.to_json
       return
     end
-    logger.info "INIT: #{params[:os]} , #{params[:mac]}"
-    render :json => {ip: "60.191.119.190"}.to_json
+    logger.info "INIT: #{params[:model]} , #{params[:os]} , #{params[:mac]}"
+    render :json => {ip: "60.191.119.190", xmpp:"60.191.119.190" }.to_json
   end
 end
