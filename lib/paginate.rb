43048e6e2a6fed23bd4af7792f313043bdbca8ba
pulsen <huang123qwe@126.com>
2012-09-25 15:38:54 +0800

    管理员签到列表

--- lib/paginate.rb
+++ lib/paginate.rb
@@ -3,9 +3,9 @@ module Paginate
     @page =  page ? page.to_i : 1
     @pcount =  pcount ? pcount.to_i : 15
     cons = model.classify.constantize
-    @total_entries = cons.where(hash[:sql]).length
+    @total_entries = cons.where(hash).length
     @last_page = (@total_entries+@pcount-1)/@pcount
     skip = (@page - 1)*@pcount
-    model.constantize.where(hash[:sql]).skip(skip).limit(@pcount).sort(sort)
+    cons.where(hash).skip(skip).limit(@pcount).sort(sort)
   end
 end
