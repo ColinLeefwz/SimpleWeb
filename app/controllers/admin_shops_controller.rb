4c4bc2082f94e64b8afc1d5c71560f7a727befdd
pulsen <huang123qwe@126.com>
2012-09-25 10:19:49 +0800

    简单分页

--- app/controllers/admin_shops_controller.rb
+++ app/controllers/admin_shops_controller.rb
@@ -1,14 +1,11 @@
 class AdminShopsController < ApplicationController
-
+  include Paginate
+  
   before_filter :admin_authorize
   layout "admin"
 
   def index
-    @page = params[:page].to_i
-    @pcount = params[:pcount].to_i
-    @page = 1 if @page==0
-    @pcount = 200 if @pcount==0
-    skip = (@page - 1)*@pcount
+
     hash,lo = {},nil
     lo = [params[:lat].to_f , params[:lng].to_f] if !params[:lat].blank? && !params[:lng].blank?
     hash.merge!({ lo: { "$within" => { "$center" => [lo, 0.1]} }}) if lo
@@ -17,12 +14,10 @@ class AdminShopsController < ApplicationController
     hash.merge!( {level: params[:level]}) if !params[:level].blank?
     hash.merge!({city: params[:city]}) if !params[:city].blank?
 
-    @shops_len = Shop.where(hash).length
-    @last_page = (@shops_len+@pcount-1)/@pcount
-
-
-    @shops = Shop.where(hash).skip(skip).limit(@pcount).sort(horder)
+    @shops = paginate("Shop", hash, horder, params[:page], 200  )
     @shops = @shops.entries.keep_if{|s| s.del != 1}
+
+
   end
 
 
