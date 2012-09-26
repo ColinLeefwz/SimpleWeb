036891ad75adeb57eb9d50ee103daeebb40c89e1
pulsen <huang123qwe@126.com>
2012-09-26 16:58:03 +0800

    商家每日签到添加utotal,ctotal字段

--- app/models/checkin_shop_stat.rb
+++ app/models/checkin_shop_stat.rb
@@ -3,6 +3,8 @@ class CheckinShopStat
   field :_id, type: Integer
   field :users, type:Array
   field :ips, type:Array
+  field :utotal, type: Integer
+  field :ctotal, type: Integer
 
   def shop
     Shop.find_by_id(self.id)
