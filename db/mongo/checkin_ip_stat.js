2b42e61c07e0c492cc317abf7b51bddef8152873
pulsen <huang123qwe@126.com>
2012-10-08 16:12:24 +0800

    js checkin id 代码提取

--- db/mongo/checkin_ip_stat.js
+++ db/mongo/checkin_ip_stat.js
@@ -1,13 +1,12 @@
-var checkinIpStat = function(days){
-    var z = '0000000000000000'
-    var yesterday = new Date(parseInt(((new Date()).valueOf()/1000)-(24*60*60))*1000)
-    var daysago = new Date(parseInt(((new Date()).valueOf()/1000)-(days*24*60*60))*1000)
-    var idOfBeginYesterday = parseInt(daysago.setHours(0,0,0)/1000).toString(16) + z
-    var idOfEndYesterday = parseInt(yesterday.setHours(23,59,59)/1000).toString(16) + z
+db.loadServerScripts();
 
+var checkinIpStat = function(days){
+    var idOfBeginDaysAgo;
+    var idOfEndYesterday;
+    [idOfBeginDaysAgo, idOfEndYesterday] = gen_days_id(days)
     db.checkins.find({
         _id: {
-            $gt: ObjectId(idOfBeginYesterday),
+            $gt: ObjectId(idOfBeginDaysAgo),
             $lt: ObjectId(idOfEndYesterday)
         },
         ip: {
