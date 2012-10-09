1fdddefd8cf3da081f28cefb7f3fb11ce6cd9f59
pulsen <huang123qwe@126.com>
2012-10-09 14:58:18 +0800

    ip 获取信息

--- db/mongo/ip_info.rb
+++ db/mongo/ip_info.rb
@@ -1,11 +1,13 @@
-ips = CheckinIpStat.where({:city => {'$exists' => false}})
+ips = CheckinIpStat.where({:data => {'$exists' => false}})
 
 ips.each do |ip|
   begin
     uri = "http://ip.taobao.com/service/getIpInfo.php?ip=" + ip.id
     ipinfo_hash = JSON.parse(RestClient.get(uri))
     next if ipinfo_hash['code']== 1
-    ip.data = ipinfo_hash['data']
+    data = ipinfo_hash['data']
+    data.delete('ip')
+    ip.data = data
     ip.save!
   rescue
     next
