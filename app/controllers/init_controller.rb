500081f8e34c9ff8ac4786b84552743489b84bf6
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-10-10 22:25:48 +0800

    xmpp virtual ip

--- app/controllers/init_controller.rb
+++ app/controllers/init_controller.rb
@@ -6,8 +6,7 @@ class InitController < ApplicationController
       return
     end
     logger.info "INIT: #{params[:model]} , #{params[:os]} , #{params[:mac]}, #{params[:ver]}"
-		ip = "60.191.119.190"
 		#ip = "192.168.244.4" if real_ip=="58.100.92.146"
-    render :json => {ip: ip, xmpp:"42.121.98.157" }.to_json
+    render :json => {ip: "60.191.119.190", xmpp: $xmpp_ip }.to_json
   end
 end
