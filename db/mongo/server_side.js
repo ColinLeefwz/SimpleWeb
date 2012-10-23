6b39c4d7a8370bda9ecb4e7b43bbf7dd2e6ddb8b
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-10-23 10:35:28 +0800

    取消离线消息统计，因为不再使用openfire+mongodb保存离线消息

--- db/mongo/server_side.js
+++ db/mongo/server_side.js
@@ -1,39 +1,3 @@
-//离线消息大小的统计
-
-var sum_reduce = function(key, cols){
-    var count=0;
-    cols.forEach(function(v) {
-        count+=v[key];
-    });
-    return {
-        sum: count
-    };
-};
-
-var offline_size_user = function(user){
-    return sum_reduce('len',db.offlines.find({
-        "user":user
-    }))
-}
-
-var offline_size_all = function(){
-    return sum_reduce('len',db.offlines.find())
-}
-
-db.system.js.save({
-    "_id" : "sum_reduce",
-    "value" : sum_reduce
-})
-db.system.js.save({
-    "_id" : "offline_size_user",
-    "value" : offline_size_user
-})
-db.system.js.save({
-    "_id" : "offline_size_all",
-    "value" : offline_size_all
-})
-
-
 //经纬度纠偏
 
 var num_to_rad = function(d){
