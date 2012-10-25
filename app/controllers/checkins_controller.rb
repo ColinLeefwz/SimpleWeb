7abca6f82133f157faf60325a099fe5a755a281f
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-10-25 16:27:05 +0800

    refactor

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,43 +1,32 @@
 # coding: utf-8
 
 class CheckinsController < ApplicationController
-  # GET /checkins
-  # GET /checkins.json
-  def index
-    #request.headers.keys.each do |key|
-    #      logger.debug "#{key} : #{request.headers[key]}"
-    #end
-    @checkins = Checkin.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @checkins }
-    end
-  end
-
-  def show
-    @checkin = Checkin.find(params[:id])
-  
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render :json => @checkin }
-    end
-  end
-
-  def new
-    @checkin = Checkin.new
-  end
-  # POST /checkins
-  # POST /checkins.json
   def create
     raise "user != session user" if params[:user_id].to_s != session[:user_id].to_s
-    @checkin = Checkin.new
-    @checkin.loc = [params[:lat].to_f, params[:lng].to_f]
-    @checkin.acc = params[:accuracy]
-    @checkin.uid = Moped::BSON::ObjectId(params[:user_id]) 
-    @checkin.sex = session_user.gender
+    send_welcome_msg_if_not_invisible
+    checkin = Checkin.new
+    checkin.loc = [params[:lat].to_f, params[:lng].to_f]
+    checkin.acc = params[:accuracy]
+    checkin.uid = Moped::BSON::ObjectId(params[:user_id]) 
+    checkin.sex = session_user.gender
+    checkin.sid = params[:shop_id]
+    checkin.od = params[:od]
+    if params[:altitude]
+      checkin.alt = params[:altitude].to_f
+      checkin.altacc = params[:altacc]
+    end
+    checkin.ip = real_ip
+    checkin.save!
+    send_coupon_if_exist
+    render :json => checkin.to_json
+  end
+
+  private
+
+  def send_welcome_msg_if_not_invisible
     if session_user.invisible!=2
-      if @checkin.sex==2
+      if checkin.sex==2
         message = "Hi，我来了~😊"
       else
         message = "Hi，我来啦~😝"
@@ -45,40 +34,17 @@ class CheckinsController < ApplicationController
       RestClient.post("http://#{$xmpp_ip}:5280/api/room", 
           :roomid  => params[:shop_id].to_s , :message=> message ,
           :uid => params[:user_id].to_s)  {|response, request, result| puts response }
-    end
-
-    @checkin.sid = params[:shop_id]
-    @checkin.od = params[:od]
-    if params[:altitude]
-      @checkin.alt = params[:altitude].to_f
-      @checkin.altacc = params[:altacc]
-    end
-    @checkin.ip = real_ip
-
-    respond_to do |format|
-      if @checkin.save
-        coupon = Coupon.where({shop_id:params[:shop_id]}).last
-        coupon = Coupon.gen_demo(params[:shop_id]) if coupon.nil?
-        coupon.send_coupon(session[:user_id]) if coupon
-        format.html { redirect_to @checkin, :notice => 'Checkin was successfully created.' }
-        format.json { render :json => @checkin, :status => :created, :location => @checkin }
-      else
-        format.html { render :action => "new" }
-        format.json { render :json => @checkin.errors, :status => :unprocessable_entity }
-      end
+      #TODO: 处理rest调用出错和重试
+      #TODO: 消息持久化。目前是直接投递的，导致没有保存。
     end
   end
 
-  # DELETE /checkins/1
-  # DELETE /checkins/1.json
-  def destroy
-    @checkin = Checkin.find(params[:id])
-    @checkin.destroy
-
-    respond_to do |format|
-      format.html { redirect_to checkins_url }
-      format.json { head :no_content }
-    end
+  def send_coupon_if_exist
+    coupon = Coupon.where({shop_id:params[:shop_id]}).last
+    coupon = Coupon.gen_demo(params[:shop_id]) if coupon.nil?
+    coupon.send_coupon(session[:user_id]) if coupon
   end
+
+
 
 end
