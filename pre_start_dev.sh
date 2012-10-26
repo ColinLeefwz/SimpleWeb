f4d539473b0728c76028ea857b01f31b9bc7198a
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-10-26 16:15:27 +0800

    m

--- pre_start_dev.sh
+++ pre_start_dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo mongod --fork --logpath=/var/log/mongo.log
 sudo haproxy -f ./haproxy.development.cfg
-nohup redis-server&
+redis-server /usr/local/etc/redis.conf
 
 #设置www.dface.cn指向本机后，就可以使用发布系统的网址http://www.dface.cn来访问开发环境。
 
