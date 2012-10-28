22bdd51a9db0fe2ff9e6d50d263f6eacd99f4b99
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-10-28 20:50:04 +0800

    附近商家

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -7,13 +7,21 @@ class ShopController < ApplicationController
     pcount = params[:pcount].to_i
     page = 1 if page==0
     pcount = 20 if pcount==0
-    skip = (page-1)*pcount
-    lo = [params[:lat].to_f , params[:lng].to_f]
-    hash = {city:"0571"}
-    hash.merge!( {name: /#{params[:name]}/ }  )  if params[:name]
-    hash.merge!( {t: params[:type].to_i }  )  if params[:type]
-    shops = Shop.where(hash).order_by([:utotal,:desc]).skip(skip).limit(pcount)
-    render :json =>  shops.map {|s| s.safe_output_with_users}.to_json
+    str = "nearby_shops([#{params[:lat]},#{params[:lng]}],#{page},#{pcount}"
+    if params[:name]
+      str << ",#{params[:t].to_i},/#{params[:name]}/)"
+    elsif params[:t]
+      str << ",#{params[:t]})"
+    else
+      str << ")"
+    end
+    arr = Mongoid.default_session.command(eval:str)["retval"]
+    hash = arr.map do |x|
+      s = Shop.new(x)
+      s.id = x["_id"].to_i
+      s.safe_output_with_users
+    end
+    render :json =>  hash.to_json
   end
   
   def users
