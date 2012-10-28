5d5cd18de32531e55c5734dafe9b41ce5ff2f702
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-10-28 21:14:57 +0800

    附近商家排序

--- db/mongo/server_side.js
+++ db/mongo/server_side.js
@@ -228,7 +228,12 @@ var nearby_shops = function(loc,page,pcount,t,name){
     var search_hash = {utotal:{$gt:0},city:city};
     if(t>0) search_hash["t"]=t;
     if(name)  search_hash["name"]= name ;
-    var cursor = db.shops.find(search_hash).sort({utotal:-1}).skip(skip).limit(pcount);
+    var cursor = db.shops.find(search_hash).sort({ 
+        $by : function( a, b ){ 
+            if(a.utotal!=b.utotal) return b.utotal-a.utotal;
+            else return get_distance(loc,b.lo) - get_distance(loc,a.lo);
+        }
+    }).skip(skip).limit(pcount);
     var ret = [];
     while ( cursor.hasNext() ) ret.push(cursor.next());
     var diff = pcount-ret.length;
