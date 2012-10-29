7a506568b1d82026aa51bf598b1a5f5851a4e18c
pulsen <huang123qwe@126.com>
2012-10-29 17:36:08 +0800

    优惠券图片显示接口方法

--- coupon/gen_demo.sh
+++ coupon/gen_demo.sh
@@ -7,7 +7,7 @@ fi
 convert -font 'msyh.ttf' -fill "#309797" -pointsize 24 -draw "text 250,55\"${1}\"" -draw "image Over 28,28 189,189 \"${4}\""   demo.png $3
 desc=$2
 leng=${#desc}
-for((i=1;i<=((leng+14)/15);i++));do
+for((i=1;i<=(leng+14)/15;i++));do
  hi=$[70+i*30]
  convert -font 'msyh.ttf' -fill "#3097ff" -pointsize 20 -draw "text 250,$hi\"${desc:((i-1)*15):15}\"" $3 $3
 done
