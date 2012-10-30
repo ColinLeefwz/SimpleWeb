d4a8450704216f01fd4f36225ef9e882b5de6898
pulsen <huang123qwe@126.com>
2012-10-30 14:24:35 +0800

    优惠券图片生成，添加时间

--- coupon/gen_demo.sh
+++ coupon/gen_demo.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-if [ $# -ne 4 ]
+if [ $# -ne 5 ]
 then
    echo "$0 title output_filename"
    exit 1
 fi
-convert -font 'msyh.ttf' -fill "#309797" -pointsize 24 -draw "text 250,55\"${1}\"" -draw "image Over 28,28 189,189 \"${4}\""   demo.png $3
-desc=$2
-leng=${#desc}
-for((i=1;i<=(leng+14)/15;i++));do
- hi=$[70+i*30]
- convert -font 'msyh.ttf' -fill "#3097ff" -pointsize 20 -draw "text 250,$hi\"${desc:((i-1)*15):15}\"" $3 $3
-done
+convert -font 'msyh.ttf' -fill "#309797" -pointsize 24 -kerning 1.5 -draw "text 245,55\"${1}\"" -fill "gray" -pointsize 20 -kerning 1 -draw "text 245,100\"${2}\"" -draw "image Over 28,28 189,189 \"${4}\""   demo.png $3
+convert -font 'msyh.ttf' -fill "red" -pointsize 15 -kerning 2 -draw "text 410,280\"${5}\""  $3 $3
 convert -resize 290x112 $3 "${3}_2.jpg"
 
