198da108081a97827d01e97756f7f842a723352c
pulsen <huang123qwe@126.com>
2012-10-31 11:34:38 +0800

    图片生成

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -59,11 +59,11 @@ class Coupon
     demo = Coupon.new
     demo.shop_id = sid
     demo.name = demo.shop.name+"20元代金券"
-    demo.desc = "凭本券可抵扣现场消费人民币20元\r\n本券不兑现/不找零/不开发票,\r\n复印和涂改无效，请于点餐时\r\n有效期至:#{3.days.since.strftime("%Y-%m-%d日18：00")}\r\n状态:未使用"
+    demo.desc = "凭本券可抵扣现场消费人民币20元\r\n本券不兑现/不找零/不开发票,\r\n复印和涂改无效，请于点餐时\r\n有效期至:#{3.days.since.strftime("%Y-%m-%d日18：00")}\r\n本券最终解释权规商家所有"
     demo.t=1
     demo.save
-    `cd coupon && ./gen_demo.sh '#{demo.name}' '#{demo.desc}' ../public/coupon/#{demo._id}.jpg pic1.jpg '#{demo.cat}'`
-    demo
+    re =  `cd coupon && ./gen_demo.sh '#{demo.name}' '#{demo.desc}' ../public/coupon/#{demo._id}.jpg pic1.jpg`
+    re.blank? ? demo : re
   end
 
   def cat
@@ -76,7 +76,8 @@ class Coupon
       name = self.name
       desc = self.desc
       img = self.img
-      `cd coupon && ./gen_demo.sh '#{name}' '#{desc}' ../public/coupon/#{self._id}.jpg #{img} '#{self.cat}'`
+      re = `cd coupon && ./gen_demo.sh '#{name}' '#{desc}' ../public/coupon/#{self._id}.jpg #{img} '`
+      re.blank? ? self.attributes : re
     end
   end
 
