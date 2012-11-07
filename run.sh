590e879a83b169f28b78481c9aae5e53c9d1addc
dooo <dooo@www.1dooo.com>
2012-11-07 16:19:25 +0800

    misc

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 unicorn -D -E production -c unicorn.conf.rb
-INTERVAL=0.1 PIDFILE=./resque.xmpp.pid BACKGROUND=yes QUEUE='xmpp' rake environment resque:work
-INTERVAL=5 PIDFILE=./resque.normal.pid BACKGROUND=yes QUEUE='normal' rake environment resque:work
-#sudo rails s -p 80
-#sudo ruby script/server --port=80 
-#ruby script/server_ssl &
-#ruby script/runner app/check_sms.rb &
+god -c god/all.god 
 
