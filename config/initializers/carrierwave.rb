f7f16a655b68b4d8164724bf57bc27d1b64dd256
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-11-07 17:34:32 +0800

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,11 @@
 CarrierWave.configure do |config|
   config.storage = :aliyun
-  config.aliyun_access_id = "sc1zhg0deulbspm7cmcns0um"
-  config.aliyun_access_key = '26T2lFww3gsd+0OY8DsSFXj1eQ8='
-  # you need create this bucket first!
-  #config.aliyun_bucket = "dface"
+  if ENV["RAILS_ENV"] != "production"
+    config.aliyun_access_id = "sc1zhg0deulbspm7cmcns0um"
+    config.aliyun_access_key = '26T2lFww3gsd+0OY8DsSFXj1eQ8='
+  else
+    # 发布环境的阿里云id和key, 不保存在版本控制系统中
+  end
 end
 
 module CarrierWave
