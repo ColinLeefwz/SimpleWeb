4f15ec6f660760da39524f4ed74fd63fadf079e9
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-11-07 15:08:55 +0800

    version需要定义在具体的uploader里，否则找不到aliyun_bucket

--- app/models/shop_logo.rb
+++ app/models/shop_logo.rb
@@ -5,7 +5,7 @@ class ShopLogo
   
   field :shop_id, type: Moped::BSON::ObjectId
   field :img
-  mount_uploader(:img, PhotoUploader) { def aliyun_bucket; "logo"+bucket_suffix ; end }
+  mount_uploader(:img, LogoUploader)
 
 
   def self.shop_logo(shop_id)
