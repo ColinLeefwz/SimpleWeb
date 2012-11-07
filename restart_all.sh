5b5e9c6c237f5dec1c9dfbdd576908165cbc3f3c
dooo <dooo@www.1dooo.com>
2012-11-07 16:19:25 +0800

    misc

--- restart_all.sh
+++ restart_all.sh
@@ -2,9 +2,5 @@
 
 #see http://unicorn.bogomips.org/SIGNALS.html
 kill -HUP `cat log/unicorn.pid`
-kill -QUIT `cat ./resque.xmpp.pid`
-kill -QUIT `cat ./resque.normal.pid`
-INTERVAL=0.1 PIDFILE=./resque.xmpp.pid BACKGROUND=yes QUEUE='xmpp' rake environment resque:work
-INTERVAL=5 PIDFILE=./resque.normal.pid BACKGROUND=yes QUEUE='normal' rake environment resque:work
-
+god restart resque
 
