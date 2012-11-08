0817578bc1145ac682d4d8fd63ee499e4f902da2
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-11-08 21:40:10 +0800

    图片上传成功后发送xmpp消息

--- app/models/photo2.rb
+++ app/models/photo2.rb
@@ -13,6 +13,11 @@ class Photo2
   
   index({ user_id: 1 })
   
+  def after_async_store
+    xmpp = "<message to='#{to_uid}@dface.cn' from='#{user_id}@dface.cn' type='chat'><body>[img:#{self.id}]</body></message>"
+    RestClient.post("http://#{$xmpp_ip}:5280/rest", xmpp) 
+  end
+  
   def user
     User.find(self.user_id)
   end
