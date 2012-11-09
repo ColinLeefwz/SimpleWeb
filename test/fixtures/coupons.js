b428d4043d4c4227099644511fb129c5ad83b481
pulsen <huang123qwe@126.com>
2012-11-09 10:52:54 +0800

    优惠券单元测试,use重写,img上传的两种模式

---
+++ test/fixtures/coupons.js
@@ -0,0 +1,29 @@
+db.coupons.remove();
+db.coupons.insert({
+    _id: ObjectId('507fc5bfc9ad42d756a412e1'),
+    shop_id: 1,
+    name: '测试优惠券1',
+    desc: "测试优惠券1详情,规则只能下载一次",
+    ratio: 2,
+    rule: '0'
+})
+
+db.coupons.insert({
+    _id: ObjectId('507fc5bfc9ad42d756a412e2'),
+    shop_id: 1,
+    name: '测试优惠券1',
+    desc: "测试优惠券1详情,规则只能有一个未使用",
+    ratio: 2,
+    rule: 1
+})
+
+db.coupons.insert({
+    _id: ObjectId('507fc5bfc9ad42d756a412e3'),
+    shop_id: 1,
+    name: '测试优惠券1',
+    desc: "测试优惠券1详情,规则无限制下载",
+    ratio: 2,
+    rule: 2
+})
+
+
