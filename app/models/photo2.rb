d0f5c74fa43f362e968e8c9b2fa581f136d8250d
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-11-10 15:54:03 +0800

    个人图片消息id格式："img#{id}"

--- app/models/photo2.rb
+++ app/models/photo2.rb
@@ -14,7 +14,7 @@ class Photo2
   index({ user_id: 1 })
   
   def after_async_store
-    xmpp = "<message id='#{self.id}' to='#{to_uid}@dface.cn' from='#{user_id}@dface.cn' type='chat'><body>[img:#{self.id}]</body></message>"
+    xmpp = "<message id='img#{self.id}' to='#{to_uid}@dface.cn' from='#{user_id}@dface.cn' type='chat'><body>[img:#{self.id}]</body></message>"
     RestClient.post("http://#{$xmpp_ip}:5280/rest", xmpp) 
   end
   
