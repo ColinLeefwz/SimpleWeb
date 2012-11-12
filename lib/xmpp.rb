26fa81337ddad033f6f2a63bd3d9d5ef2041e892
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-11-12 09:20:21 +0800

    add

---
+++ lib/xmpp.rb
@@ -0,0 +1,12 @@
+class Xmpp
+
+  def self.chat(from,to,msg)
+    "<message to='#{to}@dface.cn' from='#{from}@dface.cn' type='chat'><body>#{msg}</body></message>"
+  end
+  
+  def self.gchat(from,to,msg)
+    "<message to='#{to}@dface.cn' from='#{from}@c.dface.cn' type='groupchat'><body>#{msg}</body></message>"
+  end 
+
+
+end
