1fb4d0f725905178503aa8c340e3c1e6ba746771
pulsen <huang123qwe@126.com>
2012-11-12 16:45:09 +0800

    黑名单集成测试

--- test/integration/mobile_api/user_blacks_test.rb
+++ test/integration/mobile_api/user_blacks_test.rb
@@ -12,10 +12,10 @@ class UserBlacksTest < ActionDispatch::IntegrationTest
 
     #登录添加黑名单
     login("502e6303421aa918ba000005")
-    assert_blank session_user.blacks_s
+    assert_blank luser.blacks_s
     post "/blacklists/create", {:user_id => luser.id, :block_id => user1.id}
     assert_response :success
-    assert session_user.blacks_s.detect{|data| data["id"] == user1.id}
+    assert luser.reload.blacks_s.detect{|data| data["id"] == user1.id}
     assert_equal luser.reload.blacks_s.count, 1
     assert_equal response.body, {"id"=>"502e6303421aa918ba00007c","report"=>0,"cat"=>Time.now}.to_json
 
@@ -32,22 +32,22 @@ class UserBlacksTest < ActionDispatch::IntegrationTest
     get "/blacklists?id=#{luser.id}"
     assert_response :success
     data = JSON.parse(response.body).last['data']
-    assert_equal data, [{"name"=>"袁乐天","signature"=>"","wb_uid"=>"a1", "gender"=>1.0,"birthday"=>"","jobtype"=>nil,"multip"=>false,"id"=>"502e6303421aa918ba00007c","logo"=>"","logo_thumb"=>"","logo_thumb2"=>"","friend"=>false,"follower"=>false}]
+    assert_equal data, [{"name"=>"袁乐天","signature"=>"","wb_uid"=>"a1","gender"=>1.0,"birthday"=>"","jobtype"=>nil,"pcount"=>0,"id"=>"502e6303421aa918ba00007c","logo"=>"","logo_thumb"=>"","logo_thumb2"=>"","friend"=>false,"follower"=>false}]
 
     #注销后黑名单列表
     logout
     get "/blacklists?id=#{luser.id}"
     assert_response :success
     data = JSON.parse(response.body).last['data']
-    assert_equal data, [{"name"=>"袁乐天","signature"=>"","wb_uid"=>"a1", "gender"=>1.0,"birthday"=>"","jobtype"=>nil,"multip"=>false,"id"=>"502e6303421aa918ba00007c","logo"=>"","logo_thumb"=>"","logo_thumb2"=>"","friend"=>false,"follower"=>false}]
+    assert_equal data, [{"name"=>"袁乐天","signature"=>"","wb_uid"=>"a1","gender"=>1.0,"birthday"=>"","jobtype"=>nil,"pcount"=>0,"id"=>"502e6303421aa918ba00007c","logo"=>"","logo_thumb"=>"","logo_thumb2"=>"","friend"=>false,"follower"=>false}]
 
     #再添加一个黑名单
     login("502e6303421aa918ba000005")
     post "/blacklists/create", {:user_id => luser.id, :block_id => user2.id}
     assert_response :success
-    assert session_user.blacks_s.detect{|data| data["id"] == user2.id }
+    assert luser.reload.blacks_s.detect{|data| data["id"] == user2.id }
     assert_equal luser.reload.blacks_s.count, 2
-    
+
     #未登录删除黑名单
     logout
     post '/blacklists/delete', {:user_id => luser.id, :block_id => user1.id}
@@ -55,16 +55,16 @@ class UserBlacksTest < ActionDispatch::IntegrationTest
     assert_equal response.body, {"error"=>"user 502e6303421aa918ba000005 != session user "}.to_json
     assert_equal luser.reload.blacks_s.count, 2
     assert luser.reload.blacks_s.detect{|data| data["id"] == user1.id}
-    
+
     #登录删除黑名单
     login("502e6303421aa918ba000005")
-    post '/blacklists/delete', {:user_id => session_user.id, :block_id => user1.id}
+    post '/blacklists/delete', {:user_id => luser.id, :block_id => user1.id}
     assert_response :success
     assert_equal luser.reload.blacks_s.count, 1
     assert_equal luser.reload.blacks_s.detect{|data| data["id"] == user1.id}, nil
 
     #再删除黑名单
-    post '/blacklists/delete', {:user_id => session_user.id, :block_id => user2.id}
+    post '/blacklists/delete', {:user_id => luser.id, :block_id => user2.id}
     assert_response :success
     assert_equal luser.reload.blacks_s.count, 0
     assert_equal luser.reload.blacks_s.detect{|data| data["id"] == user2.id}, nil
