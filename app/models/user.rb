1e759b19b099e88ce2ef5e016f4ec35b8bee9ad3
pulsen <huang123qwe@126.com>
2012-11-15 14:21:20 +0800

    个人信息展示，设置测试

--- app/models/user.rb
+++ app/models/user.rb
@@ -131,6 +131,7 @@ class User
     hash.merge!(last_location(user_id))
   end
 
+  #当前用户的最后位置。 user_id 是查看当前用户的user
   def last_location( user_id )
     return {:last => "隐身"} if block?(user_id)
     loc = Checkin.where({uid:self._id}).sort({_id:1}).last
