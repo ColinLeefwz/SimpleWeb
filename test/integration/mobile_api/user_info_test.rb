dab03d54b9370b3010ec3b11c9822edf808d18a6
pulsen <huang123qwe@126.com>
2012-11-15 14:21:20 +0800

    个人信息展示，设置测试

---
+++ test/integration/mobile_api/user_info_test.rb
@@ -0,0 +1,57 @@
+# coding: utf-8
+require 'test_helper'
+require 'integration/helpers/mobile_helper'
+
+class UserInfoTest < ActionDispatch::IntegrationTest
+  test "设置，查看个人信息" do
+    reload('users.js')
+    reload("checkins.js")
+    luser = User.find('502e6303421aa918ba000005')
+    user1 = User.find('502e6303421aa918ba00007c')
+
+    #未登录获取当前用户信息
+    get "/user_info/get_self"
+    assert_response :success
+    assert_equal response.body, {"error"=>"not login"}.to_json
+
+    #登录获取当前登录用户信息
+    login(luser.id)
+    get "/user_info/get_self"
+    assert_response :success
+    assert_equal JSON.parse(response.body), {"gender"=>1.0,"hobby"=>"","invisible"=>0.0,"jobtype"=>nil,"name"=>"袁乐天","oid"=>154.0,"password"=>"c84dad462d5b7282","signature"=>"","wb_uid"=>"a1","pcount"=>0,"id"=>"502e6303421aa918ba000005","logo"=>"","logo_thumb"=>"","logo_thumb2"=>""}
+
+    #未登录获取他人个人信息
+    logout
+    get "/user_info/get?id=#{luser.id}"
+    assert_response :success
+    assert_equal response.body, {"error"=>"not login"}.to_json
+
+    #登录获取他人个人信息
+    login(user1.id)
+    get "/user_info/get?id=#{luser.id}"
+    assert_response :success
+    assert_equal JSON.parse(response.body), {"gender"=>1.0,"hobby"=>"","invisible"=>0.0,"jobtype"=>nil,"name"=>"袁乐天","oid"=>154.0,"signature"=>"","wb_uid"=>"a1","pcount"=>0,"id"=>"502e6303421aa918ba000005","logo"=>"","logo_thumb"=>"","logo_thumb2"=>"","friend"=>false,"follower"=>false,"last"=>""}
+
+    #未登录设置个人信息
+    logout
+    post '/user_info/set', {:name => "测试name", :gender => 1, :birthday => '1988-08-08', :signature => '测试签名档', :job => '程序员', :jobtype => 1, :hobby => '计算机', :invisible => 0}
+    assert_response :success
+    assert_equal response.body, {"error"=>"not login"}.to_json
+    assert_equal luser.reload.attributes, {"_id"=> luser.id,"gender"=>1.0,"hobby"=>"","invisible"=>0.0,"jobtype"=>nil,"name"=>"袁乐天","oid"=>154.0,"password"=>"c84dad462d5b7282","signature"=>"","wb_uid"=>"a1","pcount"=>0}
+
+    #登录设置个人信息
+    login(luser.id)
+    post '/user_info/set', {:name => "测试name", :gender => 1, :birthday => '1988-08-08', :signature => '测试签名档', :job => '程序员', :jobtype => 1, :hobby => '计算机', :invisible => 0}
+    assert_response :success
+    assert_equal JSON.parse(response.body), {"_id"=> '502e6303421aa918ba000005',"gender"=>1,"hobby"=>"计算机","invisible"=>0,"jobtype"=>1,"name"=>"测试name","oid"=>154.0,"password"=>"c84dad462d5b7282","signature"=>"测试签名档","wb_uid"=>"a1","pcount"=>0,"birthday"=>"1988-08-08","job"=>"程序员"}
+
+    #登录获取当前登录用户信息
+    logout
+    login(luser.id)
+    get "/user_info/get_self"
+    assert_response :success
+    assert_equal JSON.parse(response.body), {"birthday"=>"1988-08-08","gender"=>1.0,"hobby"=>"计算机","invisible"=>0.0,"job"=>"程序员","jobtype"=>1,"name"=>"测试name","oid"=>154.0,"password"=>"c84dad462d5b7282","pcount"=>0,"signature"=>"测试签名档","wb_uid"=>"a1","id"=>"502e6303421aa918ba000005","logo"=>"","logo_thumb"=>"","logo_thumb2"=>""}
+
+  end
+end
+
