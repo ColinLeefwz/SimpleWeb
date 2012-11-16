c0b7ba7483078b86087852a21eaedff91d36cb54
pulsen <huang123qwe@126.com>
2012-11-16 16:55:34 +0800

    ip签到统计排序，加users，查询，商家数大于20不统计

--- app/models/checkin_ip_stat.rb
+++ app/models/checkin_ip_stat.rb
@@ -3,8 +3,10 @@ class CheckinIpStat
   field :_id, type: String
   field :shops, type:Array
   field :stotal, type: Integer
+  field :utotal, type: Integer
   field :ctotal, type: Integer
   field :data, type: Array
+  field :users, type:Array
   
 end
 
