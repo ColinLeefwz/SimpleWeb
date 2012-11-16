d8c404247313f8f045e5084a98898af4979ea8dd
pulsen <huang123qwe@126.com>
2012-11-16 16:55:34 +0800

    ip签到统计排序，加users，查询，商家数大于20不统计

--- app/controllers/ip_checkin_stats_controller.rb
+++ app/controllers/ip_checkin_stats_controller.rb
@@ -6,14 +6,44 @@ class IpCheckinStatsController < ApplicationController
   layout "admin"
 
   def index
-    sort = {stotal: -1}
+    sort = {}
     hash = {}
+    unless params[:ip].blank?
+      ip = params[:ip].gsub('.','\.')
+      hash.merge!({_id: /^#{ip}/})
+    end
+
+    case params[:order].to_s
+    when ''
+      sort.merge!({ctotal: -1})
+    when '1'
+      sort.merge!({ctotal: 1})
+    when '2'
+      sort.merge!({_id: -1})
+    when '3'
+      sort.merge!({_id: 1})
+    when '4'
+      sort.merge!({stotal: -1})
+    when '5'
+      sort.merge!({stotal: 1})
+    when '6'
+      sort.merge!({utotal: -1})
+    when '7'
+      sort.merge!({utotal: 1})
+    end
+
+
     @checkin_ip_stats = paginate("CheckinIpStat", params[:page], hash, sort )
   end
 
   def show_shops
     @checkin_ip_stat = CheckinIpStat.find(params[:id])
     @shops =  paginate_arr(@checkin_ip_stat.shops, params[:page])
+  end
+
+  def show_users
+    @checkin_ip_stat = CheckinIpStat.find(params[:id])
+    @users =  paginate_arr(@checkin_ip_stat.users, params[:page])
   end
 
   def checkin_list
