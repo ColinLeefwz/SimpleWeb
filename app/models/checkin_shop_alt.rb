a183ec1aec345d09cf07d250214e985a56fd7929
pulsen <huang123qwe@126.com>
2012-11-23 09:51:15 +0800

    签到高度统计

--- app/models/checkin_shop_alt.rb
+++ app/models/checkin_shop_alt.rb
@@ -1,4 +1,4 @@
-class CheckinShopStat
+class CheckinShopAlt
   include Mongoid::Document
   field :_id, type: String
   field :datas, type: Hash #{_id => 商家id,max => [alt最大值,alt最大值时的altacc],min => [alt最小值,alt最小时的altacc],avgalt => alt平均值,avgaltacc => altacc平均值, fcalt => alt方差 ,fcaltacc => altacc方差}
