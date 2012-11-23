98b2b00b246862b1be5733a181123240e608b3b0
pulsen <huang123qwe@126.com>
2012-11-23 09:51:15 +0800

    签到高度统计

---
+++ app/controllers/shop_checkin_alts_controller.rb
@@ -0,0 +1,17 @@
+class ShopCheckinAltsController < ApplicationController
+  include Paginate
+  before_filter :admin_authorize
+  layout "admin"
+
+  def index
+    hash = {}
+    sort = {}
+    @shop_checkin_alts = paginate("CheckinShopAlt", params[:page], hash, sort)
+  end
+
+  def shops
+    @checkin_shop_alt = CheckinShopAlt.find(params[:id])
+    @hshops = paginate_arr(@checkin_shop_alt.datas.to_a, params[:page])
+  end
+end
+
