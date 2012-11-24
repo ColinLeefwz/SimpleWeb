fc4b5e1d4b31b8f5d67ec493138ef9677b378741
web2 <web2@dface.cn>
2012-11-24 14:14:12 +0800

    取消moped的node discovery

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,3 +1,26 @@
 
 #Mongoid.logger = Logger.new($stdout)
 Mongoid.logger = Rails.logger
+
+if ENV["RAILS_ENV"] != "production"
+
+module Moped
+
+  # The cluster represents a cluster of MongoDB server nodes, either a single
+  # node, a replica set, or a mongos server.
+  class Cluster
+
+    def nodes
+	puts "disable Node Discovery."
+      @nodes
+    end
+
+    def refresh(nodes_to_refresh = @nodes)
+      @nodes.each {|node| node.refresh}
+      @nodes
+    end
+  end
+end
+
+end
+
