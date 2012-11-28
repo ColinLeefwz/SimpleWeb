4006ba1ca513edaccbd56ecb2da157892407230e
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-11-28 20:12:15 +0800

    test

--- test/integration/mobile_api/photo2_test.rb
+++ test/integration/mobile_api/photo2_test.rb
@@ -53,18 +53,5 @@ class Photo2Test < ActionDispatch::IntegrationTest
         :uid => "502e6303421aa918ba000001") 
   end  
 
-  test "实际部署的生产系统上测试阿里云文件上传" do
-    if `ifconfig eth1`.to_s.length > 0
-      puts "本机器是生产系统"
-      raise "Not set ALIYUN_ACCESS_ID in env" unless ENV["ALIYUN_ACCESS_ID"]
-      conn=CarrierWave::Storage::Aliyun::Connection.new({
-        aliyun_access_id:ENV["ALIYUN_ACCESS_ID"],
-        aliyun_access_key:ENV["ALIYUN_ACCESS_KEY"],
-        aliyun_bucket:"dface"
-        })
-      tstr=Time.now.to_i.to_s
-      conn.put(tstr+`hostname`[0..-2] , "Test#{tstr}")
-    end
-  end  
   
 end
