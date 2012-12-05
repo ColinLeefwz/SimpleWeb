98205fb49f2a8f882f128f1b01909a21d8644015
22 <22@dface.cn>
2012-12-05 22:30:05 +0800

    m

--- db/mongo/baidu-mapabc-merge/duplicate_remove.js
+++ db/mongo/baidu-mapabc-merge/duplicate_remove.js
@@ -2,11 +2,12 @@ db.loadServerScripts();
 
 function dup(offset){
 	db.shops.find({_id:{$lt:20347004}}).sort({_id:1}).skip(offset).forEach(function(x){
-	  dups = db.shops.find({lo : { $near : x.lo , $maxDistance : 0.02 },name:x.name, _id:{$ne : x._id}});
+	  dups = db.shops.find({_id:{$gt:x._id}, lo : { $near : x.lo , $maxDistance : 0.02 },name:x.name, _id:{$ne : x._id}});
 	  if(dups.length()>0){
 		  x.sames = dups.toArray();
 		  db.tmp.insert(x);
 	  }
+	  if(x._id%10000==0) print(x._id);
 	});
 }
 
