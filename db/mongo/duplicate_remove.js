cf3e0cc8e49946ab11cd1a7a2beacbc767e8544a
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-12-05 17:04:31 +0800

    m

--- db/mongo/duplicate_remove.js
+++ db/mongo/duplicate_remove.js
@@ -1,6 +1,6 @@
 print(db.shops.count());
 
-db.shops.find().forEach(function(x){
+db.shops.find({_id:{$lt:20347004}}).sort({_id:1}).forEach(function(x){
   db.shops.remove({loc : { $near : x.loc , $maxDistance : 0.001 },name:x.name, _id:{$ne : x._id}});
 });
 
