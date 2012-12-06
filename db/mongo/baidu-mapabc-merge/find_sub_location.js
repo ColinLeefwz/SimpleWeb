3864b4d219b29f31b5bfbb30234d0c975bb22ec9
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-12-06 10:42:02 +0800

    find sub shop

--- db/mongo/baidu-mapabc-merge/find_sub_location.js
+++ db/mongo/baidu-mapabc-merge/find_sub_location.js
@@ -3,42 +3,43 @@
 { "_id" : ObjectId("500fdd85421aa945f4a6b76b"), "loc" : [ 30.278031, 120.111866 ], "name" : "嘉绿苑西区(东二门)", "type" : "商务住宅;住宅区;住宅小区" }
 */
 
+db.loadServerScripts();
+
 var num=0;
 
-var e = new RegExp("\\([^\\)]*\\)$|（[^（]*）$");
+var e = new RegExp("\\([^\\)]*\\)$");
 
 var old_prefix="";
-var old_x=null;
+var prefix_shop=null;
 
-function diff(x,y){
-	if(y==null) return 100;
-	dx = (x.lo[0]-y.lo[0])*1;
-	dy = (x.lo[1]-y.lo[1])*1;
-	return Math.sqrt(Math.pow(dx,2)+Math.pow(dy,2));
-}
 
 db.shops.find({name:e,t:6}).sort({name:1}).forEach(function(x){
   num +=1;
   var prefix = x.name.substring(0,x.name.lastIndexOf("("));
   if(prefix.length==0){
 	old_prefix="";
-	old_x=null;
 	return;
   }
   if(prefix==old_prefix){
-	if(diff(x,old_x)<0.03){
-		db.tmp3.insert(old_x);
-		db.tmp3.insert(x);	
+	if(get_distance(x.lo,prefix_shop.lo)<3000){
+		prefix_shop.subs.push(x);
+		return;
 	}
+  }
+  if(prefix_shop!=null){
+	  db.tmp4.insert(prefix_shop);
+	  prefix_shop = null;
+  }
+  prefix_shop = db.shops.findOne({name:prefix,lo:{$within:{$center:[x.lo,0.02]}}});
+  if(prefix_shop!=null){
+	  prefix_shop.subs = [x];
   }else{
-	  var prefix_shop = db.shops.findOne({name:prefix});
-	  if(prefix_shop){
-  		db.tmp3.insert(prefix_shop);
-  		db.tmp3.insert(x);	
-	  }
+	  prefix_shop = x;
+	  prefix_shop.name_old = x.name;
+	  prefix_shop.name = prefix;
+	  prefix_shop.subs = [];
   }
   old_prefix = prefix;
-  old_x = x;
   if(num%1000==0) print(":"+num);
 })
 
