36308db83372e8fe5f7861f5d563537ea540bcef
pulsen <huang123qwe@126.com>
2012-12-07 16:58:03 +0800

    微薄接口

--- app/models/sina_user.rb
+++ app/models/sina_user.rb
@@ -1,3 +1,12 @@
 class SinaUser
-    include Mongoid::Document
+  include Mongoid::Document
+  field :_id, type: Integer
+
+  def self.find_by_id(id)
+    begin
+      self.find(id)
+    rescue
+      nil
+    end
+  end
 end
