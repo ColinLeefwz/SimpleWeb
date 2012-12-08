cb765e1fff6ff4a42071d432a7ea5ca423cd3580
web1 <web1@dface.cn>
2012-12-08 17:33:47 +0800

    gem变更或者resque任务变更时，要彻底停止进程然后启动

--- restart_all.sh
+++ restart_all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 #see http://unicorn.bogomips.org/SIGNALS.html
-kill -HUP `cat log/unicorn.pid`
-god restart resque
+kill -KILL `cat log/unicorn.pid`
+god stop resque
+pgp resque | awk '{print $2}' | xargs kill
+
+unicorn -D -E production -c unicorn.conf.rb
+god start resque
 
