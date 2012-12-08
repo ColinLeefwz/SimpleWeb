c28d54075275d3707ab6cad999a41fd9ce5d6c2f
web1 <web1@dface.cn>
2012-12-08 09:24:33 +0800

    mongo database change in cornjob

--- cron-job.sh
+++ cron-job.sh
@@ -2,12 +2,12 @@
 stime=`date +"%Y-%m-%d %H:%M:%S"`
 
 cd /mnt/lianlian
-/mnt/mongodb/bin/mongo 127.0.0.1/dface /mnt/lianlian/db/mongo/checkin_day.js
-/mnt/mongodb/bin/mongo 127.0.0.1/dface /mnt/lianlian/db/mongo/checkin_shop_stat.js
-/mnt/mongodb/bin/mongo 127.0.0.1/dface /mnt/lianlian/db/mongo/checkin_ip_stat.js
-/mnt/mongodb/bin/mongo 127.0.0.1/dface /mnt/lianlian/db/mongo/user_day.js
-/mnt/mongodb/bin/mongo 127.0.0.1/dface /mnt/lianlian/db/mongo/checkin_user_stat.js
-/mnt/mongodb/bin/mongo 127.0.0.1/dface /mnt/lianlian/db/mongo/checkin_loc_acc.js
+/mnt/mongodb/bin/mongo 10.135.44.107/dface /mnt/lianlian/db/mongo/checkin_day.js
+/mnt/mongodb/bin/mongo 10.135.44.107/dface /mnt/lianlian/db/mongo/checkin_shop_stat.js
+/mnt/mongodb/bin/mongo 10.135.44.107/dface /mnt/lianlian/db/mongo/checkin_ip_stat.js
+/mnt/mongodb/bin/mongo 10.135.44.107/dface /mnt/lianlian/db/mongo/user_day.js
+/mnt/mongodb/bin/mongo 10.135.44.107/dface /mnt/lianlian/db/mongo/checkin_user_stat.js
+/mnt/mongodb/bin/mongo 10.135.44.107/dface /mnt/lianlian/db/mongo/checkin_loc_acc.js
 /mnt/.rvm/bin/ruby script/rails r  'Checkin.clear_yesterday_redis'
 /mnt/.rvm/bin/ruby script/rails r  'CheckinShopStat.init_user_count'
 /mnt/.rvm/bin/ruby script/rails r db/mongo/ip_info.rb
