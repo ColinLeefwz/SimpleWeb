ff7d823f4797d5076a83c638c36b55aec1dc3f41
pulsen <huang123qwe@126.com>
2012-12-14 15:12:47 +0800

    poi 抓ios用户

--- app/models/sina_user.rb
+++ app/models/sina_user.rb
@@ -1,8 +1,7 @@
 class SinaUser
   include Mongoid::Document
+  #  field :_id, type: String
   store_in session: "dooo"
-
-  field :_id, type: Integer
 
   def self.find_by_id(id)
     begin
@@ -11,4 +10,45 @@ class SinaUser
       nil
     end
   end
+
+  def convert_to_user
+    return if User.where({wb_uid:self._id}).to_a.first
+    user = User.new({wb_uid:self.id, name:self.screen_name, gender:dface_gender, wb_v:self.verified, wb_vs:self.verified_reason, auto:true  })
+    user_logo = UserLogo.new({user_id: user._id})
+    if logo_store_local("public/uploads/tmp/#{user_logo.id}.jpg")
+      user_logo.img_tmp = "#{user_logo.id.to_s}.jpg"
+      user.head_logo_id = user_logo.id
+      CarrierWave::Workers::StoreAsset.perform("UserLogo",user_logo.id.to_s,"img") if user.save && user_logo.save
+    end
+  end
+
+  def dface_gender
+    return "1" if self.gender == 'm'
+    return '2' if self.gender == 'f'
+    '0'
+  end
+
+  def logo_store_local(path, err_num=0)
+    begin
+      sleep(1)
+      logo_data= RestClient.get(self.avatar_large)
+      open(path, "wb") { |file| file.write(logo_data)}
+      true
+    rescue
+      err_num += 1
+      return nil if err_num == 4
+      sleep(err_num * 2)
+      logo_store_local(path, err_num)
+    end
+  end
+
+  def self.poi_suser_convert(poi)
+    sina_users = SinaPoi.find(poi).datas.select{|su| su[2].match(/iphone|ipad/i)}
+    sina_users.each do |s|
+      su = SinaUser.find_by_id(s[0])
+      su.convert_to_user if su
+    end
+  end
+
+
 end
