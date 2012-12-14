a419eae66ffdba8ef0aa70d4075fa7796dfaa14c
pulsen <huang123qwe@126.com>
2012-12-14 17:28:03 +0800

    user,user_logo

--- app/models/sina_user.rb
+++ app/models/sina_user.rb
@@ -14,12 +14,14 @@ class SinaUser
   def convert_to_user
     return if User.where({wb_uid:self._id}).to_a.first
     user = User.new({wb_uid:self.id, name:self.screen_name, gender:dface_gender, wb_v:self.verified, wb_vs:self.verified_reason, auto:true, pcount:1  })
+    user.save
     user_logo = UserLogo.new({user_id: user._id})
-    if logo_store_local("public/uploads/tmp/#{user_logo.id}.jpg")
+    user_logo.save
+    if logo_store_local("public/uploads/tmp/#{user_logo.reload.id.to_s}.jpg")
       user_logo.img_tmp = "#{user_logo.id.to_s}.jpg"
       user.head_logo_id = user_logo.id
-      user.collection.insert(user.attributes)
-      UserLogo.collection.insert(user_logo.attributes)
+      user.save
+      user_logo.save
       CarrierWave::Workers::StoreAsset.perform("UserLogo",user_logo.id.to_s,"img")
     end
   end
