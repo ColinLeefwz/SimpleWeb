964a456d3ea5ff5e5031a9e4ad078ddc8facedf8
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-12-16 13:55:46 +0800

    multiple database test

--- test/integration/production_test.rb
+++ test/integration/production_test.rb
@@ -20,10 +20,45 @@ class ProductionTest < ActionDispatch::IntegrationTest
     assert_equal out.length, 0 
   end
   
+  def mongoid_tst
+    (1..10).each do |x|
+      user = User.new
+      user.id = x
+      user.wb_uid = x
+      user.save!     
+    end
+    (1..10).each do |x|
+      user = User.find(x)
+      assert_equal x, user.id
+    end
+    (1..10).each do |x|
+      user = User.find(x)
+      user.delete
+    end
+    (1..10).each do |x|
+      user = SinaUser.new
+      user.id = x
+      user.save!     
+    end
+    (1..10).each do |x|
+      user = SinaUser.find(x)
+      assert_equal x, user.id
+    end
+    (1..10).each do |x|
+      user = SinaUser.find(x)
+      user.delete
+    end
+    Mongoid.default_session.command(eval:"db.shops.count()")["retval"]
+    Mongoid.session(:dooo).command(eval:"db.shops.count()")["retval"]  
+  end
+  
   test "实际部署的生产系统上测试" do
     if `ifconfig eth1`.to_s.index("inet addr:42")
       puts "本机器是生产系统"
       aliyun_tst
+      mongoid_tst
+    else
+      mongoid_tst
     end
   end  
   
