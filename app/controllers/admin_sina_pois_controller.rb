1dc5d1da9c5a8cc0a5c453e6f5ae13f938732407
pulsen <huang123qwe@126.com>
2012-12-19 14:44:31 +0800

    微博附近商家

--- app/controllers/admin_sina_pois_controller.rb
+++ app/controllers/admin_sina_pois_controller.rb
@@ -10,6 +10,12 @@ class AdminSinaPoisController < ApplicationController
       hash.merge!({:city=> params[:city], :title => /#{params[:title]}/})
     end
     hash.merge!({:_id => params[:id] }) unless params[:id].blank?
+    case params[:correspond].to_i
+    when 1
+      hash.merge!({:baidu_id => {"$exists" => true} })
+    when 2
+      hash.merge!({:baidu_id => {"$exists" => false} })
+    end
 
     @sina_pois = paginate("SinaPoi", params[:page], hash, sort, 20  )
   end
@@ -21,4 +27,27 @@ class AdminSinaPoisController < ApplicationController
     render :json => {:text => str}
   end
 
+
+  def near
+    @sina_poi = SinaPoi.find(params[:id])
+    @shops = Baidu.where({:lo => {"$within" => {"$center" => [[@sina_poi.lat.to_f, @sina_poi.lon.to_f], 0.01]}}}).select{|m| name_similar(m.name, @sina_poi.title)}
+  end
+
+  private
+  def name_similar(bn, wbn)
+    bna = bn.split(/[()]/)
+    wbna = wbn.split(/[()]/)
+    bn1 = bna.join('').scan(/[\u4e00-\u9fa5]|[^\u4e00-\u9fa5]+/)
+    wbn1 = wbna.join('').scan(/[\u4e00-\u9fa5]|[^\u4e00-\u9fa5]+/)
+    if (bn1&wbn1).length >= (bn1.length < wbn1.length ? bn1.length : wbn1.length )/2.0
+      return true
+    end
+    bn1 = bna.first.scan(/[\u4e00-\u9fa5]|[^\u4e00-\u9fa5]+/)
+    wbn1 = wbna.first.scan(/[\u4e00-\u9fa5]|[^\u4e00-\u9fa5]+/)
+    if (bn1&wbn1).length >= (bn1.length < wbn1.length ? bn1.length : wbn1.length )/2.0
+      return true
+    end
+
+    return false
+  end
 end
