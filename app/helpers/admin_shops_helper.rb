eb9a308d755317ab9f66060364c7982240b1de38
pulsen <huang123qwe@126.com>
2012-12-20 13:57:13 +0800

    商家数据查询

--- app/helpers/admin_shops_helper.rb
+++ app/helpers/admin_shops_helper.rb
@@ -1,9 +1,10 @@
+# encoding: utf-8
 module AdminShopsHelper
   def import_type
     types = []
     File.open('db/mongo/baidu-mapabc-merge/type.txt', 'r') do |f|
-      f.each_line{|l| types << l.scan(/[\u4e00-\u9fa5;]+/)}
+      f.each_line{|l| types << l.scan(/[\u4e00-\u9fa5;\/a-zA-Z]+/)}
     end
-    (types-[[]]).map{|m| m.push(m[0])}.unshift(['',''])
+    types = (types-[[]]).reject{|r| r[0] =~ /^(金融|医疗)/}.unshift(['',''])
   end
 end
