71a6d38ed6105001f3208a070fe93c2e2df0b93e
pulsen <huang123qwe@126.com>
2012-12-21 15:12:16 +0800

    商家查询两个分类都需要

--- app/helpers/admin_shops_helper.rb
+++ app/helpers/admin_shops_helper.rb
@@ -1,6 +1,14 @@
 # encoding: utf-8
 module AdminShopsHelper
   def import_type
-    [["活动",0],["酒吧",1],["咖啡",2],["茶馆",3],["餐饮",4],["酒店",5],["休闲娱乐",6],["运动",7],["购物",8],["广场",9],["写字楼",10],["住宅",11],["学校",12],["交通",13]].unshift(['',''])
+    types = []
+    File.open('db/mongo/baidu-mapabc-merge/type.txt', 'r') do |f|
+      f.each_line{|l| types << l.scan(/[\u4e00-\u9fa5;\/a-zA-Z]+/)}
+    end
+    types = (types-[[]]).reject{|r| r[0] =~ /^(金融|医疗)/}.unshift(['',''])
+  end
+
+  def import_t
+    [["活动",0],["酒吧",1],["咖啡",2],["茶馆",3],["餐饮",4],["酒店",5],["休闲娱乐",6],["运动",7],["购物",8],["广场",9],["写字楼",10],["住宅",11],["学校",12],["交通",13]]
   end
 end
