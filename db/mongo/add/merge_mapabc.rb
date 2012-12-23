8d2c401b688f6cf028be98de295c06456df264fe
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-12-23 19:34:48 +0800

    fix

--- db/mongo/add/merge_mapabc.rb
+++ db/mongo/add/merge_mapabc.rb
@@ -20,12 +20,12 @@ end
 
 def has_similar(x)
 	  Baidu.collection.find({lo:{"$near" => x["lo"], "$maxDistance" => 0.001 }, type:/^餐饮/}).each do |bd|
-	  	return bd if similar(x.name,bd.name)
+	  	return bd if similar(x["name"],bd["name"])
 	  end
 	  return nil
 end
 
-Mapabc.collection.find({type:/^餐饮/, bid:{"$exists" => false}}).each do |x|
+Mapabc.collection.find({type:/^餐饮/, bid:{"$exists" => false}}).sort({_id:1}).each do |x|
 	begin
 	  name = x["name"]
 	  type = x["type"]
