fc5f23ff1962f7f46c44b1dc47b0e6533d305d55
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2012-12-27 21:23:59 +0800

    m

--- app/models/sina_user.rb
+++ app/models/sina_user.rb
@@ -12,7 +12,8 @@ class SinaUser
   end
 
   def convert_to_user
-    return if User.where({wb_uid:self._id}).to_a.first
+    oldu = User.where({wb_uid:self._id}).first
+    return if oldu
     user = User.new({wb_uid:self.id, name:self.screen_name, gender:dface_gender, wb_v:self.verified, wb_vs:self.verified_reason, auto:true  })
     user_logo = UserLogo.new({user_id: user._id})
     if logo_store_local("public/uploads/tmp/#{user_logo.id}.jpg")
