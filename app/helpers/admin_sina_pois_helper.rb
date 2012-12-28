1112caf09c9cc9899280fbf4e8a6fdd04482cd85
pulsen <huang123qwe@126.com>
2012-12-28 17:17:32 +0800

    微博商家出错

--- app/helpers/admin_sina_pois_helper.rb
+++ app/helpers/admin_sina_pois_helper.rb
@@ -3,10 +3,10 @@ module AdminSinaPoisHelper
   def link_correspond_shop(poi)
     if poi.respond_to?(:shop_id)
       shop = Shop.find(poi.shop_id)
-      link_to "S:#{shop.name}", "/admin_shops/show?id=#{poi.shop_id}"
+      link_to "S:#{shop.name.try(:name)}", "/admin_shops/show?id=#{poi.shop_id}"
     elsif poi.respond_to?(:baidu_id)
       baidu = Baidu.find(poi.baidu_id)
-      link_to "B:#{baidu.name}", "/admin_baidu?id=#{poi.baidu_id}"
+      link_to "B:#{baidu.name.try(:name)}", "/admin_baidu?id=#{poi.baidu_id}"
     end
   end
 
