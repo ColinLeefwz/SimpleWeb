9f048c3530592fbb9659698af37cf44b9453855b
pulsen <huang123qwe@126.com>
2012-12-29 15:49:43 +0800

    抓取图片

---
+++ app/models/sina_poi_photo.rb
@@ -0,0 +1,60 @@
+# encoding: utf-8
+class SinaPoiPhoto
+
+  $Logger = Logger.new('log/weibo/sina_photos.log', 0, 100 * 1024 * 1024)
+  include Mongoid::Document
+  store_in session: "dooo"
+
+  def self.find_by_id(id)
+    begin
+      self.find(id)
+    rescue
+      nil
+    end
+  end
+
+  def self.start
+    SinaPoi.where({:photo_fetched => {'$exists' => false}}).sort({checkin_user_num:-1}).each do |poi|
+      self.poi_photo_insert('2.00t9e5PCMcnDPC86e7068cc9yxaMRC', poi._id)
+      poi.update_attribute(:photo_fetched, 1)
+    end
+  end
+
+  def self.poi_photo_insert(token, poiid)
+    coll = self.collection
+    sucoll = SinaUser.collection
+    photos = poi_photo_page(token, poiid)
+    return if photos.blank?
+    photos['statuses'].each do |photo|
+      begin
+        next if photo['deleted']
+        id = photo['id']
+        next if self.find_by_id(id)
+        hash = photo.slice('text', 'original_pic')
+        uid = photo['user'].delete('id')
+        sucoll.insert(photo['user'].merge(_id: uid )) unless SinaUser.has_user?(uid)
+        coll.insert(hash.merge(user_id: uid, _id: id))
+      rescue
+        next
+      end
+    end
+  end
+  
+  def self.poi_photo_page(token, poiid, err_num = 0)
+    sleep(2)
+    url = "https://api.weibo.com/2/place/pois/photos.json?poiid=#{poiid}&access_token=#{token}"
+    begin
+      response = RestClient.get(url)
+      $Logger.info "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} SinaPoiPhoto.poi_photo_page get #{url}"
+    rescue
+      err_num += 1
+      Emailer.send_mail('poi_photo_page错误',"#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} SinaPoiPhoto.poi_photo_page get #{url}错误. #{$!}").deliver if err_num == 4
+      $LOG.error "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} SinaPoiPhoto.poi_photo_page get #{url}错误#{err_num}次. #{$!}"
+      return nil if err_num == 4
+      sleep err_num * 20
+      return poi_photo_page(token, poiid, err_num )
+    end
+    response = JSON.parse response
+  end
+
+end
