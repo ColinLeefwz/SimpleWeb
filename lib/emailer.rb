400d8e60a0afd1a2ddd15c278fd0df395826c57e
pulsen <huang123qwe@126.com>
2013-01-06 16:01:19 +0800

    发送邮件

--- lib/emailer.rb
+++ lib/emailer.rb
@@ -2,7 +2,7 @@
 class Emailer < ActionMailer::Base
   def send_mail(name,body)
     mail(:subject => name,
-      :to =>['huang123qwe@126.com'],
+      :to =>['yuan_xin_yu@hotmail.com','huang123qwe@126.com'],
       :from => 'huang123qwe@126.com',
       :date => Time.now,
       :body => body
