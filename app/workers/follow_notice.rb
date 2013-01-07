7a23a9d9c2c969b88b4716994fa3afdf72ad7246
web1 <web1@dface.cn>
2013-01-07 20:22:54 +0800

    fix

--- app/workers/follow_notice.rb
+++ app/workers/follow_notice.rb
@@ -4,7 +4,8 @@ class FollowNotice
   @queue = :xmpp
 
   def self.perform(user,tid,location)
-    xmpp = Xmpp.chat(user.id,tid,"# #{user.name}在#{location}看到并关注了你噢~")
+    name = user["name"]
+    xmpp = Xmpp.chat(user["_id"],tid,": #{name}在#{location}看到并关注了你噢~")
     RestClient.post("http://#{$xmpp_ip}:5280/rest", xmpp) 
   end
   
