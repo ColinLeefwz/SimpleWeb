0352397be55f8b8af79ba6f9cd77c33ad31ed326
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-07 16:23:48 +0800

    加关注的提醒消息

--- app/workers/weibo_photo.rb
+++ app/workers/weibo_photo.rb
@@ -1,9 +1,11 @@
+# encoding: utf-8
+
 class WeiboPhoto
   @queue = :normal
 
   def self.perform(token, text, url)
     RestClient.post('https://api.weibo.com/2/statuses/upload_url_text.json', 
-        :access_token  => token , :status => text, 
+        :access_token  => token , :status => text+" \n(来自脸脸 http://www.dface.cn)", 
         :url => url)  {|response, request, result| puts response }
   end
 end
