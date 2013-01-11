4ed1166094f06cca1fd51d562793e8d0fb41ed8c
pulsen <huang123qwe@126.com>
2013-01-11 17:25:27 +0800

    查询

--- app/controllers/admin_shops_controller.rb
+++ app/controllers/admin_shops_controller.rb
@@ -18,23 +18,16 @@ class AdminShopsController < ApplicationController
     case params[:v]
     when '1'
       hash.merge!({v:{'$exists' => true}})
-    when '2'
-      hash.merge!({v:{'$exists' => false}})
     end
 
     case params[:d]
     when '1'
       hash.merge!({d:{'$exists' => true}})
-    when '2'
-      hash.merge!({d:{'$exists' => false}})
     end
 
     case params[:del]
-    when '0'
     when '1'
       hash.merge!({del:{'$exists' => true}})
-    else
-      hash.merge!({del:{'$exists' => false}})
     end
 
     @page =  params[:page].blank? ? 1 : params[:page].to_i
