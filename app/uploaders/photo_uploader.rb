4a3183be8675f0ad3842e9fbe6834bf1a1a7f961
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-12 16:26:53 +0800

    图片分辨率从960恢复到640

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,7 +4,7 @@ class PhotoUploader < CommonPhotoUploader
 
   def aliyun_bucket; "dface"+bucket_suffix ; end
   
-  #process :resize_to_limit => [960, 960]
+  process :resize_to_limit => [640, 640]
 
   version :t2 do
     process :resize_to_fit => [200, 200]
