d2629b0e8b7d0630676f7a44fea8151a4b7d58a0
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-12 15:44:42 +0800

    取消图片大图的比例控制

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -3,7 +3,8 @@
 class PhotoUploader < CommonPhotoUploader
 
   def aliyun_bucket; "dface"+bucket_suffix ; end
-  process :resize_to_limit => [960, 960]
+  
+  #process :resize_to_limit => [960, 960]
 
   version :t2 do
     process :resize_to_fit => [200, 200]
