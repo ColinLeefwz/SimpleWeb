7840b665fae3a818d1e9b5ccb481fc073ef9dcb6
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-15 14:38:44 +0800

    m

--- lib/similarity.rb
+++ lib/similarity.rb
@@ -4,11 +4,9 @@ module Similarity
   include Gps
   
   def trim(str)
-  	str.sub!(/\./,"")
-  	str.sub!(/\(/,"")
-  	str.sub!(/\)/,"")
-  	str.sub!(/（/,"")
-  	str.sub!(/）/,"")	
+    str.sub!(/\([^)]+\)/, "")
+    str.sub!(/（[^）]+）/, "")
+  	str.sub!(/\./,"")    
   	str
   end
   
@@ -39,10 +37,15 @@ module Similarity
     ret
   end
   
+  def trim_town_prefix(str)
+    return str[3..-1] if str.length>5 && (str[2]=="镇" || str[2]=="区")
+    return str
+  end
+  
   def trim_citys_province(str,citys,province)
     ret = trim_province_prefix(str,province)
     citys.each {|x| ret = trim_city_prefix(ret,x) }
-    ret
+    trim_town_prefix(ret)
   end
   
   def trim_type(str,t)
@@ -51,25 +54,50 @@ module Similarity
     when 2 then str.sub(/(咖啡|咖啡厅|咖啡屋|咖啡吧)$/i,"")
     when 3 then str.sub(/(茶馆|茶楼|茶艺|茶吧|茶园|茶庄)$/i,"")
     when 4 then str.sub(/(餐厅|店)$/,"")            
-    when 5 then str.sub(/(大酒店|酒店|旅馆|宾馆|客房)$/,"")
+    when 5 then str.sub(/(大酒店|酒店|旅馆|宾馆|客房|旅店)$/,"")
     else str
     end
   end
-
-  def str_similar(str1,str2,citys,province,shop1,shop2)
-    return 0.3 if(str1=="" || str2=="") 
-  	s1 = trim(trim_citys_province(str1.downcase,citys,province))
-  	s2 = trim(trim_citys_province(str2.downcase,citys,province))
-    s1 = trim_type(s1,shop1["t"])
-    s2 = trim_type(s2,shop2["t"])
-    #puts "#{s1} - #{s2}"
+  
+  def trim_bracket(str)
+    str.sub(/\([^)]+\)/, "")
+  end
+  
+  def str_similar(s1,s2)
   	a1 = s1.split(//).to_set
   	a2 = s2.split(//).to_set
     a3 = a1 & a2
     m1 = (a3.size*1.0/a1.size)
     m2 = (a3.size*1.0/a2.size)
-    return (m1+m2)/2
+    (m1+m2)/2
   end
+
+  def name_similar(str1,str2,citys,province,shop1,shop2)
+  	s1 = trim(trim_citys_province(str1.downcase,citys,province))
+  	s2 = trim(trim_citys_province(str2.downcase,citys,province))
+    s1 = trim_type(s1,shop1["t"])
+    s2 = trim_type(s2,shop2["t"])
+    #puts "#{s1} - #{s2}"   if $0=="script/rails"
+    fac1 = str_similar(s1,s2)
+    fac2 = str_similar(s1[0,2],s2[0,2])
+    fac3 = (fac2==0) ? 0.1 : 0
+    return 0.8*fac1 + 0.2*fac2 - fac3
+  end
+  
+  def addr_similar(str1,str2,citys,province,shop1,shop2)
+    return 0.3 if(str1=="" || str2=="") 
+  	s1 = trim(trim_citys_province(str1.downcase,citys,province))
+  	s2 = trim(trim_citys_province(str2.downcase,citys,province))
+    x1 = s1.split(" ")
+    x2 = s2.split(" ")
+    s1=x1[x1.length-1]
+    s2=x2[x2.length-1]
+    #puts "#{s1} - #{s2}"
+    ret = str_similar(s1,s2)
+    factor = s1[-2..-1]==s2[-2..-1]? 0.05 : -0.25
+    return ret+factor
+  end
+  
   
   def distance(shop1,shop2)
     if(shop2["lo"][0].class==Array)
@@ -101,12 +129,12 @@ module Similarity
       province = x.s
       citys << x.name
     end
-    name_score = 70*str_similar(shop1["name"],shop2["name"],citys,province,shop1,shop2)
-    addr_score = 12*str_similar(shop1["addr"],shop2["addr"],citys,province,shop1,shop2)
-    type_score = 8*(shop1["t"]==shop2["t"]? 1:0)
-    #puts "distance: #{distance(shop1,shop2)}"
+    name_score = 65*name_similar(shop1["name"],shop2["name"],citys,province,shop1,shop2) + 5*str_similar(shop1["name"],shop2["name"])
+    addr_score = 12*addr_similar(shop1["addr"],shop2["addr"],citys,province,shop1,shop2)
+    type_score = 4*(shop1["t"]==shop2["t"]? 1:0) + 4*(shop1["type"]==shop2["type"]? 1:0)
+    puts "distance: #{distance(shop1,shop2)}"  if $0=="script/rails"
     dist_score = dist_score(distance(shop1,shop2))
-    #puts [name_score,addr_score,type_score,dist_score]
+    puts [name_score,addr_score,type_score,dist_score] if $0=="script/rails"
     return name_score+addr_score+type_score+dist_score
   end
 
