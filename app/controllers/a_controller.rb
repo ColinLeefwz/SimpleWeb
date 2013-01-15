ee8ccc66daa7864f1c0718b7d5df8ae262fee6b4
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-15 14:38:44 +0800

    m

--- app/controllers/a_controller.rb
+++ app/controllers/a_controller.rb
@@ -3,7 +3,7 @@ class AController < ApplicationController
   def index
     c = Channel.new
     c.ip = real_ip
-    c.v = params[:v]
+    c.v = params[:v] #1微博来自脸脸，2首次发微博分享, 3照片链接， 4二维码
     c.time = Time.now
     c.agent = request.env["HTTP_USER_AGENT"]
     c.save
