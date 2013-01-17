852c89f0658f71f1e083363a8a373ce03128f6c4
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-17 20:54:24 +0800

    m

--- app/workers/weibo_first.rb
+++ app/workers/weibo_first.rb
@@ -5,7 +5,7 @@ class WeiboFirst
 
   def self.perform(token)
     RestClient.post('https://api.weibo.com/2/statuses/upload_url_text.json', 
-        :access_token  => token , :status => "刚巧遇上了，没有什么特别的话想说，惟有轻轻地一句：“ 我也在这里！” #脸脸#有点意思的现场社交应用，你也来吧！iPhone版： http://www.dface.cn/a?v=2", 
+        :access_token  => token , :status => "#脸脸#有点意思的现场社交应用。刚巧遇上了，没有什么特别的话想说，惟有轻轻地一句：“ 我也在这里！” ，你也来吧！iPhone版： http://www.dface.cn/a?v=2", 
         :url => "http://www.dface.cn/twocode/images/lianlian.jpg")  {|response, request, result| puts response }
   end
 end
