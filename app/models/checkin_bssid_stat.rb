2a84cb8ff23d321326872f00514f225d20edce47
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-19 21:49:03 +0800

    bssid统计

---
+++ app/models/checkin_bssid_stat.rb
@@ -0,0 +1,34 @@
+class CheckinBssidStat
+  include Mongoid::Document
+  field :_id, type: String
+  field :shops, type:Array
+  
+  def self.init_bssid
+    Checkin.where({bssid:{"$exists" => true}}).each do |x|
+      CheckinBssidStat.insert_checkin(x)
+    end
+  end
+  
+  def self.insert_checkin(ck)
+    CheckinBssidStat.insert(ck["bssid"],ck["sid"],ck["uid"])
+  end
+  
+  def self.insert(bssid,sid,uid)
+    b = CheckinBssidStat.where({"_id" => bssid}).first
+    if b.nil?
+      CheckinBssidStat.collection.insert({"_id" => bssid, "shops" => [{id:sid,users:[uid]}] })
+    else
+      shops = b.shops
+      shop = shops.find{|x| x.id==sid}
+      if shop.nil?
+        b.push(:shops, {id:sid,users:[uid]} )
+      elsif shop.users.size<5
+        shop.users << uid
+        b.shops = shops
+        b.save!
+      end
+    end
+  end
+  
+end
+
