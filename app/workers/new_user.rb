686064a161056318f12d0ab138c08665ed5c0688
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-19 13:00:27 +0800

    city显示

--- app/workers/new_user.rb
+++ app/workers/new_user.rb
@@ -6,7 +6,7 @@ class NewUser
   def self.perform(uid,sid)
     user = User.find(uid)
     shop = Shop.find(sid)
-    xmpp = Xmpp.chat(uid,"502e6303421aa918ba000001","新用户来了:#{user.show_gender} #{shop.city} #{shop.name}")
+    xmpp = Xmpp.chat(uid,"502e6303421aa918ba000001","新用户来了:#{user.show_gender} #{shop.city_fullname} #{shop.name}")
     RestClient.post("http://#{$xmpp_ip}:5280/rest", xmpp) 
   end
 end
