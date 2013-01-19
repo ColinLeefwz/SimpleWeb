626a396ccd41a6f0f86347e9e3fba8fa1015e470
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-19 22:06:51 +0800

    fix

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -35,10 +35,16 @@ class ShopController < ApplicationController
     page = 1 if page==0
     pcount = 20 if pcount==0
     users = shop.users(session[:user_id],(page-1)*pcount,pcount)
+    if users.nil?
+      render :json => [].to_json
+      return
+    end
     fm = users.group_by {|item| item["gender"]==2 ? "f" : "m" }
     fmf = fm["f"]
     fmf = [] if fmf.nil?
-    render :json => fmf.concat(fm["m"]).to_json
+    fmm = fm["m"]
+    fmm = [] if fmm.nil?
+    render :json => fmf.concat(fmm).to_json
   end
   
   def info
