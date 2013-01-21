76a1c6ad95886698c62765ba9b05d830e4b8b326
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-21 09:28:35 +0800

    notify 4 user

--- app/workers/new_user.rb
+++ app/workers/new_user.rb
@@ -4,9 +4,16 @@ class NewUser
   @queue = :normal
 
   def self.perform(uid,sid)
+    ["502e6303421aa918ba000001","50446058421aa92042000002","50bc20fcc90d8ba33600004b","502e6303421aa918ba000079"].each do |to|
+      NewUser.notify(uid,sid, to)
+    end
+  end
+  
+  def self.notify(uid,sid, to)
     user = User.find(uid)
     shop = Shop.find(sid)
-    xmpp = Xmpp.chat(uid,"502e6303421aa918ba000001","新用户来了:#{user.show_gender} #{shop.city_fullname} #{shop.name}")
+    xmpp = Xmpp.chat(uid,to,"新用户来了:#{user.show_gender} #{shop.city_fullname} #{shop.name}")
     RestClient.post("http://#{$xmpp_ip}:5280/rest", xmpp) 
   end
+  
 end
