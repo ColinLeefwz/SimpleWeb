b401df0fa37df074e4bcdfc138e144c3cdeebee8
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-22 15:08:40 +0800

    m

--- app/workers/new_user.rb
+++ app/workers/new_user.rb
@@ -7,10 +7,16 @@ class NewUser
     ["502e6303421aa918ba000001","50446058421aa92042000002","50bc20fcc90d8ba33600004b","502e6303421aa918ba000079"].each do |to|
       NewUser.notify(uid,sid, to)
     end
+    ["50fe2e9bc90d8b6c3a0001fd","50fe294cc90d8b143a000136"].each do |to|
+      NewUser.notify(uid,sid, to, 1)
+    end
   end
   
-  def self.notify(uid,sid, to)
+  def self.notify(uid,sid, to, gender=0)
     user = User.find(uid)
+    if gender!=0
+      return if user.gender!=gender
+    end
     shop = Shop.find(sid)
     xmpp = Xmpp.chat(uid,to,"新用户来了:#{user.show_gender} #{shop.city_fullname} #{shop.name}")
     RestClient.post("http://#{$xmpp_ip}:5280/rest", xmpp) 
