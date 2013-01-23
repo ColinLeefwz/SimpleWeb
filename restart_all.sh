f849bf9e3b6bfaf602c3d639ecacf15cf8d96a5f
web2 <web2@dface.cn>
2013-01-23 11:28:32 +0800

    m

--- restart_all.sh
+++ restart_all.sh
@@ -4,7 +4,8 @@
 kill -KILL `cat log/unicorn.pid`
 god stop resque
 pgp resque | awk '{print $2}' | xargs kill
-
+sleep 2
+echo `pgp resque`
 unicorn -D -E production -c unicorn.conf.rb
 god start resque
 
