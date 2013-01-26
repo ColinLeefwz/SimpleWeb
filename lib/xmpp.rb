ac668242968fd7fa7b0da0b86589155cdcc5feab
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-26 13:41:55 +0800

    bugfix: 商家名称中带&导致rest接口投递xmpp消息失败

--- lib/xmpp.rb
+++ lib/xmpp.rb
@@ -1,11 +1,13 @@
 class Xmpp
 
   def self.chat(from,to,msg)
-    "<message id='#{$uuid.generate}' to='#{to}@dface.cn' from='#{from}@dface.cn' type='chat'><body>#{msg}</body></message>"
+    msg2 = CGI.escapeHTML(msg)
+    "<message id='#{$uuid.generate}' to='#{to}@dface.cn' from='#{from}@dface.cn' type='chat'><body>#{msg2}</body></message>"
   end
   
   def self.gchat(from,to,msg)
-    "<message id='#{$uuid.generate}' to='#{to}@dface.cn' from='#{from}@c.dface.cn' type='groupchat'><body>#{msg}</body></message>"
+    msg2 = CGI.escapeHTML(msg)
+    "<message id='#{$uuid.generate}' to='#{to}@dface.cn' from='#{from}@c.dface.cn' type='groupchat'><body>#{msg2}</body></message>"
   end 
 
 
