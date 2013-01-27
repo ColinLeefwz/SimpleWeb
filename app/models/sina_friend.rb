aba391e6a7f3d1a49058bee98ece2fa51ad4b1b4
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-27 09:36:05 +0800

    sina friend

--- app/models/sina_friend.rb
+++ app/models/sina_friend.rb
@@ -1,29 +1,35 @@
 # encoding: utf-8
 class SinaFriend
   include Mongoid::Document
+  field :_id, type: String
+  field :data, type: Hash #{ids, total_number}
+
 
   def insert_ids(wb_uid, token)
     coll = self.collection
-    data =  all_page(wb_uid,token)
-    coll.insert(:_id => wb_uid.to_s, :data =>data)
+    hash =  all_page(wb_uid,token)
+    return nil if hash.nil? || hash[:total_number]==0
+    coll.insert(:_id => wb_uid.to_s, :data =>hash)
     data
   end
   
   def init_all(token)
     User.where({auto:{"$ne" => true}}).each do |user|
-      next if user.wb_uid.nil? || SinaFriend.find_by_id(user.wb_uid)
+      next if user.wb_uid.nil? || !user.wb_uid.match(/^[\d]+$/)
+      next if SinaFriend.find_by_id(user.wb_uid)
       insert_ids(user.wb_uid,token)
+      sleep(1.5)
     end
   end
 
   private
   
   def single_page(wb_uid,token,cursor=0,err_num = 0)
-    #sleep(1.5)
     url = "https://api.weibo.com/2/friendships/friends/ids.json?count=#{5000}&cursor=#{cursor}&&access_token=#{token}&uid=#{wb_uid}"
     begin
       response = RestClient.get(url)
-    rescue
+    rescue Exception => e
+      puts e
       return nil
       err_num += 1
       return nil if err_num == 4
@@ -36,7 +42,7 @@ class SinaFriend
 
   def all_page(wb_uid, token)
     data = single_page(wb_uid,token)
-    return [] if data.nil?
+    return nil if data.nil?
     total_number = data["total_number"]
     ids = []
     0.upto((total_number-1)/5000) do |page|
