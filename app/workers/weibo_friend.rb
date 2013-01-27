5bcf3625227365ad23b1ef56af99bec554a2e15c
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-27 12:51:54 +0800

    m

--- app/workers/weibo_friend.rb
+++ app/workers/weibo_friend.rb
@@ -6,13 +6,19 @@ class WeiboFriend
   def self.perform(token, wb_uid, uid)
     data = SinaFriend.new.insert_ids(wb_uid,token)
     user = User.find(uid)
+    friend_notice_all(user)
+  end
+  
+  def self.friend_notice_all(user)
     user.sina_friends_not_lianlian_friends.each do |x|
       WeiboFriend.friend_notice(user,x)
     end
   end
   
   def self.friend_notice(user,x)
-    xmpp = Xmpp.chat(x.id,user.id,": 您的微博好友#{x.wb_name}也在使用脸脸，也在脸脸中加TA为好友吧。")
+    name = x.wb_name
+    name = x.name if name.nil?
+    xmpp = Xmpp.chat(x.id,user.id,": 您的微博好友#{name}也在使用脸脸，在脸脸中也加TA为好友吧。")
     RestClient.post("http://#{$xmpp_ip}:5280/rest", xmpp) 
   end
 end
