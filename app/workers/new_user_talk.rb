d05301d3e62340c1eaf60ede231bc49254e1634d
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-01-29 23:13:43 +0800

    m

---
+++ app/workers/new_user_talk.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+class NewUserTalk
+  @queue = :normal
+
+  def self.perform(uid,sid,seq)
+    user = User.find(uid)
+    if user.gender==2
+      to = "50bc20fcc90d8ba33600004b" #浦靠谱
+    else
+      to = "50bec2c1c90d8bd12f000086" #amanda林
+    end
+    if seq==1
+      xmpp1 = Xmpp.chat(to,uid,"hi，在干吗呢")
+      RestClient.post("http://#{$xmpp_ip}:5280/rest", xmpp1)
+    end
+    if seq==2
+      shop = Shop.find(sid)
+      xmpp2 = Xmpp.chat(to,usid,"你在#{shop.name}？")
+      RestClient.post("http://#{$xmpp_ip}:5280/rest", xmpp2)
+    end
+  end
+  
+end
