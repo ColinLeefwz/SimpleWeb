a0553c257ff632e18de039f3716ec450ee4e58ee
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-02-05 14:36:39 +0800

    m

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -10,6 +10,9 @@ class CheckinsController < ApplicationController
   end
 
   def new_shop
+    if params[:sname].length<4
+      render :json => {error: "地点名称不能少于四个字"}.to_json
+    end
     shop = Shop.new
     shop._id = Shop.next_id
     shop.name = params[:sname]
