5ddaec29c9b12324ba01aaeef3770abdd7ece8e8
web1 <web1@dface.cn>
2013-02-05 16:24:12 +0800

    m

--- restart_all.sh
+++ restart_all.sh
@@ -2,10 +2,5 @@
 
 #see http://unicorn.bogomips.org/SIGNALS.html
 kill -KILL `cat log/unicorn.pid`
-god stop resque
-pgp resque | awk '{print $2}' | xargs kill
-sleep 2
-echo `pgp resque`
 unicorn -D -E production -c unicorn.conf.rb
-god start resque
-
+./restart_resque.sh
