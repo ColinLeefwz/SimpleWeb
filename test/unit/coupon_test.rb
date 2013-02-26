4ba0a66bae6d0eda7dd9604ab50fddc259ea451d
pulsen <huang123qwe@126.com>
2013-02-26 17:51:29 +0800

    优惠券发送规则调整

--- test/unit/coupon_test.rb
+++ test/unit/coupon_test.rb
@@ -7,7 +7,10 @@ class CouponTest < ActiveSupport::TestCase
   # end
   def setup
     reload('coupons.js')
+    reload('checkins.js')
   end
+
+
 
   test ".use(user_id) 优惠券第一次使用。" do
     coupon = Coupon.find('507fc5bfc9ad42d756a412e2')
@@ -26,84 +29,34 @@ class CouponTest < ActiveSupport::TestCase
     coupon.use(user.id)
     assert_equal coupon.users.select{|s| s['id'] == user.id}.map { |m| m.keys }, [['id', 'dat', 'uat'],['id', 'dat', 'uat']]
   end
-
-
-
-  test ".allow_send? 优惠券规则只能下载一次,第一次下载" do
-    coupon = Coupon.find('507fc5bfc9ad42d756a412e1')
+  
+  test "#allow_send_checkin? 规则是新用户首次签到优惠，新用户签到第一次可以发优惠券， 第二次不可以发。 " do
     user = User.find('502e6303421aa918ba000005')
-    assert coupon.rule.to_i == 0
-    assert coupon.allow_send?(user.id)
+    coupon = Coupon.find('507fc5bfc9ad42d756a412e3')
+    Checkin.create(uid: user._id, sid: coupon.shop_id)
+    assert_equal coupon.allow_send_checkin?(user._id), true
+    Checkin.create(uid: user._id, sid: coupon.shop_id)
+    assert_equal coupon.allow_send_checkin?(user._id), nil
   end
 
-  test ".allow_send? 优惠券规则只能下载一次,第二次下载" do
-    coupon = Coupon.find('507fc5bfc9ad42d756a412e1')
-    user = User.find('502e6303421aa918ba000005')
-    assert coupon.rule.to_i == 0
-    coupon.download(user.id)
-    assert !coupon.allow_send?(user.id)
+  test "#allow_send_checkin? 规则是新用户首次签到优惠， 老用户签到 " do
+    user = User.find('502e6303421aa918ba00007c')
+    coupon = Coupon.find('507fc5bfc9ad42d756a412e3')
+    Checkin.create(uid: user._id, sid: coupon.shop_id)
+    assert_equal coupon.allow_send_checkin?(user._id), nil
   end
 
-  test ".allow_send? 优惠券规则只能有一个未使用,第一次下载" do
-    coupon = Coupon.find('507fc5bfc9ad42d756a412e2')
+  test "#allow_send_checkin? 规则是累计签到优惠， 第三次签到才可以发优惠券。超过部分不发" do
     user = User.find('502e6303421aa918ba000005')
-    assert coupon.rule.to_i == 1
-    assert coupon.allow_send?(user.id)
-  end
-
-  test ".allow_send? 优惠券规则只能有一个未使用,未使用下载" do
-    coupon = Coupon.find('507fc5bfc9ad42d756a412e2')
-    user = User.find('502e6303421aa918ba000005')
-    assert coupon.rule.to_i == 1
-    coupon.download(user.id)
-    assert !coupon.allow_send?(user.id)
+    coupon = Coupon.find('507fc5bfc9ad42d756a412e4')
+    Checkin.create(uid: user._id, sid: coupon.shop_id)
+    assert_equal coupon.allow_send_checkin?(user._id), nil
+    Checkin.create(uid: user._id, sid: coupon.shop_id)
+    assert_equal coupon.allow_send_checkin?(user._id), nil
+    Checkin.create(uid: user._id, sid: coupon.shop_id)
+    assert_equal coupon.allow_send_checkin?(user._id), true
+    Checkin.create(uid: user._id, sid: coupon.shop_id)
+    assert_equal coupon.allow_send_checkin?(user._id), nil
   end
   
-  test ".allow_send? 优惠券规则只能有一个未使用,使用后下载。" do
-    coupon = Coupon.find('507fc5bfc9ad42d756a412e2')
-    user = User.find('502e6303421aa918ba000005')
-    assert coupon.rule.to_i == 1
-    coupon.download(user.id)
-    coupon.use(user.id)
-    assert coupon.allow_send?(user.id)
-  end
-
-  test ".allow_send? 优惠券规则无限制下载, 第一下载。" do
-    coupon = Coupon.find('507fc5bfc9ad42d756a412e3')
-    user = User.find('502e6303421aa918ba000005')
-    assert coupon.rule.to_i == 2
-    assert coupon.allow_send?(user.id)
-  end
-  
-  test ".allow_send? 优惠券规则无限制下载, 未使用下载。" do
-    coupon = Coupon.find('507fc5bfc9ad42d756a412e3')
-    user = User.find('502e6303421aa918ba000005')
-    assert coupon.rule.to_i == 2
-    coupon.download(user.id)
-    assert coupon.allow_send?(user.id)
-  end
-
-  test ".allow_send? 优惠券规则无限制下载, 使用后下载。" do
-    coupon = Coupon.find('507fc5bfc9ad42d756a412e3')
-    user = User.find('502e6303421aa918ba000005')
-    assert coupon.rule.to_i == 2
-    coupon.download(user.id)
-    coupon.use(user.id)
-    assert coupon.allow_send?(user.id)
-  end
-
-  test ".allow_send? 优惠券规则无限制下载, 多次下载使用后下载。" do
-    coupon = Coupon.find('507fc5bfc9ad42d756a412e3')
-    user = User.find('502e6303421aa918ba000005')
-    assert coupon.rule.to_i == 2
-    coupon.download(user.id)
-    coupon.use(user.id)
-    coupon.download(user.id)
-    coupon.use(user.id)
-    coupon.download(user.id)
-    coupon.download(user.id)
-    assert coupon.allow_send?(user.id)
-  end
-
-
 end
