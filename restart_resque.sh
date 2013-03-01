1a92ad915c6dd128a8e5f2bf7665ef6c5fb1807f
web1 <web1@dface.cn>
2013-03-01 21:38:29 +0800

    m

--- restart_resque.sh
+++ restart_resque.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-god stop resque
 while pgrep -f resque-1
 do 
  echo 'killing resque...'
  sleep 1
- pkill -signal QUIT -f resque-1
+ pkill -QUIT -f resque-1
 done
-god start resque
+god restart resque
 
