a1192dc5012d30fc2dd2d3f6dd2dbcd83e0194f9
web2 <web2@dface.cn>
2013-03-05 15:14:52 +0800

    conf

--- unicorn.conf.rb
+++ unicorn.conf.rb
@@ -6,7 +6,7 @@
 # See also http://unicorn.bogomips.org/examples/unicorn.conf.rb for
 # a more verbose configuration using more features.
 
-listen 8082 # by default Unicorn listens on port 8080
+listen "127.0.0.1:8082" # by default Unicorn listens on port 8080
 worker_processes 3 # this should be >= nr_cpus
 pid "/mnt/lianlian/log/unicorn.pid"
 stderr_path "/mnt/lianlian/log/unicorn.log"
