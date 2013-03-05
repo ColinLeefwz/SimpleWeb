abeae01e42545140d119a28084197257646649cb
pulsen <huang123qwe@126.com>
2013-03-05 17:39:15 +0800

    find_primary和其测试

--- test/unit/checkin_test.rb
+++ test/unit/checkin_test.rb
@@ -1,7 +1,17 @@
+# coding: utf-8
 require 'test_helper'
 
 class CheckinTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    reload('checkins.js')
+  end
+
+  test "find_primary 取id" do
+    checkin = Checkin.find_primary('50598b371d41c8c11a000002')
+    assert_equal checkin.id.to_s, '50598b371d41c8c11a000002'
+  end
 end
