5a1aeee3f9922d8378d0786f879229f040b80240
pulsen <huang123qwe@126.com>
2013-03-08 14:57:41 +0800

    地点添加，报错

--- app/controllers/admin_user_add_shops_controller.rb
+++ app/controllers/admin_user_add_shops_controller.rb
@@ -1,17 +1,30 @@
+# encoding: utf-8
 class AdminUserAddShopsController < ApplicationController
   include Paginate
   before_filter :admin_authorize
   layout "admin"
 
   def index
-    hash = {creator: {"$exists" => true}, t: nil}
+    hash = {creator: {"$exists" => true}}
+     
+    case params[:t].to_s
+    when ''
+      hash.merge!({t: nil})
+    when '1'
+      hash.merge!({t: {"$exists" => true}})
+    end
+
+    case params[:del].to_s
+    when ''
+      hash.merge!({del: {"$exists" => false}})
+    when '1'
+      hash.merge!({del: 1})
+    end
+    hash.merge!({})
     hash.merge!( {name: /#{params[:name]}/ }  )  unless params[:name].blank?
     hash.merge!({city: params[:city]}) unless params[:city].blank?
-
     @page =  params[:page].blank? ? 1 : params[:page].to_i
-    @shops = Shop.where(hash).skip((@page-1)*20).limit(20).sort({_id: -1})
-
-
+    @shops = Shop.where(hash).skip((@page-1)*10).limit(10).sort({_id: -1})
   end
 
   def show
@@ -20,7 +33,7 @@ class AdminUserAddShopsController < ApplicationController
 
   def baidu_map
     @shop = Shop.find(params[:id])
-    @shop.lob = @shop.lo_to_lob
+    #    @shop.lob = @shop.lo_to_lob
   end
 
   def edit
@@ -29,6 +42,10 @@ class AdminUserAddShopsController < ApplicationController
 
   def update
     @shop = Shop.find(params[:id])
+    @shop.lob = @shop.lob.split(/[,，]/).map { |m| m.to_f  }.reverse
+    if @shop.lob.count == 2
+      params[:shop][:lo] = @shop.lob_to_lo
+    end
     if @shop.update_attributes(params[:shop])
       redirect_to :action => :show, :id => @shop.id
     else
@@ -39,7 +56,7 @@ class AdminUserAddShopsController < ApplicationController
 
   def del
     @shop = Shop.find(params[:id])
-    Del.insert(@shop)
+    shop.shop_del
     redirect_to :action => :index
   end
 
