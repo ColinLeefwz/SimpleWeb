ceca9679052d6fd148d1aafa1330988fa81406ac
pulsen <huang123qwe@126.com>
2013-03-18 15:07:11 +0800

    测试出错

--- test/fixtures/shop_faqs.js
+++ test/fixtures/shop_faqs.js
@@ -4,31 +4,31 @@ db.shop_faqs.insert({
     sid: 1,
     title: "问题1",
     text: "答案1",
-    od: '1'
+    od: '01'
 })
 
 db.shop_faqs.insert({
     sid: 1,
     title: "问题2",
     text: "答案2",
-    od: '2'
+    od: '02'
 })
 db.shop_faqs.insert({
     sid: 1,
     title: "问题3",
     text: "答案3",
-    od: '3'
+    od: '03'
 })
 db.shop_faqs.insert({
     sid: 1,
     title: "问题4",
     text: "答案4",
-    od: '4'
+    od: '04'
 })
 db.shop_faqs.insert({
     sid: 1,
     title: "问题5",
     text: "答案5",
-    od: '5'
+    od: '05'
 })
 
