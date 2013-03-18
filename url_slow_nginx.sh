c02f043b3af7a30b6755cd618a8a6afa6dc9526d
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-03-18 13:43:29 +0800

    登录接口调整

--- url_slow_nginx.sh
+++ url_slow_nginx.sh
@@ -1,2 +1,1 @@
-tail -n 10000  /home/dooo/nginx-1.2.3/build/access.log  | ruby -ne 'arr=$_.split(); print arr[-2]," ",arr.to_s,"\n"' | sort -nr | head 
-
+tail -n 10000 /var/log/nginx/www.access.log | ruby -ne 'arr=$_.split(); print arr[-1]," ",arr[1..-2].to_s,"\n"' | sort -nr | head -n 100
