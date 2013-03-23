19df402cbd249ab913adeb27a5f25734c1aa358a
web2 <web2@dface.cn>
2013-03-23 16:51:13 +0800

    mongodb to riak: baidu-offset

---
+++ db/riak/migrate_offsetbaidu.rb
@@ -0,0 +1,12 @@
+client = Ripple.client
+bucket = client.bucket('boffsets')
+Mongoid.session(:dooo)[:offsetbaidus].where({v:{"$exists" => false}}).each do |x|
+  k = "%.2f%.2f" %  x["loc"]
+  v = x["d"].map{|d| d.round(5)}
+  obj = bucket.get_or_new(k)
+  if obj.data.nil?
+    obj.data=v
+    obj.store
+  end
+  Mongoid.session(:dooo)[:offsetbaidus].find({_id:x["_id"]}).update({"$set" => {v:true}})
+end
