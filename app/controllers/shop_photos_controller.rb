0cc3d2ac98db3c368c485971065dab9d1b15245d
pulsen <huang123qwe@126.com>
2013-03-25 16:01:26 +0800

    商家后台照片墙

---
+++ app/controllers/shop_photos_controller.rb
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+class ShopPhotosController < ApplicationController
+  before_filter :shop_authorize
+  include Paginate
+  layout 'shop'
+
+  def index
+    hash = {:room => session[:shop_id]}
+    sort = {:updated_at =>  -1}
+    @photos = paginate("Photo", params[:page], hash, sort,10)
+  end
+
+  def ajax_del
+    photo = Photo.find(params[:id])
+    photo.delete
+    render :json => {:text => '删除成功'}
+  end
+
+end
