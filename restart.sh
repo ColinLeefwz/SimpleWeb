3f131733c2e1876ebcd77e459b5ca7c956d91bc8
web2 <web2@dface.cn>
2013-03-26 15:21:47 +0800

    unicorn 重启监控

--- restart.sh
+++ restart.sh
@@ -2,4 +2,21 @@
 
 #see http://unicorn.bogomips.org/SIGNALS.html
 kill -HUP `cat log/unicorn.pid`
+sleep 2
+
+while true
+do 
+  if tail -n 3 log/unicorn.log | grep "worker=0 ready"
+  	then
+  	echo "restart successful."
+  	break
+  fi
+  
+  if tail -n 3 log/unicorn.log | grep "ERROR"
+  	then
+  	echo "restart has ERROR."
+  	break
+  fi
+  echo "."
+done
 
