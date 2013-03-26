d802e4bc4340e126709a76cff1c15a202cd4e7ee
pulsen <huang123qwe@126.com>
2013-03-26 11:20:31 +0800

    微博绑定

---
+++ app/models/bind_wb.rb
@@ -0,0 +1,35 @@
+# coding: utf-8
+class BindWb
+  include Mongoid::Document
+  field :_id, type: Integer #商家id
+  field :name #昵称
+  field :login_name #登录名
+  field :wb_uid, type: Integer #微博uid
+  field :val, type:Boolean, default:false #是否验证过
+
+  validates_presence_of :wb_uid, :message => "微博uid必填."
+  validates_presence_of :login_name, :message => "微博登录名必填."
+  validates_presence_of :name, :message => "微博昵称必填."
+
+  validate :rr
+  def rr
+    url = "https://api.weibo.com/2/users/show.json?access_token=#{$sina_token}&uid=#{self.wb_uid}"
+    begin
+      response = JSON.parse(RestClient.get(url))
+      self.errors.add(:wu_uid, '微博uid和微博昵称不匹配.')  if response["screen_name"] != self.name
+    rescue RestClient::BadRequest
+      self.errors.add(:wu_uid, '微博uid可能不存在.')
+    rescue
+    end
+  end
+
+
+  def self.find2(id)
+    begin
+      self.find(id)
+    rescue
+      nil
+    end
+  end
+
+end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -25,6 +25,7 @@ class Shop
   field :utotal, type:Integer, default:0 #截至到昨天，该商家的用户总数
   field :uftotal, type:Integer, default:0 #截至到昨天，该商家的女性用户总数
   field :shops, type:Array #子商家
+  field :psid, type:Integer #总店id
   #field :osm_id #Open Street Map node id
   field :d, type:Integer #降权
   field :v, type:Integer #加权
