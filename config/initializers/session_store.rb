becaa67dc3603384592e67a78842c2121c09ff07
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-03-27 20:40:55 +0800

    去掉无用的middleware

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -7,3 +7,15 @@
 # (create the session table with "rails generate session_migration")
 
 Lianlian::Application.config.session_store :redis_store
+
+    
+Lianlian::Application.config.middleware.delete 'Rack::Cache'   # 整页缓存，用不上
+Lianlian::Application.config.middleware.delete 'Rack::Lock'    # 多线程加锁，多进程模式下无意义
+Lianlian::Application.config.middleware.delete 'Rack::Runtime' #记录X-Runtime
+Lianlian::Application.config.middleware.delete 'ActionDispatch::RequestId' # 记录X-Request-Id（方便查看请求在群集中的哪台执行）
+Lianlian::Application.config.middleware.delete 'ActionDispatch::RemoteIp'  # IP SpoofAttack
+Lianlian::Application.config.middleware.delete 'ActionDispatch::Callbacks' # 在请求前后设置callback
+Lianlian::Application.config.middleware.delete 'ActionDispatch::Head'      # 如果是HEAD请求，按照GET请求执行，但是不返回body
+Lianlian::Application.config.middleware.delete 'Rack::ConditionalGet'      # HTTP客户端缓存才会使用
+Lianlian::Application.config.middleware.delete 'Rack::ETag'    # HTTP客户端缓存才会使用
+Lianlian::Application.config.middleware.delete 'ActionDispatch::BestStandardsSupport' # 设置X-UA-Compatible, 在nginx上设置
