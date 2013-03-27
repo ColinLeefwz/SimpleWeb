f108426b1654b51299090886afeedc3ceb68ff7c
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-03-27 20:18:10 +0800

    m

--- test/unit/checkin_test.rb
+++ test/unit/checkin_test.rb
@@ -39,7 +39,15 @@ class CheckinTest < ActiveSupport::TestCase
     ck.save!
     assert_equal 999, Checkin.first.sid
     assert_equal Checkin.find_by_id(id), Checkin.first
-    assert_equal Checkin.find_by_id(id), Rails.cache.read(Checkin.first.my_cache_key)    
+    assert_equal Checkin.find_by_id(id), Rails.cache.read(Checkin.first.my_cache_key) 
+    ck.sid=996
+    ck.save!
+    assert_equal Checkin.first.sid, 996
+    assert_equal Checkin.find_by_id(id).sid, 996     
+    ck = Checkin.find_by_id(id)
+    ck.sid=995
+    ck.save!
+    assert_equal Checkin.first.sid, 995 
   end
   
   test "cache update2" do  
@@ -48,9 +56,45 @@ class CheckinTest < ActiveSupport::TestCase
     ck.update_attribute(:sid, 998)
     assert_equal 998, Checkin.first.sid
     assert_equal Checkin.find_by_id(id), Checkin.first
+    assert_equal Checkin.find_by_id(id), Rails.cache.read(Checkin.first.my_cache_key)  
+    ck = Checkin.find(id)
+    ck.update_attributes({sid:996})
+    assert_equal Checkin.find_by_id(id).sid, 996  
+    ck = Checkin.find(id)
+    ck.update_attributes({sid:995})
+    assert_equal Checkin.find_by_id(id).sid, 995
+  end
+  
+  test "cache update3" do  
+    id = Checkin.first.id
+    ck = Checkin.find_by_id(id)
+    ck.update_attributes({sid:997})
+    assert_equal 997, Checkin.first.sid
+    assert_equal Checkin.find_by_id(id), Checkin.first
     assert_equal Checkin.find_by_id(id), Rails.cache.read(Checkin.first.my_cache_key)    
+    ck.update_attributes({sid:996})
+    assert_equal ck.sid, 996
+    #assert_equal Checkin.find(id).sid, 996 #TODO: 这里为什么失败
+    assert_equal Checkin.find_primary(id).sid, 996
+    #为什么执行find_primary后就正常了？
+    assert_equal Checkin.find(id).sid, 996 
   end
 
+  test "cache update4" do  
+    id = Checkin.first.id
+    ck = Checkin.find_by_id(id)
+    ck.update_attributes!({od:100, sid:996})
+    assert_equal 996, Checkin.first.sid
+    assert_equal 100, Checkin.first.od
+    assert_equal Checkin.find_by_id(id), Checkin.first
+    assert_equal Checkin.find_by_id(id), Rails.cache.read(Checkin.first.my_cache_key)    
+    ck = Checkin.find(id)
+    ck.update_attributes!({od:1, sid:1})
+    assert_equal Checkin.find_by_id(id).od, 1
+    assert_equal Checkin.find_by_id(id).sid, 1
+    
+  end
+  
   test "cache destroy" do  
     id = Checkin.first.id
     ck = Checkin.find_by_id(id)  
