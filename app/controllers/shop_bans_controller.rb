5d24366a43af5bc6698178245129e6d164c82eec
pulsen <huang123qwe@126.com>
2013-04-03 14:06:30 +0800

    商家管理后台用户屏蔽，解除屏蔽

---
+++ app/controllers/shop_bans_controller.rb
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+class ShopBansController < ApplicationController
+  before_filter :shop_authorize
+  layout 'shop'
+  include Paginate
+
+  def index
+    @shop_ban = ShopBan.find2(session[:shop_id].to_i)
+    @bans = paginate_arr(@shop_ban.users, params[:page] )
+    @users = @bans.map{|b| User.find_by_id(b)}
+  end
+
+
+  def unban
+    @shop_ban = ShopBan.find2(session[:shop_id].to_i)
+    @shop_ban.users.delete(params[:uid])
+    @shop_ban.save
+    url = {:action => "index"}
+    url.merge!(:page => params[:page]) unless params[:page].blank?
+    redirect_to  url
+  end
+
+
+end
