8d0fe7d84aeef7c1f69627ea581b1ca946096b1d
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-04-08 21:46:07 +0800

    绑定微博的商家，参加活动的优惠券自动@到商家微博

--- coupon/gen_demo.sh
+++ coupon/gen_demo.sh
@@ -5,6 +5,6 @@ then
    echo "参数错误:请按[名称 详情 最终生成的图片保存的路径 上传的小图片路径]顺序传参"
    exit 1
 fi
-convert -font 'msyh.ttf' -fill "#309797" -pointsize 24 -kerning 1.5 -draw "text 232,50\"${1}\"" -fill "gray" -pointsize 20 -kerning 1 -draw "text 232,90\"${2}\"" -draw "image Over 15,17 189,189 \"${4}\""   demo.png $3
+convert -quality 70 -font 'msyh.ttf' -fill "#309797" -pointsize 24 -kerning 1.5 -draw "text 232,50\"${1}\"" -fill "gray" -pointsize 20 -kerning 1 -draw "text 232,90\"${2}\"" -draw "image Over 15,17 189,189 \"${4}\""   demo.png $3
 #convert -resize 290x112 $3 "${3}_2.jpg"
 
