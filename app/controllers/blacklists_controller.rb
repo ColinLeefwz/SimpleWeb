c3831d2530c162a231344fefdf9342f7647fe88d
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-04-08 11:11:27 +0800

    黑名单保存到单独的表中

--- app/controllers/blacklists_controller.rb
+++ app/controllers/blacklists_controller.rb
@@ -3,23 +3,26 @@ class BlacklistsController < ApplicationController
   before_filter :user_is_session_user, :except => [:index]
 
   def index
-    users = User.find_by_id(params[:id]).blacks_s.map {|x| User.find_by_id(x["id"]) }
-    users.delete(nil)
+    users = UserBlack.where({uid:params[:id]}).map {|x| User.find_by_id(x["bid"]) }
     users.delete_if {|x| x.name.index(params[:name])==nil } unless params[:name].nil?
     output_users(users)
   end
 
   def create
     report = params[:report].to_i
-    hash = {id:Moped::BSON::ObjectId(params[:block_id]), report:report, cat:Time.now }
-    session_user.add_to_set(:blacks, hash) unless session_user.black?(params[:block_id])
+    ub = UserBlack.new
+    ub.uid = session[:user_id]
+    ub.bid = Moped::BSON::ObjectId(params[:block_id])
+    ub.report = report
+    ub.save!
     Resque.enqueue(XmppBlack, session[:user_id], params[:block_id], 'block')
     Resque.enqueue(XmppBlackNotice, session[:user_id], params[:block_id]) if report==1
     render:json => hash.to_json
   end
 
   def delete
-    session_user.pull(:blacks,{id:Moped::BSON::ObjectId(params[:block_id])})
+    bid = Moped::BSON::ObjectId(params[:block_id])
+    UserBlack.delete_all({uid:session[:user_id], bid:bid})
     Resque.enqueue(XmppBlack, session[:user_id], params[:block_id], 'unblock')
     render :json => {:deleted => params[:block_id]}.to_json
   end
