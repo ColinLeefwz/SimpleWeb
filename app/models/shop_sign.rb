2f6b169edd4e6d0e5c67472d036c616099b515a9
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-04-09 10:40:14 +0800

    签约商家合同

---
+++ app/models/shop_sign.rb
@@ -0,0 +1,12 @@
+class ShopSign
+  include Mongoid::Document
+  field :htid #合同编号
+  field :name #商家名称
+  field :sid, type:Integer #对应的脸脸商家id，如果有多家店，对应到主店
+  field :attch #附件
+
+  validates_presence_of :htid, :name, :password
+  validates_uniqueness_of :htid, :sid
+
+
+end
