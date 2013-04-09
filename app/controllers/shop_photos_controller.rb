ed1691899e38ffc232390e995eadda53c5386a59
pulsen <huang123qwe@126.com>
2013-04-09 09:19:27 +0800

    照片墙详情，赞列表，评论列表，评论隐藏

--- app/controllers/shop_photos_controller.rb
+++ app/controllers/shop_photos_controller.rb
@@ -6,15 +6,35 @@ class ShopPhotosController < ApplicationController
   layout 'shop'
 
   def index
-    hash = {:room => session[:shop_id].to_i.to_s}
+    hash = {:room => session[:shop_id].to_i.to_s, :hide => nil}
     sort = {:updated_at =>  -1}
     @photos = paginate("Photo", params[:page], hash, sort,10)
   end
 
   def ajax_del
     photo = Photo.find(params[:id])
-    photo.delete
+    photo.update_attribute(:hide, true)
     render :json => {:text => '删除成功'}
+  end
+
+  def show
+    @photo = Photo.find_by_id(params[:id])
+  end
+
+  def show_like
+    @photo = Photo.find_by_id(params[:id])
+    @likes = paginate_arr(@photo.like.to_a, params[:page])
+  end
+
+  def show_com
+    @photo = Photo.find_by_id(params[:id])
+    @coms = paginate_arr(@photo.com.to_a.reject{|p| p["hide"]}, params[:page])
+  end
+
+  def hide_com
+    photo = Photo.find(params[:id])
+    result = photo.hidecom(params[:uid], params[:t])
+    render :json => {:text => result ? 0 : 1 }
   end
 
 end
--- app/helpers/shop_checkins_helper.rb
+++ app/helpers/shop_checkins_helper.rb
@@ -1,21 +1,6 @@
 # encoding: utf-8
 module ShopCheckinsHelper
-  def img_gender(gender)
-    case gender
-    when 1
-      image_tag("../images/sign6.jpg")
-    when 2
-      image_tag("../images/sign7.jpg")
-    end
-  end
 
-  def age(birthday)
-    return if birthday.blank?
-    l = Time.now.to_date
-    b = birthday
-    a = l.year-b.year
-    a-1 if "#{l.month}#{l.day}" < "#{b.month}#{b.day}"
-  end
 
   def ban_link(uid)
     if session_shop.ban_user(uid)
