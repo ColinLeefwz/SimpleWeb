e30dbca17f489b063323c3505e8aaec8c8761b1c
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-04-18 11:43:40 +0800

    m

--- app/models/shop_sign.rb
+++ app/models/shop_sign.rb
@@ -7,9 +7,7 @@ class ShopSign
   field :name #商家名称
   field :sid, type:Integer #对应的脸脸商家id，如果有多家店，对应到主店
   field :attch #附件
-  ALIYUN_ACCESS_ID="ACS4wSgMTzClBisY"
-  ALIYUN_ACCESS_KEY="5e4VzNCmHz"
-  
+
   mount_uploader(:attch, ShopSignUploader)
 
   validates_presence_of :htid, :name
@@ -21,8 +19,8 @@ class ShopSign
     #    au = "http://oss.aliyuncs.com/lianlian/516e50b520f318d4ea000001/0.doc"
     uri = URI.parse(au)
     ite = Time.now.to_i+seconds
-    signature = `cd oss && python OssSignature.py "#{ALIYUN_ACCESS_KEY}" "GET\n\n\n#{ite}\n#{uri.path}"`
-    "#{au}?OSSAccessKeyId=#{ALIYUN_ACCESS_ID}&Expires=#{ite}&Signature=#{signature.chomp}"
+    signature = `cd oss && python OssSignature.py "#{ENV['ALIYUN_ACCESS_KEY']}" "GET\n\n\n#{ite}\n#{uri.path}"`
+    "#{au}?OSSAccessKeyId=#{ENV['ALIYUN_ACCESS_ID']}&Expires=#{ite}&Signature=#{signature.chomp}"
   end
 
 end
