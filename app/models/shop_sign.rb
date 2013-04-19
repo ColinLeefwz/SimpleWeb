5f2d10dae0bf4df4db943943d96ae9f609dee3e1
pulsen <huang123qwe@126.com>
2013-04-19 11:46:56 +0800

    商家合同上传

--- app/models/shop_sign.rb
+++ app/models/shop_sign.rb
@@ -7,10 +7,11 @@ class ShopSign
   field :name #商家名称
   field :sid, type:Integer #对应的脸脸商家id，如果有多家店，对应到主店
   field :attch #附件
+  delegate :_id, :name, :to => :shop, :prefix => true, :allow_nil => true
 
   mount_uploader(:attch, ShopSignUploader)
 
-  validates_presence_of :htid, :name
+  validates_presence_of :htid, :name, :sid
   validates_uniqueness_of :htid, :sid
 
 
@@ -21,6 +22,10 @@ class ShopSign
     ite = Time.now.to_i+seconds
     signature = `cd oss && python OssSignature.py "#{ENV['ALIYUN_ACCESS_KEY']}" "GET\n\n\n#{ite}\n#{uri.path}"`
     "#{au}?OSSAccessKeyId=#{ENV['ALIYUN_ACCESS_ID']}&Expires=#{ite}&Signature=#{signature.chomp}"
+  end
+
+  def shop
+    Shop.find_by_id(sid)
   end
 
 end
