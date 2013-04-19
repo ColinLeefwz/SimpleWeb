f79a1ae71383c9280a8848d3f7248740bafe8876
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-04-19 17:17:52 +0800

    脸脸商城

--- app/controllers/aroundme_controller.rb
+++ app/controllers/aroundme_controller.rb
@@ -16,9 +16,10 @@ class AroundmeController < ApplicationController
     end
     if is_session_user_kx
       arr << Shop.find_by_id($llcf)
+      arr << Shop.find_by_id($llsc)
     end
     if in_zi_wei_yuan_dian?(lo)
-      arr << Shop.find_by_id(21830231)
+      arr << Shop.find_by_id(21830231) #延安路•紫微大街 
     end
     render :json =>  arr.map{|x| x.safe_output_with_users}.to_json
   end
