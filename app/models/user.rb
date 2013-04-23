df98b21a515c33583a1d1cbfd206d68556aee6c9
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-04-23 19:21:46 +0800

    m

--- app/models/user.rb
+++ app/models/user.rb
@@ -157,6 +157,7 @@ class User
     hash = self.attr_with_id
     hash.delete("password")
     hash.delete("follows")
+    hash.delete("wb_uid") if self.wb_hidden
     #TODO: 性能优化
     hash.merge!( head_logo_hash).merge!( relation_hash(user_id) )
     hash.merge!(last_location(user_id))
