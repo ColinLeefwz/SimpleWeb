a73518bc94b602610b3c9d26cdff7615e3480bb6
pulsen <huang123qwe@126.com>
2013-04-27 18:08:22 +0800

    优惠券下载，使用测试

---
+++ test/unit/coupon_down_test.rb
@@ -0,0 +1,38 @@
+# coding: utf-8
+require 'test_helper'
+
+class CouponDownTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    CouponDown.delete_all
+    reload('coupons.js')
+  end
+
+  coupon = Coupon.find('507fc5bfc9ad42d756a412e1')
+  user = User.find('502e6303421aa918ba00007c')
+  user2 = User.find('502e6303421aa918ba000002')
+
+  test ".download(coupon, user_id, photo_id=nil, sid = nil) 下载优惠券" do
+    assert_equal CouponDown.download(coupon, user.id).attributes.slice("cid", "sid","uid"), {"cid"=>coupon.id, "sid"=>1, "uid"=> user.id}
+  end
+
+  test ".download(coupon, user_id, photo_id=nil, sid = nil) 分店下载优惠券" do
+    assert_equal CouponDown.download(coupon, user.id,nil, 111).attributes.slice("cid", "sid","uid", 'sub_sid'), {"cid"=>coupon.id, "sid"=>1, "uid"=> user.id, 'sub_sid' => 111}
+  end
+
+  test "#use(user_id, data) 用户没下载使用优惠券报错" do
+    down = CouponDown.download(coupon, user.id)
+    assert_raise RuntimeError do
+      down.use(user2.id, nil)
+    end
+  end
+
+  test "#use(user_id, data) 用户使用优惠券" do
+    down = CouponDown.download(coupon, user.id)
+    assert_equal down.use(user.id, nil), true
+  end
+
+  
+end
