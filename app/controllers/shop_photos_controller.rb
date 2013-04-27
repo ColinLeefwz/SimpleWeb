8b1281fd675118a74a548223d7b78de6c30dd9cb
pulsen <huang123qwe@126.com>
2013-04-27 10:23:48 +0800

    修改照片墙清除缓存

--- app/controllers/shop_photos_controller.rb
+++ app/controllers/shop_photos_controller.rb
@@ -14,12 +14,14 @@ class ShopPhotosController < ApplicationController
   def ajax_del
     photo = Photo.find(params[:id])
     photo.update_attribute(:hide, true)
+    expire_cache_shop(photo.room)
     render :json => {:text => '已隐藏'}
   end
 
   def ajax_undel
     photo = Photo.find(params[:id])
     photo.unset(:hide)
+    expire_cache_shop(photo.room)
     render :json => {:text => "已取消"}
   end
 
@@ -40,13 +42,20 @@ class ShopPhotosController < ApplicationController
   def hide_com
     photo = Photo.find(params[:id])
     result = photo.hidecom(params[:uid], params[:t])
+    expire_cache_shop(photo.room)
     render :json => {:text => result ? 0 : 1 }
   end
 
   def unhide_com
     photo = Photo.find(params[:id])
     result = photo.unhidecom(params[:uid], params[:t])
+    expire_cache_shop(photo.room)
     render :json => {:text => result ? 0 : 1 }
+  end
+
+  private 
+  def expire_cache_shop(sid)
+    Rails.cache.delete("SP#{sid}-5")
   end
 
 end
