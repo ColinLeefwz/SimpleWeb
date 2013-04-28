f8337a2686af9dec116ab819fb39cd5dc7c327c5
pulsen <huang123qwe@126.com>
2013-04-28 17:29:56 +0800

    商家管理员后台优惠券

--- app/helpers/shop_coupons_helper.rb
+++ app/helpers/shop_coupons_helper.rb
@@ -16,4 +16,11 @@ module ShopCouponsHelper
     a = l.year-b.year
     a-1 if "#{l.month}#{l.day}" < "#{b.month}#{b.day}"
   end
+
+  def to_local(datetime, format = "%Y-%m-%d %H:%M")
+    return '' unless datetime.is_a?(Date)
+    datetime = datetime.utc? ? datetime.since(8.hours) : datetime
+    datetime.strftime(format)
+  end
+
 end
