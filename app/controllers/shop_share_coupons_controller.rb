0d966108e48c18537c61e97c659f194196177559
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-04-30 14:17:42 +0800

    m

--- app/controllers/shop_share_coupons_controller.rb
+++ app/controllers/shop_share_coupons_controller.rb
@@ -62,7 +62,7 @@ class ShopShareCouponsController < ApplicationController
         @coupon.save
         CarrierWave::Workers::StoreAsset.perform("Coupon",@coupon.id.to_s,"img")
       end
-      Rails.cache.delete("views/SI#{@coupon.shop_id}.json") if @coupon.hint
+      Rails.cache.delete("views/SI#{@coupon.shop_id}.json") if @coupon.text
       redirect_to :action => :show, :id => @coupon.id
     else
       flash.now[:notice] = '发布失败.'
@@ -85,7 +85,7 @@ class ShopShareCouponsController < ApplicationController
       @coupon.img_tmp = "coupon_#{@coupon.id}.jpg"
       @coupon.save
       CarrierWave::Workers::StoreAsset.perform("Coupon",@coupon.id.to_s,"img")
-      Rails.cache.delete("views/SI#{@coupon.shop_id}.json") if @coupon.hint
+      Rails.cache.delete("views/SI#{@coupon.shop_id}.json") if @coupon.text
       return redirect_to :action => :show, :id => @coupon.id
     end
    
@@ -94,7 +94,7 @@ class ShopShareCouponsController < ApplicationController
       if @coupon.t.to_i == 1
         @coupon.gen_img if !@coupon.img.blank? || !@coupon.img2.blank?
       end
-      Rails.cache.delete("views/SI#{@coupon.shop_id}.json") if @coupon.hint
+      Rails.cache.delete("views/SI#{@coupon.shop_id}.json") if @coupon.text
       redirect_to :action => :show, :id => @coupon.id
     else
       render :action => :edit
