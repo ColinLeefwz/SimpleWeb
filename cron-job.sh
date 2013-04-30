210505ce62e880e2ef5d3d2c82b0bc5311521ecc
pulsen <huang123qwe@126.com>
2013-04-30 16:41:31 +0800

    优惠券每日统计脚本

--- cron-job.sh
+++ cron-job.sh
@@ -10,6 +10,8 @@ mongo 10.135.44.107/dface /mnt/lianlian/db/mongo/checkin_user_stat.js
 mongo 10.135.44.107/dface /mnt/lianlian/db/mongo/checkin_loc_acc.js
 mongo 10.135.44.107/dface /mnt/lianlian/db/mongo/checkin_user_many.js
 mongo 10.135.44.107/dface /mnt/lianlian/db/mongo/user_city_day.js
+mongo 10.135.44.107/dface /mnt/lianlian/db/mongo/coupon_day_stat.js
+
 
 /mnt/.rvm/bin/ruby script/rails r  'Checkin.clear_yesterday_redis'
 /mnt/.rvm/bin/ruby script/rails r  'CheckinShopStat.init_user_count'
