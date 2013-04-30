e15461e7274cc4d26790c6ff66ec9f8e906d336f
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-04-30 10:58:01 +0800

    发布分享优惠券带hint时清空商家信息的缓存

--- app/controllers/shop_share_coupons_controller.rb
+++ app/controllers/shop_share_coupons_controller.rb
@@ -62,6 +62,7 @@ class ShopShareCouponsController < ApplicationController
         @coupon.save
         CarrierWave::Workers::StoreAsset.perform("Coupon",@coupon.id.to_s,"img")
       end
+      Rails.cache.delete("views/SI#{@coupon.sid}.json") if @coupon.hint
       redirect_to :action => :show, :id => @coupon.id
     else
       flash.now[:notice] = '发布失败.'
@@ -84,6 +85,7 @@ class ShopShareCouponsController < ApplicationController
       @coupon.img_tmp = "coupon_#{@coupon.id}.jpg"
       @coupon.save
       CarrierWave::Workers::StoreAsset.perform("Coupon",@coupon.id.to_s,"img")
+      Rails.cache.delete("views/SI#{@coupon.sid}.json") if @coupon.hint
       return redirect_to :action => :show, :id => @coupon.id
     end
    
@@ -92,7 +94,7 @@ class ShopShareCouponsController < ApplicationController
       if @coupon.t.to_i == 1
         @coupon.gen_img if !@coupon.img.blank? || !@coupon.img2.blank?
       end
-
+      Rails.cache.delete("views/SI#{@coupon.sid}.json") if @coupon.hint
       redirect_to :action => :show, :id => @coupon.id
     else
       render :action => :edit
