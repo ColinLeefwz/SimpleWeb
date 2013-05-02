106b9068a817fc3b4b3fcacc9b26c36ae0d20e8e
pulsen <huang123qwe@126.com>
2013-05-02 16:30:50 +0800

    管理员后台商家都添加链接到show

--- app/controllers/photo_day_stats_controller.rb
+++ app/controllers/photo_day_stats_controller.rb
@@ -6,7 +6,7 @@ class PhotoDayStatsController < ApplicationController
   # GET /coupons.json
   def index
     hash, sort = {}, {_id: -1}
-    @photo_day_stats =  paginate3("PhotoDayStat", params[:page], hash, sort)
+    @photo_day_stats =  paginate3("PhotoDayStat", params[:page], hash, sort, 10)
   end
  
 end
