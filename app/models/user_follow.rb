e596081cd717c48ba92fdb9278b71f834756513d
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-05-05 13:59:08 +0800

    用户的好友的redis集合

--- app/models/user_follow.rb
+++ app/models/user_follow.rb
@@ -21,6 +21,10 @@ class UserFollow
     uf.add_to_set(:follows, fid)
     uf.del_my_cache
     Rails.cache.delete("UI#{fid}#{uid}")
+    fuser = User.find_by_id(fid)
+    if fuser && fuser.friend?(uid)
+      add_good_friend_redis(uid,fid)
+    end
   end
   
   def self.del(uid,fid)
@@ -28,6 +32,32 @@ class UserFollow
     uf.pull(:follows,fid)
     uf.del_my_cache
     Rails.cache.delete("UI#{fid}#{uid}")
+    fuser = User.find_by_id(fid)
+    if fuser && fuser.friend?(uid)
+      del_good_friend_redis(uid,fid)
+    end
   end
   
+  
+  def self.init_good_friend_redis
+    UserFollow.all.each do |uf|
+      users = follows.map {|x| User.find_by_id(x) }
+      users.delete(nil)
+      users.delete_if {|x| !x.friend?(uf.id)}
+      users.each_with_index{|u,idx| $redis.zadd("Frd#{uf.id}",u.id,idx)}
+    end
+  end
+  
+  def self.add_good_friend_redis(uid1,uid2)
+    u1 = $redis.zcard("Frd#{uid1}") || 0
+    $redis.zadd("Frd#{uid1}",u1,uid2)
+    u2 = $redis.zcard("Frd#{uid2}") || 0
+    $redis.zadd("Frd#{uid2}",u2,uid1)    
+  end
+
+  def self.del_good_friend_redis(uid1,uid2)
+    $redis.zrem("Frd#{uid1}",uid2)
+    $redis.zrem("Frd#{uid2}",uid1)    
+  end
+    
 end
