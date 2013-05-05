f78e1c3120ebeb303165ef54489bdc5b930a5f60
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-05-05 22:24:06 +0800

    粉丝数据保存到redis

--- app/controllers/follow_info_controller.rb
+++ app/controllers/follow_info_controller.rb
@@ -4,6 +4,7 @@ class FollowInfoController < ApplicationController
   def followers
     who = User.find_by_id(params[:id])   
     users = who.followers
+    #TODO: redis端分页
     users.delete_if {|x| x.name.index(params[:name])==nil } unless params[:name].nil?
     users.delete_if {|x| who.black?(x._id) }
     output_users(users)
@@ -16,6 +17,7 @@ class FollowInfoController < ApplicationController
       return
     end
     users = who.follows.map {|x| User.find_by_id(x) }
+    #TODO: mongodb端分页
     users.delete(nil)
     users.delete_if {|x| x.name.index(params[:name])==nil } unless params[:name].nil?
     output_users(users.reverse)
@@ -28,6 +30,7 @@ class FollowInfoController < ApplicationController
       return
     end
     users = who.good_friends
+    #TODO: redis端分页
     users.delete_if {|x| x.name.index(params[:name])==nil } unless params[:name].nil?
     output_users(users.reverse)
   end
