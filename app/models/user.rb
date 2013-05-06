b44e9dc69b2641eec979fa6d13f74cb198783fb9
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-05-06 16:39:42 +0800

    m

--- app/models/user.rb
+++ app/models/user.rb
@@ -286,7 +286,8 @@ class User
         dis = dis / 1000.0
         dis = "距离%.1f公里" % dis
       else
-        dis = "距离#{dis.to_i}米"
+        dis = dis / 10
+        dis = "距离#{dis.to_i}0米内"
       end
       Resque.enqueue(XmppMsg, self.id, uid, ": 你的好友#{name}刚刚在#{shop.name}签到，#{dis}，快和TA打个招呼吧")
       Resque.enqueue(XmppMsg, uid, self.id, ": 你的好友#{user.name}#{timedesc}在#{arr[1]}，#{dis}，快和TA打个招呼吧")
