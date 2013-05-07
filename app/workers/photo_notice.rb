25757cc15f43c0631cd7720111659036a77aae52
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-05-07 21:18:24 +0800

    m

--- app/workers/photo_notice.rb
+++ app/workers/photo_notice.rb
@@ -3,13 +3,14 @@
 class PhotoNotice
   @queue = :normal
 
-  def self.perform(pid,uid)
+  def self.perform(pid)
     photo = Photo.find_by_id(pid)
-    photo.user.followers.each do |u|
+    user = photo.user
+    user.followers.each do |u|
       str = ": 推送：#{photo.user.name}刚刚在#{photo.shop.name}分享了一张图片"
       str += ",#{photo.desc}" unless photo.desc.nil?
-      Resque.enqueue(XmppMsg, uid, u.id, str)
-      Resque.enqueue(XmppMsg, uid, u.id, "[img:#{pid}]")
+      Resque.enqueue(XmppMsg, user.id, u.id, str)
+      Resque.enqueue(XmppMsg, user.id, u.id, "[img:#{pid}]")
     end
   end
   
