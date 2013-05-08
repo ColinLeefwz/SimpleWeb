0b7844410ebea70c1bc62924b581ca6fc8822940
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-05-08 15:49:57 +0800

    签到和发照片的通知消息

--- app/workers/photo_notice.rb
+++ app/workers/photo_notice.rb
@@ -12,6 +12,24 @@ class PhotoNotice
       Resque.enqueue(XmppMsg, user.id, u.id, str)
       Resque.enqueue(XmppMsg, user.id, u.id, "[img:#{pid}]")
     end
+    shop = photo.shop
+    same_location_realtime(user.id, shop.id, user, shop)
+  end
+
+  def self.same_location_realtime(uid,sid,user,shop)
+    now = Time.now.to_i
+    $redis.zrangebyscore("UA#{sid.to_i}", now-3600, now).each do |id|
+      next if uid==id
+      push(id,user,shop)
+    end
   end
   
+
+  def self.push(id,user,shop)
+    token = User.find_by_id(id).tk
+    return unless token
+    Resque.enqueue(PushMsg, token,
+     "#{user.name}刚刚在#{shop.name}分享了一张照片！")
+  end
+    
 end
