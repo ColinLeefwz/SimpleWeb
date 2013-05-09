bc5f9048e98624d03e9640e34d56ab892a2b0a7d
pulsen <huang123qwe@126.com>
2013-05-09 17:15:22 +0800

    商家所有图片，可以点击缩略图查看大图

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -301,7 +301,8 @@ class Shop
 
   def gchat
     $xmpp_ips.count.times do |t|
-      url = "http://#{$xmpp_ips[t]}:5280/api/gchat?room=#{self.id.to_i}"
+
+      url = "http://#{$xmpp_ips[t]}:5280/api/gchat?room=#{21828775}"
       begin
         return JSON.parse(RestClient.get(url))
       rescue
