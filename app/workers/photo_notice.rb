ccd6ca1dcb70a0d9ae382acd82de0064a11c4af1
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-05-10 12:03:32 +0800

    图片8小时PUSH通知一次

--- app/workers/photo_notice.rb
+++ app/workers/photo_notice.rb
@@ -4,14 +4,18 @@ class PhotoNotice
   @queue = :normal
 
   def self.perform(pid)
-    photo = Photo.find_primary(pid)
+    photo = Photo.find_by_id(pid)
     user = photo.user
     user.followers.each do |u|
-      next if u.id.to_s == $gfuid
-      str = ": 推送：#{photo.user.name}刚刚在#{photo.shop.name}分享了一张图片"
+      str = ": #{photo.user.name}刚刚在#{photo.shop.name}分享了一张图片"
       str += ",#{photo.desc}" unless photo.desc.nil?
-      Resque.enqueue(XmppMsg, user.id, u.id, str)
-      Resque.enqueue(XmppMsg, user.id, u.id, "[img:#{pid}]")
+      if Rails.cache.read("PhotoFan#{uid}")
+        Resque.enqueue(XmppMsg, user.id, u.id, str, "NOPUSH#{$uuid.generate}")
+      else
+        Resque.enqueue(XmppMsg, user.id, u.id, str)  
+        Rails.cache.write("PhotoFan#{uid}", 1, :expires_in => 8.hours)      
+      end
+      Resque.enqueue(XmppMsg, user.id, u.id, "[img:#{pid}]", "NOPUSH#{$uuid.generate}")
     end
     shop = photo.shop
     same_location_realtime(user.id, shop.id, user, shop)
