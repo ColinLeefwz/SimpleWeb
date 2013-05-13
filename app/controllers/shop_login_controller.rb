b44a53ff3f9d15516cbc5fe9e2beb77846d0a3ef
pulsen <huang123qwe@126.com>
2013-05-13 10:52:09 +0800

    商家登录出错调整

--- app/controllers/shop_login_controller.rb
+++ app/controllers/shop_login_controller.rb
@@ -10,30 +10,20 @@ class ShopLoginController < ApplicationController
 
 
   def login
+    #     Rails.cache.delete("LE#{real_ip}")
     return redirect_to :action  => 'index' if session[:shop_id]
-    ip = real_ip
-    error_num, aen = Rails.cache.read("LE#{ip}").to_s.split(';')
-    return  render(:text => "密码错误#{error_num}次，请一个小时后再试") if error_num.to_i == 5
     if request.post?
       shop =  Shop.find_by_id(params[:id])
       return flash.now[:notice] = 'id没有找到.' if shop.nil?
       if shop.password.blank? || shop.password != params[:password]
-        error_num = error_num.to_i+ 1
-        if aen.nil?
-          cityname, citycode = RequestApi::TaoBaoIP.fetch_city(ip), nil
-          unless cityname.blank?
-            cityname = cityname.sub(/[市]/, '')
-            citycode= City.where(name: /#{cityname}/).limit(1).to_a.first.try(:code)
-          end
-          aen =  (citycode && citycode != shop.city) ? 3 : 5
-        end
-        aen = aen.to_i
-        Rails.cache.write("LE#{ip}" ,"#{error_num};#{aen}", :expires_in => 1.hour)
-        return redirect_to :action => 'login'  if aen - error_num ==0
-        LoginFail.create(:name => params[:id], :password => params[:password], :login_at => Time.now, :ip => ip )
-        return flash.now[:notice] = "密码输入错误，您还有#{aen.to_i-error_num}次机会"
+        ip = real_ip
+        error_num, allow_err_num=  err_num(shop.city, ip)
+        return  render(:text => "您的ip已经被锁定一小时，请稍后再试!")    if allow_err_num == error_num
+        Rails.cache.write("LE#{ip}" ,"#{error_num};#{allow_err_num}", :expires_in => 1.hour)
+        LoginFail.create(:name => params[:id], :password => params[:password], :login_at => Time.now, :ip => ip, :agent => request.env['HTTP_USER_AGENT'] )
+        return flash.now[:notice] = "密码输入错误，您还有#{allow_err_num - error_num}次机会"
       end
-      LoginSuccess.create(:name => params[:id], :login_at => Time.now, :ip => ip  )
+      LoginSuccess.create(:name => params[:id], :login_at => Time.now, :ip => ip, :agent => request.env['HTTP_USER_AGENT']  )
       session[:shop_id] = shop.id
       cookies[:id], cookies[:password] =params[:id],params[:password] if params[:remember]=='1'
       o_uri_path, session[:o_uri_path] = session[:o_uri_path]||'/shop_login/index' , nil
@@ -58,13 +48,23 @@ class ShopLoginController < ApplicationController
 
   private
 
-  def asyn_city
+  def err_num(city_code, ip)
     
+    err_cache = Rails.cache.read("LE#{ip}")
+    if err_cache.nil?
+      cityname, citycode = RequestApi::TaoBaoIP.fetch_city(ip), nil
+      unless cityname.blank?
+        cityname = cityname.sub(/[市]/, '')
+        citycode= City.where(name: /#{cityname}/).limit(1).to_a.first.try(:code)
+      end
+      [1, (citycode && citycode != city_code) ? 3 : 5]
+    else
+      data = err_cache.split(';').map{|m| m.to_i }
+      data[0] += 1
+      data
+    end
   end
 
-  def write_cache(ip)
-    
-  end
 
 end
 
