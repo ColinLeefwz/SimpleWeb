5b43bf0d9770ed74d10276d1823788150f9f0f1a
pulsen <huang123qwe@126.com>
2013-05-14 10:02:16 +0800

    登录封ip测试

--- app/controllers/shop_login_controller.rb
+++ app/controllers/shop_login_controller.rb
@@ -10,19 +10,19 @@ class ShopLoginController < ApplicationController
 
 
   def login
-#    Rails.cache.delete("LE#{real_ip}")
+    #   Rails.cache.delete("LE#{real_ip}")
     return redirect_to :action  => 'index' if session[:shop_id]
     if request.post?
       shop =  Shop.find_by_id(params[:id])
       return flash.now[:notice] = 'id没有找到.' if shop.nil?
       ip = real_ip
       err_cache = cache_err_num(ip)
-      return  render(:text => "您的ip已经被锁定一小时，请稍后再试!") if !err_cache.nil? && err_cache[0]+1 == err_cache[1]
+      return  flash.now[:notice] = "您的ip已经被锁定一小时，请稍后再试!" if !err_cache.nil? && err_cache[0] == err_cache[1]
       if shop.password.blank? || shop.password != params[:password]
         error_num, allow_err_num = err_cache ? [err_cache[0] +1, err_cache[1]] :  city_err_num(shop.city, ip)
         Rails.cache.write("LE#{ip}" ,"#{error_num};#{allow_err_num}", :expires_in => 1.hour)
         LoginFail.create(:name => params[:id], :password => params[:password], :login_at => Time.now, :ip => ip, :agent => request.env['HTTP_USER_AGENT'] )
-        return flash.now[:notice] = "密码输入错误，您还有#{allow_err_num - error_num}次机会"
+        return flash.now[:notice] = (allow_err_num== error_num ? '您的ip已经被锁定一小时，请稍后再试!' :  "密码输入错误，您还有#{allow_err_num - error_num}次机会")
       end
       LoginSuccess.create(:name => params[:id], :login_at => Time.now, :ip => ip, :agent => request.env['HTTP_USER_AGENT']  )
       Rails.cache.delete("LE#{ip}")
