5a50c9bf23a77de061e074a03b6b33d1b12577b8
pulsen <huang123qwe@126.com>
2013-05-14 10:02:16 +0800

    登录封ip测试

--- test/functional/shop_login_controller_test.rb
+++ test/functional/shop_login_controller_test.rb
@@ -2,6 +2,12 @@
 require 'test_helper'
 
 class ShopLoginControllerTest < ActionController::TestCase
+
+  def setup
+    Rails.cache.delete("LE#{request.ip}")
+  end
+
+
   test '没登录跳转到登录页面' do
     get :index
     assert_redirected_to :action => "login"
@@ -12,17 +18,44 @@ class ShopLoginControllerTest < ActionController::TestCase
     assert_equal "id没有找到.", flash[:notice]
   end
 
-  test '没有开通密码的商家登陆' do
+  test '没有开通密码的商家登陆5次ip封一个小时' do
     post :login, :id => 112, :password => '123456'
-    assert_equal "密码还没有开通.", flash[:notice]
+    assert_equal "密码输入错误，您还有4次机会", flash[:notice]
+    post :login, :id => 112, :password => '123456'
+    assert_equal "密码输入错误，您还有3次机会", flash[:notice]
+    post :login, :id => 112, :password => '123456'
+    assert_equal "密码输入错误，您还有2次机会", flash[:notice]
+    post :login, :id => 112, :password => '123456'
+    assert_equal "密码输入错误，您还有1次机会", flash[:notice]
+    post :login, :id => 112, :password => '123456'
+    assert_equal "您的ip已经被锁定一小时，请稍后再试!", flash[:notice]
+    post :login, :id => 2, :password => '123456'
+    assert_equal "您的ip已经被锁定一小时，请稍后再试!", flash[:notice]
+    
   end
 
-  test '商家使用错误的密码登陆' do
-    post :login, :id => 2, :password => '123456222'
-    assert_equal "密码错误.", flash[:notice]
+  test '商家使用错误的密码登陆5次ip封一个小时' do
+    post :login, :id => 2, :password => '1234561'
+    assert_equal "密码输入错误，您还有4次机会", flash[:notice]
+    post :login, :id => 2, :password => '1234561'
+    assert_equal "密码输入错误，您还有3次机会", flash[:notice]
+    post :login, :id => 2, :password => '1234561'
+    assert_equal "密码输入错误，您还有2次机会", flash[:notice]
+    post :login, :id => 2, :password => '1234561'
+    assert_equal "密码输入错误，您还有1次机会", flash[:notice]
+    post :login, :id => 2, :password => '1234561'
+    assert_equal "您的ip已经被锁定一小时，请稍后再试!", flash[:notice]
   end
 
-  test '登陆成功' do
+  test '商家使用错误的密码登陆4次后登陆成功' do
+    post :login, :id => 2, :password => '1234561'
+    assert_equal "密码输入错误，您还有4次机会", flash[:notice]
+    post :login, :id => 2, :password => '1234561'
+    assert_equal "密码输入错误，您还有3次机会", flash[:notice]
+    post :login, :id => 2, :password => '1234561'
+    assert_equal "密码输入错误，您还有2次机会", flash[:notice]
+    post :login, :id => 2, :password => '1234561'
+    assert_equal "密码输入错误，您还有1次机会", flash[:notice]
     post :login, :id => 2, :password => '123456'
     assert_redirected_to "/shop_login/index"
   end
