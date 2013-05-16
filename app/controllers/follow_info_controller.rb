05bc1399b566fc38d852039bc87c431e6d45fa6a
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-05-16 16:22:41 +0800

    好友相关接口变更

--- app/controllers/follow_info_controller.rb
+++ app/controllers/follow_info_controller.rb
@@ -10,6 +10,7 @@ class FollowInfoController < ApplicationController
     output_users(users)
   end
   
+  #deprecated
   def friends
     who = UserFollow.find_by_id(params[:id])
     if who.nil?
@@ -23,6 +24,19 @@ class FollowInfoController < ApplicationController
     output_users(users.reverse)
   end
   
+  def friend_ids
+    who = UserFollow.find_by_id(params[:id])
+    #TODO: 排序
+    render :json =>  who.follows
+  end
+
+  def friend_infos
+    who = UserFollow.find_by_id(params[:id])
+    arr = who.follows.map{|id| User.find_by_id(id)}.find_all{|x| x!=nil}
+    render :json =>  arr.map{|u| u.safe_output}.to_json
+  end
+    
+  #deprecated
   def good_friends
     who = User.find_by_id(params[:id])
     if who.nil?
@@ -30,10 +44,33 @@ class FollowInfoController < ApplicationController
       return
     end
     users = who.good_friends
-    #TODO: redis端分页
     users.delete_if {|x| x.name.index(params[:name])==nil } unless params[:name].nil?
     output_users(users.reverse)
   end
   
+  def good_friend_ids
+    who = User.find_by_id(params[:id])
+    if who.nil?
+      render :json => [].to_json
+      return
+    end
+    render :json => who.good_friend_ids.to_json
+  end
   
+  def friend_locs
+    arr = params[:ids].split(",").map do |id|
+      loc = User.last_loc_cache(id)
+      {id:id}.merge(User.last_loc_to_hash(loc))
+    end
+    render :json => arr.to_json
+  end
+  
+  def good_friend_locs
+    friend_locs
+  end
+
+  def fan_locs
+    friend_locs
+  end
+    
 end
