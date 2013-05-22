0034ade7ee8654f5e0025648fe7db5063e183c06
pulsen <huang123qwe@126.com>
2013-05-22 15:31:48 +0800

    权限分配

---
+++ app/controllers/admin_rights_controller.rb
@@ -0,0 +1,67 @@
+class AdminRightsController < ApplicationController
+  include Paginate
+  before_filter :admin_authorize
+  layout "admin"
+
+  def index
+    hash = {}
+    sort = {}
+    @rights = paginate3("Right", params[:page], hash, sort)
+  end
+
+  def new
+    return redirect_to :action => "edit", :id => params[:id] if Right.find_by_id(params[:id])
+  end
+
+  def create
+    right = Right.new(:data => [])
+    right._id = params['id']
+    par = params.select{|k,v| k.in?(admin_controller_names)}
+    par.each do |k,v|
+      case v
+      when 'r'
+        right.data << {'c' => k, "r" => true}
+      when 'w'
+        right.data << {'c' => k, "r" => false}
+      end
+    end
+    right.save
+    redirect_to :action => "show", :id => right.id
+  end
+
+  def show
+    @right = Right.find(params[:id])
+  end
+
+  def edit
+    @right = Right.find(params[:id])
+  end
+
+  def update
+    @right = Right.find(params[:id])
+    par = params.select{|k,v| k.in?(admin_controller_names)}
+    par.each do |k,v|
+      case v
+      when 'none'
+        @right.data.delete_if{|r| r['c'] == k }
+      when 'r'
+        next if @right.data.find{|d| d['c'] == k}
+        @right.data << {'c' => k, "r" => true}
+      when 'w'
+        next if @right.data.find{|d| d['c'] == k}
+        @right.data << {'c' => k, "r" => false}
+      end
+    end
+    @right.save
+    redirect_to :action => "show", :id => @right.id
+  end
+
+
+  def admin_controller_names
+    data = YAML.load_file(Rails.root.to_s+"/config/locales/contro.yml")["right"]
+    names = []
+    data.each_value{|e| names += e.keys}
+    names
+  end
+
+end
