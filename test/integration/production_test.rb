c1580e90772e60044f77612e157d895318499c59
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-05-23 09:27:30 +0800

    防止eth1没有的时候出错

--- test/integration/production_test.rb
+++ test/integration/production_test.rb
@@ -53,7 +53,7 @@ class ProductionTest < ActionDispatch::IntegrationTest
   end
   
   test "实际部署的生产系统上测试" do
-    if `ifconfig eth1`.to_s.index("inet addr:42")
+    if `ifconfig eth0`.to_s.index("inet addr:10") && `ifconfig eth1`.to_s.index("inet addr:42")
       puts "本机器是生产系统"
       aliyun_tst
       mongoid_tst
