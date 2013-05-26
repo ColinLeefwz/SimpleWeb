b9919e3496b0349a0a8b3a608da6818e5f0c4b2a
web1 <web1@dface.cn>
2013-05-26 11:23:58 +0800

    version处理

--- android_upload.sh
+++ android_upload.sh
@@ -1,4 +1,5 @@
-rm /mnt/lianlian/public/dface.apk
-cp /mnt/dface-release.apk /mnt/lianlian/public/dface.apk
+#rm /mnt/lianlian/public/dface.apk
+mv /mnt/dface-release${1}.apk /mnt/lianlian/public/dface${1}.apk
 /mnt/Oss/oss2//osscmd deleteallobject oss://dface/dface.apk
-/mnt/Oss/oss2/osscmd put /mnt/lianlian/public/dface.apk oss://dface/dface.apk --content-type=application/octet-stream
+/mnt/Oss/oss2/osscmd put /mnt/lianlian/public/dface${1}.apk oss://dface/dface.apk --content-type=application/octet-stream
+redis-cli -h 10.200.141.172 set android_version ${1}
