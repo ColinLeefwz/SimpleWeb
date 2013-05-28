ff787bb5b27ed3ee71a58782f47b8c96cd56e750
pulsen <huang123qwe@126.com>
2013-05-28 14:42:29 +0800

    contro.yml 文件删除改成module

--- app/helpers/admin_rights_helper.rb
+++ app/helpers/admin_rights_helper.rb
@@ -1,11 +1,9 @@
 # encoding: utf-8
 module AdminRightsHelper
-  def yaml_data
-    @yaml_data ||= YAML.load_file(Rails.root.to_s+"/config/locales/contro.yml")
-  end
+
 
   def controller_names
-    @controller_names ||= yaml_data["right"].values.inject({}){|f,s| f.merge(s)}
+    @controller_names ||= ControllerName::Right.values.inject({}){|f,s| f.merge(s)}
   end
 
   def show_right(data)
