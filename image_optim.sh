d76c9ce8d90aae8d15146c7d961ae354bc74a6f6
pulsen <huang123qwe@126.com>
2013-05-28 16:57:01 +0800

    图片脚本处理

--- image_optim.sh
+++ image_optim.sh
@@ -1,29 +1,32 @@
-#find /home/dev/workspace/lianlian/public -size +8k -cmin -2000 -iname "*.jpg" -name uploads -prune -o -print | xargs jpegoptim --strip-all > /dev/null
-#find /home/dev/workspace/lianlian/public -size +8k -cmin -2 -iname "*.jpeg" | xargs jpegoptim --strip-all > /dev/null
-#find /home/dev/workspace/lianlian/public -size +2k -cmin -2 -iname "*.png" | xargs optipng > /dev/null
-#find /home/dev/workspace/lianlian/public -size +8k -cmin -2 -iname "*.jpg" | xargs
+#find /home/dev/workspace/lianlian/public -size +8k -iname "*.jpg"  | xargs jpegoptim --strip-all > /dev/null
+#find /home/dev/workspace/lianlian/public -size +8k -iname "*.jpeg" | xargs jpegoptim --strip-all > /dev/null
+#find /home/dev/workspace/lianlian/public -size +2k  -iname "*.png" | xargs optipng > /dev/null
+#find /home/dev/workspace/lianlian/public -size +8k  -iname "*.jpg" | xargs
 
 
 for i in `find /mnt/lianlian/public -size +8k -cmin -10  -iname "*.jpg"`
 do
-jpegoptim --strip-all $i > /dev/null
- rpath=${i/'/mnt/public'/}
- fname=${rpath//\//_}
-/mnt/Oss/oss2/osscmd put $i "oss://dface/$fname"
- uname="http://oss.aliyun.com/dface/$fname"
+    if [[ $i != *public/coupon* ]] && [[ $i != *public/uploads* ]]
+        then
+        jpegoptim --strip-all $i > /dev/null
+        rpath=${i/'/mnt/public'/}
+         fname=${rpath//\//_}
+        #/mnt/Oss/oss2/osscmd put $i "oss://dface/$fname"
+         uname="http://oss.aliyun.com/dface/$fname"
 
-if wget --spider  $uname ; then
-    for html in `find /mnt/lianlian/public -name "*.html"`
-    do
-     rpl   "\"$rpath\"" "\"$uname\"" $html
-     rpl    "'$rpath'" "\"$uname\"" $html
-    done
+        if wget --spider  $uname ; then
+            for html in `find /mnt/lianlian/public -name "*.html"`
+            do
+             rpl   "\"$rpath\"" "\"$uname\"" $html
+             rpl   "'$rpath'" "\"$uname\"" $html
+             rpl   "$rpath" "$uname" $html
+            done
 
-    for css in `find /mnt/lianlian/public -name "*.css"`
-    do
-     rpl  "..$rpath" $uname $css
-     rpl  "$rpath" $uname $css
-    done
-fi
-
+            for css in `find /mnt/lianlian/public -name "*.css"`
+            do
+             rpl  "..$rpath" $uname $css
+             rpl  "$rpath" $uname $css
+            done
+        fi
+    fi
 done
