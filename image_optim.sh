bad4cf73f7108cfe3f544042aafbbe007b251d48
pulsen <huang123qwe@126.com>
2013-05-29 10:07:49 +0800

    图片压缩处理脚本

--- image_optim.sh
+++ image_optim.sh
@@ -3,22 +3,70 @@
 #find /home/dev/workspace/lianlian/public -size +2k  -iname "*.png" | xargs optipng > /dev/null
 #find /home/dev/workspace/lianlian/public -size +8k  -iname "*.jpg" | xargs
 
+dir=/mnt/lianlian/public
+jpgs=`find $dir -size +8k  -iname "*.jpg"`
+jpegs=`find $dir -size +8k -cmin -1 -iname "*.jpeg"`
+pngs=`find $dir -size +2k -cmin -1 -iname "*.png"`
 
-for i in `find /mnt/lianlian/public -size +8k -cmin -10  -iname "*.jpg"`
+for i in $jpgs
 do
     if [[ $i != *public/coupon* ]] && [[ $i != *public/uploads* ]] && [[ $i != *public/phone* ]] && [[ $i != *public/phone2* ]]
         then
         jpegoptim --strip-all $i > /dev/null
-        rpath=${i/'/mnt/lianlian/public/'/}
+        rpath=${i/"$dir/"/}
          fname=${rpath//\//_}
         /mnt/Oss/oss2/osscmd put $i "oss://dface/$fname"
          uname="http://oss.aliyuncs.com/dface/$fname"
 
         if wget --spider  $uname ; then
-            html=`find /mnt/lianlian/public -name "*.html"`
+            html=`find $dir -name "*.html"`
             rpl "/$rpath" "$uname" $html
             rpl "$rpath" "$uname"  $html
-            css=`find /mnt/lianlian/public -name "*.css"`
+            css=`find $dir -name "*.css"`
+            rpl "../$rpath" "$uname" $css
+            rpl "/$rpath" "$uname" $css
+            rpl "$rpath" "$uname" $css
+        fi
+    fi
+done
+
+for i in $jpegs
+do
+    if [[ $i != *public/coupon* ]] && [[ $i != *public/uploads* ]] && [[ $i != *public/phone* ]] && [[ $i != *public/phone2* ]]
+        then
+        jpegoptim --strip-all $i > /dev/null
+        rpath=${i/"$dir/"/}
+         fname=${rpath//\//_}
+        /mnt/Oss/oss2/osscmd put $i "oss://dface/$fname"
+         uname="http://oss.aliyuncs.com/dface/$fname"
+
+        if wget --spider  $uname ; then
+            html=`find $dir -name "*.html"`
+            rpl "/$rpath" "$uname" $html
+            rpl "$rpath" "$uname"  $html
+            css=`find $dir -name "*.css"`
+            rpl "../$rpath" "$uname" $css
+            rpl "/$rpath" "$uname" $css
+            rpl "$rpath" "$uname" $css
+        fi
+    fi
+done
+
+for i in $pngs
+do
+    if [[ $i != *public/coupon* ]] && [[ $i != *public/uploads* ]] && [[ $i != *public/phone* ]] && [[ $i != *public/phone2* ]]
+        then
+        optipng $i > /dev/null
+        rpath=${i/"$dir/"/}
+         fname=${rpath//\//_}
+        /mnt/Oss/oss2/osscmd put $i "oss://dface/$fname"
+         uname="http://oss.aliyuncs.com/dface/$fname"
+
+        if wget --spider  $uname ; then
+            html=`find $dir -name "*.html"`
+            rpl "/$rpath" "$uname" $html
+            rpl "$rpath" "$uname"  $html
+            css=`find $dir -name "*.css"`
             rpl "../$rpath" "$uname" $css
             rpl "/$rpath" "$uname" $css
             rpl "$rpath" "$uname" $css
