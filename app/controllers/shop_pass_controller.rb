536dc8a4c787d20eddd42d8b8f68646564797ade
pulsen <huang123qwe@126.com>
2013-05-30 12:30:39 +0800

    密码验证

--- app/controllers/shop_pass_controller.rb
+++ app/controllers/shop_pass_controller.rb
@@ -6,11 +6,12 @@ class ShopPassController < ApplicationController
     @shop = session_shop
     if request.post?
       return flash.now[:notice] = "原来的密码输入不正确！" if @shop.password != params[:oldpass]
-      if @shop.update_attributes(params[:shop])
+      return flash.now[:notice] = "密码修改失败,请确保两次密码一致." if params[:shop][:password_confirmation] != params[:shop][:password]
+      if @shop.update_attribute(:password, params[:shop][:password])
         flash[:notice] = '密码修改成功.'
         return redirect_to '/shop_login/index'
       else
-        flash.now[:notice] = '密码修改失败,请确保两次密码一致.'
+        flash.now[:notice] = '密码修改失败.'
       end
     end
   end
