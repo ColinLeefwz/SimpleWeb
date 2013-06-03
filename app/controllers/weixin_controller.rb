a0749a0349593433d089c3016c38126277c339a3
pulsen <huang123qwe@126.com>
2013-06-03 13:57:40 +0800

    微信自动回答

---
+++ app/controllers/weixin_controller.rb
@@ -0,0 +1,22 @@
+class WeixinController < ApplicationController
+  def index
+    return render :text => params['echostr']  if params[:xml].blank?
+    case params[:xml][:MsgType]
+    when "text"
+      @text = params[:xml][:Content]
+      return render "text", :formats => :xml
+    when "location"
+      lo = [params[:xml][:Location_X].to_f,params[:xml][:Location_Y].to_f]
+      @text = Shop.where({lo:{"$near" =>lo}}).limit(5).only(:name).map{|m| m.name}.join("\n")
+      return render "text", :formats => :xml
+    when 'image'
+      @picurls = []
+      photo = Photo.last
+      @picurls << {"title" => "#{photo.user.name}发布" , "description" => photo.desc, "picurl" => photo.img.url(:t2), "url" => photo.img.url }
+      return  render "picurl", :formats => :xml
+    when "link"
+      @text = "http://dface.cn"
+      render "text", :formats => :xml
+    end
+  end
+end
