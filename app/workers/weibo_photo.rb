da09cfb465c9c3c73665d3baa25985ed1f21292c
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-06-10 11:31:51 +0800

    忽略微博接口的400/403错误

--- app/workers/weibo_photo.rb
+++ app/workers/weibo_photo.rb
@@ -3,9 +3,20 @@
 class WeiboPhoto
   @queue = :normal
 
-  def self.perform(token, text, url)
+  def self.perform0(token, text, url)
     RestClient.post('https://api.weibo.com/2/statuses/upload_url_text.json', 
         :access_token  => token , :status => text[0..139], 
         :url => url)
   end
+  
+  def self.perform(token, text, url)
+    begin
+      perform0(token, text, url)
+    rescue Exception => e
+      return if e.to_s[0,3]=="403"  || e.to_s[0,3]=="400"
+      raise e
+    end
+  end
+  
+  
 end
