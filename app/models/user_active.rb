278c44a8ae4030f95381d2ea563da7a4bc6b2d5b
pulsen <huang123qwe@126.com>
2013-06-14 16:11:04 +0800

    用户活跃度分析

--- app/models/user_active.rb
+++ app/models/user_active.rb
@@ -1,12 +1,12 @@
 # coding: utf-8
-# 旅行线路，以后也可以用作火车线路等
+
 class UserActive
   include Mongoid::Document
   field :_id, type:String 
-  field :data, type:Hash # [ { time:相对时间，lo:经纬度, sid:地点id } ]
+  field :data, type:Hash # [ { 以前的时间点或register: 以前时间点内注册并且在当前id内签到的用户数量 或 当前id内注册量 } ]
 
 
-  #user_active.id 获取要查询的时间段
+  #user_active.id 获取要查询的时间段的id
   def ttoid(uat)
     year, month,week =  uat.split("-").map{|m| m.to_i}
     if week
@@ -29,16 +29,19 @@ class UserActive
     [stid.to_i.to_s(16).ljust(24,'0'), etid.to_i.to_s(16).ljust(24,'0')]
   end
 
+  #根据id 计算时间段内注册的用户
   def register_user_num
     @rstid, @retid = ttoid(self.id)
     User.where( {_id: {'$gte' => @rstid, "$lte" => @retid}, auto: nil}).count
   end
 
+  #传入时间， 计算在传入的时间段内注册，并且当前在user_active.id 内签到的用户数
   def checkin_user_num(ctime)
     cstid, cetid = ttoid(ctime)
     Checkin.where({_id: {'$gte' => @rstid, "$lte" => @retid}, uid: {'$gte' => cstid, "$lte" => cetid}}).distinct("uid").count
   end
 
+  #计算当前user_active 并保存到mongo中
   def do_count(uaid)
     self._id = uaid
     data = {'register' =>  register_user_num}
@@ -49,6 +52,7 @@ class UserActive
     self.save
   end
 
+  #类方法， cron的调用接口， 月初或每月的每周后一天执行。
   def self.do_count(uaid = nil)
     if uaid.nil?
       time = Time.now
