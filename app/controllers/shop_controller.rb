9d1fed1254d44ba29edc7850c3b6c0a03998541c
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-06-17 15:56:57 +0800

    m

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -74,8 +74,7 @@ class ShopController < ApplicationController
   
   def auth
     shop = Shop.find_by_id(params[:shop_id])
-    user = User.find_by_id(params[:user_id])
-    flag = shop.group.auth(user.id, params[:txt])
+    flag = shop.group.auth(session[:user_id], params[:txt])
     if flag
       render :json => {ret:1}.to_json
     else
