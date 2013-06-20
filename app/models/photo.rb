7bd719be322b084044c397bfc9b294da31029bef
pulsen <huang123qwe@126.com>
2013-06-20 16:14:54 +0800

    马甲6分后三分之一概率随机赞

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -52,7 +52,7 @@ class Photo
     end
     return if ENV["RAILS_ENV"] == "test"
     Resque.enqueue(XmppRoomMsg2, room.to_i.to_s, user_id, "[img:#{self._id}]#{self.desc}")
-    Resque.enqueue_in(30.seconds, PhotoLike, self._id, self.user.gender)
+    Resque.enqueue_in(360.seconds, PhotoLike, self._id, self.user.gender) if rand(1..3)==1
   end
   
   def send_wb
