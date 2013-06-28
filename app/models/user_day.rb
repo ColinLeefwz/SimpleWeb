7d7730cb3538d00a9b2add6e55a201c93cfc4379
tony <tony_doofy@163.com>
2013-06-28 11:11:47 +0800

    每日用户新增图形化

--- app/models/user_day.rb
+++ app/models/user_day.rb
@@ -3,8 +3,8 @@
 class UserDay
   include Mongoid::Document
   field :_id, type: String #日期
-  # field :umtotal, type: Integer #男性用户总数
-  # field :uftotal, type: Integer #女性用户总数
+  field :umtotal, type: Integer #男性用户总数
+  field :uftotal, type: Integer #女性用户总数
   field :wb, type: Integer
   field :qq, type: Integer
   
