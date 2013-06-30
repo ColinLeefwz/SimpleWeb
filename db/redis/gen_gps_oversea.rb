f1549927d561b6a264f8af045ccd17fb0d53b7e2
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-06-30 14:08:17 +0800

    更新商家和用户的city

--- db/redis/gen_gps_oversea.rb
+++ db/redis/gen_gps_oversea.rb
@@ -27,4 +27,39 @@ rescue Exception => e
 end
 
 
+Shop.where({"$and" => [_id:{"$gt" => 20347004},_id:{"$lt" => 21000000}],city:nil}).each do |shop|
+  city = Shop.get_ex_city(shop.lo)
+  if city
+    shop.update_attribute(:city, city)
+  else
+    puts shop.id
+  end
+end
 
+User.where({city:nil, auto:{"$ne" => true}}).each do |user|
+  ck = user.checkins.last
+  next if ck.nil?
+  city = ck.city
+  if city.nil? && ck.shop
+    city = ck.shop.city
+  end
+  user.update_attribute(:city, city)
+end
+User.where({city:{"$exists" => false}, auto:{"$ne" => true}}).each do |user|
+  ck = user.checkins.last
+  next if ck.nil?
+  city = ck.city
+  if city.nil? && ck.shop
+    city = ck.shop.city
+  end
+  user.update_attribute(:city, city)
+end
+
+User.where({city:/^x/}).each do |u|
+  ck = u.checkins.last
+  x = u.gender
+  x = 1 if x!=2
+  key = "HOT#{x}U#{u.city}"
+  puts key
+  $redis.zadd(key,ck.cati, ck.uid)
+end
