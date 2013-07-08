ce72e1537c9d79c08f138ff01c07a2e909e89341
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-07-08 09:29:21 +0800

    预设密码的群

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,7 @@ class Group
   include Mongoid::Document
   field :name #名称 ， 默认为"线路名称＋团号"
   field :code #团号
+  field :pass #加入时的默认密码，可以没有
   field :line_id, type: Moped::BSON::ObjectId #所属线路
   field :admin_sid, type:Integer #所属旅行社
   field :sid, type:Integer #旅行团对应的虚拟群  
@@ -28,7 +29,13 @@ class Group
     self.update_attribute(:sid, s.id)
   end
   
+  #加入一个群目前有两种情况
+  #1. 婚礼现场，预设密码，输入密码进入
+  #2. 旅行团，预设人员，输入手机号码进入
   def auth(uid, str)
+    if pass
+      return str==pass
+    end
     user = users.find{|hash| hash["phone"]==str && hash['id'].nil?}
     if user
       user.merge!({"id" => uid})
