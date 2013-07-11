9d830f8dc31d9e9d8ea5c1dc122d9548e28e6979
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-07-11 10:34:29 +0800

    m

--- app/workers/new_user.rb
+++ app/workers/new_user.rb
@@ -5,7 +5,7 @@ class NewUser
 
   def self.perform(uid,sid,od)
     ud=UserDevice.find_by_id(uid)
-    ["502e6303421aa918ba000001","50446058421aa92042000002","50ffd0e5c90d8bf7480000b7","516940f2c90d8b64f0000058"].each do |to|
+    ["502e6303421aa918ba000001","50446058421aa92042000002","50ffd0e5c90d8bf7480000b7","516940f2c90d8b64f0000058","51b16477c90d8ba01f0000bb","51b5659dc90d8b511d00002f"].each do |to|
       NewUser.notify(uid,sid, to, od, ud)
     end
     ["502e6303421aa918ba000079"].each do |to|
