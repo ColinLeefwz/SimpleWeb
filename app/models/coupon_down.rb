36e6e03db60a1e6a151ffa2e1a78bc9f3927874a
pulsen <huang123qwe@126.com>
2013-07-12 10:55:22 +0800

    优惠券编号

--- app/models/coupon_down.rb
+++ app/models/coupon_down.rb
@@ -13,6 +13,7 @@ class CouponDown
   field :photo_id, type: Moped::BSON::ObjectId # 分享类优惠券的分享图片id
   field :sub_sid, type: Integer #获得主店分享类优惠券时，实际分享发生的分店id
   field :data #消费时输入的数据，可以是消费金额／手机号码／服务员编号等
+  field :num #优惠券下载编号， 每个优惠券独立编号
 
   with_options :prefix => true, :allow_nil => true do |option|
     option.delegate :name, :gender, :birthday, :weibo_home,:show_gender, :to => :user
@@ -23,7 +24,6 @@ class CouponDown
 
   index({cid: 1, uid:1})
   index({dat: -1})
-  
   
   def shop
     Shop.find_by_id(sid)
@@ -52,6 +52,11 @@ class CouponDown
     s += "]"
     s
   end
+
+  def self.next_num(cid)
+    cpdown = self.where({cid: cid}).sort({num: -1}).limit(1).first
+    (cpdown && cpdown.num) ? cpdown.num.succ : 1
+  end
   
   def self.download(coupon, user_id, photo_id=nil, sid = nil)
     cpdown = CouponDown.new
@@ -61,6 +66,7 @@ class CouponDown
     cpdown.dat = Time.now
     cpdown.photo_id = photo_id if photo_id
     cpdown.sub_sid = sid if sid
+    cpdown.num = next_num(coupon.id)
     cpdown.save!
     cpdown.gen_share_coupon_img if photo_id
     cpdown
@@ -77,14 +83,19 @@ class CouponDown
     raise "图片分享后才能生成分享类优惠券" unless photo_id
     path = share_coupon_img_path
     return path if File.exist?("public"+path)
-    `cd coupon && ./gen_demo.sh '#{coupon.name}' '#{coupon.desc}' ../public#{path} #{Photo.img_url(photo_id, :t2)}`
+    desc = coupon.desc
+    dea = desc.split(/\r\n/)
+    cnum = "#{coupon.num}#{num.to_s.rjust(3,'0')}"
+    dea[4] = "优惠券编号: #{cnum}"
+    desc = dea.join("\r\n")
+
+    `cd coupon && ./gen_demo.sh '#{coupon.name}' '#{desc}' ../public#{path} #{Photo.img_url(photo_id, :t2)}`
     return path
   end
   
   def share_coupon_img_path
     "/uploads/tmp/cpd_#{self.id}.jpg"
   end
-
 
   #优惠券下发后没有收到时间的， 30秒后至3分钟内自动重发
   def self.auto_resend(tim = Time.now)
