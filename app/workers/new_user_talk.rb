abc0e2317d13c66bbbc7f7f72216a851cba17e07
tony <tony_doofy@163.com>
2013-07-16 16:03:48 +0800

    陪聊用户分配

--- app/workers/new_user_talk.rb
+++ app/workers/new_user_talk.rb
@@ -5,29 +5,53 @@ class NewUserTalk
 
   def self.perform(uid,sid,seq)
     user = User.find(uid)
-    if user.gender==2
-      to = ["50bc20fcc90d8ba33600004b", #“浦靠谱” 运营总监浦希哲
-        "51418836c90d8bc37b000567" #'怪咖叔叔', 马甲，  运营-孙世杰
-      ]
-    else
-      to = ["50bec2c1c90d8bd12f000086", #amanda林
-        "513ed1e7c90d8b590100016f"  #球球爱嘟嘴, 马甲，运营-董玉华
-      ] #
-      #to = User.fake_user(User.find_by_id(uid)).id
+
+    hour = Time.now.hour
+    week = Time.now.wday
+    if (1..5).include?week && hour > 8 && hour < 19
+      if user.gender == 2
+        to = ["51418836c90d8bc37b000567" #'怪咖叔叔', 马甲，  运营-孙世杰
+             ]
+      else
+        to = ["513ed1e7c90d8b590100016f",  #球球爱嘟嘴, 马甲，运营-董玉华
+              "50bec2c1c90d8bd12f000086"   #amanda林
+             ]
+      end
+    
+    elsif (1..5).include?week && hour < 24 && hour > 19
+      if user.gender == 2
+        to = ["51418836c90d8bc37b000567" #'怪咖叔叔', 马甲，  运营-孙世杰
+             ]
+      else
+        to = ["513ed1e7c90d8b590100016f",  #球球爱嘟嘴, 马甲，运营-董玉华
+              "50bec2c1c90d8bd12f000086"   #amanda林
+             ]
+      end
+
+    else (6..7).include?week && hour > 9 && hour < 24
+      if user.gender == 2
+        to = ["51427b92c90d8b670c00027b" #Jap UU  马甲
+             ]
+        chat2(to, uid)
+      else
+        to = ["514190f8c90d8bc67b00054a" #可儿
+             ]
+        chat2(to, uid)
+      end
     end
+
     to = to[user.id.generation_time.sec%to.size]
-    if seq==1
+
+    if seq == 1
       Xmpp.send_chat(to, uid, "hi")
     end
-    if seq==2
+    if seq == 2
       shop = Shop.find_by_id(sid)
       Xmpp.send_chat(to, uid, "你在#{shop.name}？")
-      chat2(uid) if user.gender==2
     end
   end
-  
-  def self.chat2(uid)
-    to = "51427b92c90d8b670c00027b" #Jap UU  马甲
+
+  def self.chat2(to, uid)
     hour = Time.now.hour
     if hour>20 && hour <=3
       msg = "晚上好🌙💤"
@@ -44,5 +68,5 @@ class NewUserTalk
     end
     Resque.enqueue_in(15.seconds,XmppMsg, to ,uid, msg)
   end
-  
+
 end
