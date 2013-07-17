6228d40c9ffb9817bad61ef0821ffb8f053cc492
JevanWu <jevanwu@gmail.com>
2013-07-17 10:31:21 +0800

    initial prodygia project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prodygia::Application.config.secret_key_base = 'b3c608cd6834e6a842ec8dae17ed7c2f57c5b95a58339988f840d7fb283ef5b943ec46f0aadb0b59683449d5b950f4e70921be6d18c4897463315f8e01b49fae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Prodygia::Application.config.session_store :cookie_store, key: '_prodygia_session'
