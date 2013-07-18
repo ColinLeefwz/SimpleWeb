bef6e42a6aa2d6afb06d87e818987ae64f6eb09a
pulsen <huang123qwe@126.com>
2013-07-18 17:48:09 +0800

    优惠券最新下载

---
+++ app/controllers/admin_coupon_reports_controller.rb
@@ -0,0 +1,25 @@
+class AdminCouponReportsController < ApplicationController
+  include Paginate
+  before_filter :admin_authorize
+  layout "admin"
+  
+  def index
+    hash = {}
+    sort = {_id: -1}
+    if !params[:city].blank? && !params[:shop].blank?
+      sids = Shop.where({city: params[:city], name: /^#{params[:shop]}/}).only(:_id).distinct(:_id)
+      hash.merge!(sid: {"$in" => sids})
+    end
+    hash.merge!(sid: params[:sid].to_i) if !params[:sid].blank?
+    hash.merge!(cid: params[:cid]) if !params[:cid].blank?
+    if !params[:uname].blank?
+      uid = User.where({name: /#{params[:uname]}/}).only(:_id).distinct(:_id)
+      hash.merge!(uid: {"$in" => uid})
+    end
+    hash.merge!(uid: params[:uid]) if !params[:uid].blank?
+    @coupon_downs = paginate3("CouponDown", params[:page], hash, sort)
+  end
+
+
+
+end
