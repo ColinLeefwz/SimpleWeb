bdf281eab9d29719c7095635f689d5589a64aaa5
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-07-21 14:19:06 +0800

    m

--- test/integration/mobile_api/user_info_test.rb
+++ test/integration/mobile_api/user_info_test.rb
@@ -23,7 +23,7 @@ class UserInfoTest < ActionDispatch::IntegrationTest
     login(luser.id)
     get "/user_info/get_self"
     assert_response :success
-    assert_equal JSON.parse(response.body), {"gender"=>1.0,"hobby"=>"","invisible"=>0.0,"jobtype"=>nil,"name"=>"袁乐天","oid"=>154.0,"password"=>"c84dad462d5b7282","signature"=>"","wb_uid"=>"a1","pcount"=>0,"id"=>"502e6303421aa918ba000005","logo"=>"","logo_thumb"=>"","logo_thumb2"=>""}
+    assert_equal JSON.parse(response.body), {"gender"=>1,"hobby"=>"","invisible"=>0.0,"jobtype"=>nil,"name"=>"袁乐天","oid"=>154.0,"password"=>"c84dad462d5b7282","signature"=>"","wb_uid"=>"a1","pcount"=>0,"id"=>"502e6303421aa918ba000005","logo"=>"","logo_thumb"=>"","logo_thumb2"=>""}
 
     #未登录获取他人个人信息
     logout
@@ -38,14 +38,14 @@ class UserInfoTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     data.delete('last')
     data.delete('time')
-    assert_equal data, {"gender"=>1.0,"hobby"=>"","invisible"=>0.0,"jobtype"=>nil,"name"=>"袁乐天","oid"=>154.0,"signature"=>"","wb_uid"=>"a1","pcount"=>0,"id"=>"502e6303421aa918ba000005","logo"=>"","logo_thumb"=>"","logo_thumb2"=>"","friend"=>false,"follower"=>false}
+    assert_equal data, {"gender"=>1,"hobby"=>"","invisible"=>0.0,"jobtype"=>nil,"name"=>"袁乐天","oid"=>154.0,"signature"=>"","wb_uid"=>"a1","pcount"=>0,"id"=>"502e6303421aa918ba000005","logo"=>"","logo_thumb"=>"","logo_thumb2"=>"","friend"=>false,"follower"=>false}
 
     #未登录设置个人信息
     logout
     post '/user_info/set', {:name => "测试name", :gender => 1, :birthday => '1988-08-08', :signature => '测试签名档', :job => '程序员', :jobtype => 1, :hobby => '计算机', :invisible => 0}
     assert_response :success
     assert_equal response.body, {"error"=>"not login"}.to_json
-    assert_equal luser.reload.attributes, {"_id"=> luser.id,"gender"=>1.0,"hobby"=>"","invisible"=>0.0,"jobtype"=>nil,"name"=>"袁乐天","oid"=>154.0,"password"=>"c84dad462d5b7282","signature"=>"","wb_uid"=>"a1","pcount"=>0}
+    assert_equal luser.reload.attributes, {"_id"=> luser.id,"gender"=>1,"hobby"=>"","invisible"=>0.0,"jobtype"=>nil,"name"=>"袁乐天","oid"=>154.0,"password"=>"c84dad462d5b7282","signature"=>"","wb_uid"=>"a1","pcount"=>0}
 
     #登录设置个人信息
     login(luser.id)
@@ -58,7 +58,7 @@ class UserInfoTest < ActionDispatch::IntegrationTest
     login(luser.id)
     get "/user_info/get_self"
     assert_response :success
-    assert_equal JSON.parse(response.body), {"birthday"=>"1988-08-08","gender"=>1.0,"hobby"=>"计算机","invisible"=>0.0,"job"=>"程序员","jobtype"=>1,"name"=>"测试name","oid"=>154.0,"password"=>"c84dad462d5b7282","pcount"=>0,"signature"=>"测试签名档","wb_uid"=>"a1","id"=>"502e6303421aa918ba000005","logo"=>"","logo_thumb"=>"","logo_thumb2"=>""}
+    assert_equal JSON.parse(response.body), {"birthday"=>"1988-08-08","gender"=>1,"hobby"=>"计算机","invisible"=>0.0,"job"=>"程序员","jobtype"=>1,"name"=>"测试name","oid"=>154.0,"password"=>"c84dad462d5b7282","pcount"=>0,"signature"=>"测试签名档","wb_uid"=>"a1","id"=>"502e6303421aa918ba000005","logo"=>"","logo_thumb"=>"","logo_thumb2"=>""}
 
   end
   
