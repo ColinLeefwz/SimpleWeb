352dfe02017072817e446bc1f97434489bb9a58e
JevanWu <jevanwu@gmail.com>
2013-07-22 16:58:27 +0800

    test of welcome controller

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe WelcomeController do
+  context 'GET index' do
+  	it "assigns @prodygia_picks" do
+  		prodia = Session.where(status: "Prodygia Picks")
+  		get :index
+  		expect(assigns(:prodygia_picks)).to eq(prodia)
+  	end
 
+  	it "assigns @scheduled" do
+  		scheduled = Session.where(status: "Scheduled")
+  		get :index
+  		expect(assigns(:scheduled)).to eq(scheduled)
+  	end
+
+  	it "assigns @upcoming" do
+  		upcoming = Session.where(status: "Upcoming")
+  		get :index
+  		expect(assigns(:upcoming)).to eq(upcoming)
+  	end
+
+  	it "redirect to 'index' page" do
+  		get :index
+  		expect(response).to render_template("index")
+  	end
+  end
 end
