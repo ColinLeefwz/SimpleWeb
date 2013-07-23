23037961ff9e40af3bb4b41be33307c971b45be9
tony <tony_doofy@163.com>
2013-07-23 10:25:33 +0800

    错误修改

--- lib/controller_name.rb
+++ lib/controller_name.rb
@@ -16,7 +16,7 @@ module ControllerName
     7=>{"admin_user_days"=>"每日新增", "admin_user_hours"=>"每小时新增","user_device_stats"=>"每日注册机型统计",
       "admin_user_city_days"=>"每日城市分布","admin_user_actives" => "用户活跃度", "admin_user_day_actives" => "用户每日活跃度"},
     8=>{},
-    9=>{"checkin_days"=>"每日签到", "admin_admin_activity_shop_day_stats"=>"活动商家每日新增用户首次签到统计","shop_checkin_stats"=>"商家签到统计",
+    9=>{"checkin_days"=>"每日签到", "admin_activity_shop_day_stats"=>"活动商家每日新增用户首次签到统计","shop_checkin_stats"=>"商家签到统计",
       "user_checkin_stats"=>"用户签到统计", "ip_checkin_stats"=>"ip签到统计", "shop_checkin_alts"=>"签到高度统计",
       "checkin_loc_accs"=>"经纬度误差统计", "checkin_user_many"=>"多次签到"},
     10=>{},
