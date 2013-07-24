5b97e90f6812834cd6358505ca251bef0cd96a86
pulsen <huang123qwe@126.com>
2013-07-24 16:46:33 +0800

    路线中添加合作商家

--- app/models/line.rb
+++ app/models/line.rb
@@ -5,6 +5,15 @@ class Line
   field :name #线路名称
   field :admin_sid, type:Integer #所属旅行社
   field :arr, type:Array #线路 [ { time:相对时间，lo:经纬度, sid:地点id } ]
+
+
+  def shop_line_partner
+    ShopLinePartner.find_by_id(id)
+  end
+
+  def partners
+    (shop_line_partner && shop_line_partner.partners)||{}
+  end
   
 end
 
