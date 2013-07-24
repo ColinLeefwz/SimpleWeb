cf169a7a22dd394b1067b707ee80a726db2d66e0
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-07-24 18:53:09 +0800

    紧急切换xmpp服务器ip

--- app/workers/xmpp_room_msg2.rb
+++ app/workers/xmpp_room_msg2.rb
@@ -4,7 +4,7 @@ class XmppRoomMsg2
   @queue = :xmpp
   
   def self.perform(room, uid, msg)
-    RestClient.post("http://#{$xmpp_ip}:5280/api/room", 
+    RestClient.post("http://#{$xmpp_ip[1]}:5280/api/room", 
         :roomid  => room , :message=> msg , :uid => uid)
   end
   
