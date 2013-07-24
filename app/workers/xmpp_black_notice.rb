6cae4acb083c7cdcad4559897449d91bddbd0631
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-07-24 18:53:09 +0800

    紧急切换xmpp服务器ip

--- app/workers/xmpp_black_notice.rb
+++ app/workers/xmpp_black_notice.rb
@@ -6,6 +6,6 @@ class XmppBlackNotice
   def self.perform(uid,bid)
     bu = User.find(bid)
     xmpp = Xmpp.chat($gfuid, uid,": 您好！您对#{bu.name}的举报我们已经收到，在调查清楚后会立刻处理。谢谢您对脸脸的支持！")
-    RestClient.post("http://#{$xmpp_ip}:5280/rest", xmpp) 
+    RestClient.post("http://#{$xmpp_ip[1]}:5280/rest", xmpp) 
   end
 end
