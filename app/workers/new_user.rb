032c044d40aa811c3c0b77bb283b67259ea59fb6
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-07-28 21:23:05 +0800

    防止一个用户在不同的地点收到重复的陪聊信息

--- app/workers/new_user.rb
+++ app/workers/new_user.rb
@@ -4,6 +4,8 @@ class NewUser
   @queue = :normal
 
   def self.perform(uid,sid,od)
+    return if Rails.cache.read("NEWUSER#{uid}")
+    Rails.cache.write("NEWUSER#{uid}", 1)
     ud=UserDevice.find_by_id(uid)
     ["502e6303421aa918ba000001","50446058421aa92042000002","50ffd0e5c90d8bf7480000b7","516940f2c90d8b64f0000058","51b16477c90d8ba01f0000bb","51b5659dc90d8b511d00002f"].each do |to|
       NewUser.notify(uid,sid, to, od, ud)
