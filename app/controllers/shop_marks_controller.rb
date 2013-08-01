103d307489ecd7e470f108f11a33c34b0bba0fd3
pulsen <huang123qwe@126.com>
2013-08-01 09:18:09 +0800

    商家评价

--- app/controllers/shop_marks_controller.rb
+++ app/controllers/shop_marks_controller.rb
@@ -19,19 +19,15 @@ class ShopMarksController < ApplicationController
   end
 
   def  com_authorize
+    render :text => "已评过，不可以再评" if ShopMark.where({sid: params[:sid], uid: params[:uid]}).limit(1).first
     @user = User.find_by_id(params[:uid])
     @shop = Shop.find_by_id(params[:sid])
     @group = Group.find_by_id(params[:gid])
-   
-    if @user.nil?  || @shop.nil?
-      render :text => "不可评论"
-    end
-
+    render :text => "不可评价" if @user.nil?  || @shop.nil?
     if @group
       render :text => "你不是此旅行团的成员" unless @group.users.find{|u| u['id'] == params[:uid]}
       render :text => "此商家不是本次旅行团的合作商家" unless @group.line.partners.values.flatten.include?(params[:sid])
     end
-
   end
 
 end
