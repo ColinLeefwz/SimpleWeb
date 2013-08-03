631b4260ee5aae8b5b736ef3bbbfb855e480a6e6
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-08-03 22:31:43 +0800

    bssid的redis类型从set调整为zset

--- app/models/checkin_bssid_stat.rb
+++ app/models/checkin_bssid_stat.rb
@@ -3,7 +3,7 @@ class CheckinBssidStat
   include Mongoid::Document
   field :_id, type: String
   field :ssid, type: String
-  field :shops, type:Array #{id:商家id,users:[用户id,...]}, 使用该bssid签到的商家及其用户
+  #field :shops, type:Array #{id:商家id,users:[用户id,...]}, 使用该bssid签到的商家及其用户
   field :shop_id, type:Integer #实际所属商家
   field :mobile, type:Boolean #是否是可移动的WIFI
 
@@ -13,82 +13,22 @@ class CheckinBssidStat
     return ckt.shop_id
   end
 
-  def self.kx_users(arr)
-    arr.map{|x| x.to_s}.to_set & $kxs
-  end
-  
-  def self.init_shop_id
-    puts CheckinBssidStat.where({shop_id:nil}).count
-    sum=0
-    CheckinBssidStat.where({shop_id:nil}).each do |x|
-      flag = x.try_set_shop_id
-      sum+=1 if flag
-    end
-    puts sum
-  end
-  
-  def try_set_shop_id
-    arr = shops.map {|hash| [hash["id"] ,CheckinBssidStat.kx_users(hash["users"]).size, hash["users"].size] }
-    arr2 = arr.find_all{|a| a[1]>0 || a[2]>1} #可信用户签到，或者有两人以上签到
-    return false unless arr2.length>0
-    puts self._id
-    arr2.each {|a| a[3]=CheckinShopStat.find_by_id(a[0])}
-    arr2 = arr2.find_all {|a| a[3]!=nil}
-    arr2.each {|a| a[3]=a[3].ctotal}
-    arr2.each {|a| a[4]=a[1]*20+a[2]*10+a[3]} #积分
-    arr2 = arr2.find_all {|a| a[4]>25}
-    if arr2.size==0   
-      puts "没有商家达到25分的最低标准" 
-      return false
-    end
-    if arr2.size==1
-      puts "唯一商家: #{Shop.find_by_id(arr2[0][0]).name}"
-      self.update_attribute(:shop_id, arr2[0][0])
-      return true
-    end
-    arr2.sort! {|a,b| b[4]<=>a[4]}
-    if arr2[0][4]>arr2[1][4]+2
-      puts arr2
-      puts "积分最大商家: #{Shop.find_by_id(arr2[0][0]).name}"
-      self.update_attribute(:shop_id, arr2[0][0])
-      return true
-    else
-      puts "积分基本相等商家: #{Shop.find_by_id(arr2[0][0]).name}，#{Shop.find_by_id(arr2[1][0]).name}"
-      return false
-    end
-  end
-  
-  def self.init_bssid
-    Checkin.where({bssid:{"$exists" => true}}).each do |x|
-      CheckinBssidStat.insert_checkin(x)
-    end
-  end
-  
   def self.insert_checkin(ck,ssid)
     CheckinBssidStat.insert(ck["bssid"],ck["sid"],ck["uid"],ssid)
   end
   
   def self.insert(bssid,sid,uid,ssid)
-    b = CheckinBssidStat.where({"_id" => bssid}).first
+    return if bssid.nil? || bssid.size<10
+    b = CheckinBssidStat.find_by_id(bssid)
     return if b && b.mobile
-    add_bssid_redis(bssid,sid)
     if b.nil?
-      CheckinBssidStat.collection.insert({"_id" => bssid, "ssid" => ssid, "shops" => [{id:sid,users:[uid]}] })
-    else
-      b.update_attribute(:ssid, ssid) if b.ssid.nil? && ssid
-      return if b.shop_id
-      shops = b.shops
-      shop = shops.find{|x| x["id"]==sid}
-      if shop.nil?
-        b.push(:shops, {id:sid,users:[uid]} )
-        b.update_attribute(:mobile,true) if b.is_mobile_wifi
-      elsif shop["users"].size<5
-        shop["users"] = shop["users"].to_set.add(uid).to_a
-        b.shops = shops
-        b.save!
-        b.try_set_shop_id if shop["users"].size>1
-      end
+      b = CheckinBssidStat.new
+      b._id = bssid
+      b.ssid = ssid
+      b.mobile = true if b.is_mobile_wifi
+      b.save!
     end
+    add_bssid_redis(bssid,sid)
   end
   
   def is_mobile_wifi
@@ -103,10 +43,17 @@ class CheckinBssidStat
     return false
   end
   
+  def shop_ids
+    $redis.zrange("BSSID#{_id}",0,-1)
+  end
+  
+  def shops
+    shop_ids.map{|x| Shop.find_by_id(x)}.find_all{|x| x!=nil}
+  end
+  
   def print_shop_distance
-    ss = shops.map {|x| Shop.find_by_id(x["id"])}
-    ss = ss.find_all{|x| x!=nil}
     puts "#{ssid}, #{_id}, #{shop_id}"
+    ss = shops
     ss.each_with_index do |x,i| 
       dis = x.shop_distance(ss[i-1])
       puts "#{dis}\t #{x.name},#{x.city},#{x.city_fullname}"
@@ -115,8 +62,7 @@ class CheckinBssidStat
   end
   
   def shop_distance_large_than(distance)
-    ss = shops.map {|x| Shop.find_by_id(x["id"])}
-    ss = ss.find_all{|x| x!=nil}
+    ss = shops
     ss.each_with_index do |x,i| 
       return true if x.shop_distance(ss[i-1])>distance
     end
@@ -125,13 +71,25 @@ class CheckinBssidStat
   
   def self.init_bssid_redis
     Checkin.where({bssid:{"$exists" => true}}).each do |ck|
+      next if ck.del
       add_bssid_redis(ck["bssid"],ck["sid"])
     end
   end
   
-  def self.add_bssid_redis(bssid,sid)
-    $redis.sadd("BSSID#{bssid}",sid.to_i)
+  def self.add_bssid_redis(bssid,sid,inc=1)
+    score = $redis.zscore("BSSID#{bssid}",sid.to_i).to_i
+    $redis.zadd("BSSID#{bssid}",score+inc, sid.to_i)
   end  
+  
+  def self.init_mobile_flag
+    CheckinBssidStat.where({"mobile" => {"$ne" => true}}).each do |cbs|
+      if cbs.is_mobile_wifi
+        puts cbs.to_json
+        cbs.set(:mobile, true) 
+      end
+    end
+    ""
+  end
   
 end
 
