115207d2c457e3d25561e924c5ef90e8b05852ed
pulsen <huang123qwe@126.com>
2013-08-07 10:47:40 +0800

    商家看点评

--- app/models/shop_mark.rb
+++ app/models/shop_mark.rb
@@ -6,6 +6,23 @@ class ShopMark
   field :gid, type: Moped::BSON::ObjectId #旅行团
   field :mark, type: Integer #评价得分
   field :com #评语
-  field :com_at,type: DateTime #评价时间
+
+  with_options :prefix => true, :allow_nil => true do |option|
+    option.delegate :name, :to => :user
+    option.delegate :name, :to => :shop
+    option.delegate :name, :to => :group
+  end
+
+  def shop
+    Shop.find_by_id(sid)
+  end
+
+  def user
+    User.find_by_id(uid)
+  end
+
+  def group
+    Group.find_by_id(gid)
+  end
 
 end
