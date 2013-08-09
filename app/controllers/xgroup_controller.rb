867af894cb7b647ea6d50158563b8b80af960cab
pulsen <huang123qwe@126.com>
2013-08-09 17:53:22 +0800

    接口

--- app/controllers/xgroup_controller.rb
+++ app/controllers/xgroup_controller.rb
@@ -1,7 +1,19 @@
+# coding: utf-8
 class XgroupController < ApplicationController
 
   def create
-
+    errmsg = blank_check(:id, :data)
+    return render :json => {error: errmsg} if errmsg
+    return render :json => {error: "id已有"} if Line.find_by_id(params[:id])
+    data = params[:data]
+    begin
+      group = Group.new(name: data['name'], line_id: data['line_id'], fat: data['fat'], tat: data['tat'], code: data['tid'], users: data['users'],admin_sid: 21834762 )
+      group.id = params[:id]
+      group.save
+    rescue
+      render :json => {error: 'data数据格式错误'}
+    end
+    render :json => {ok: group.id}
   end
 
 
@@ -9,4 +21,10 @@ class XgroupController < ApplicationController
     
   end
 
+  
+
+  def blank_check(*arr)
+    arr.each{|a| return "#{a.to_s}必填" if params[a].blank?}
+    return nil
+  end
 end
