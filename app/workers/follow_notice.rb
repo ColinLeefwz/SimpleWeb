5e0fefacec61ef045a467cb756976f4f5f85e22b
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-08-10 10:59:10 +0800

    处理短时间内重复加关注

--- app/workers/follow_notice.rb
+++ app/workers/follow_notice.rb
@@ -4,12 +4,12 @@ class FollowNotice
   @queue = :xmpp
 
   def self.perform(uid,tid)
-    UserFollow.find_or_new(uid, tid)
+    return unless UserFollow.find_or_new(uid, tid)
     user = User.find_by_id(uid)
     user = User.find_primary(uid) if user.nil?
     loc = User.last_loc_cache(tid)
     loc = User.last_loc_cache(uid) if loc.nil?
-    Xmpp.send_chat(uid,tid,": #{user.name}在#{loc[1]}看到并关注了你噢~")
+    Xmpp.send_chat(uid,tid,": #{user.name}在#{loc[1]}看到并关注了你噢~", "#{uid}#{tid}") #多次关注消息ID一致，防止消息重发
   end
   
 end
