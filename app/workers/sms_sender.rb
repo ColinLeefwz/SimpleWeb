8c5d66bd2e8ce4c90eede4e0b48c5cd0e70a3a6d
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-08-10 15:59:16 +0800

    重构伪号码000的判断

--- app/workers/sms_sender.rb
+++ app/workers/sms_sender.rb
@@ -9,7 +9,6 @@ class SmsSender
   
   def self.send_sms_ihuiyi(phone, text)
     return true if ENV["RAILS_ENV"] != "production"
-    return true if phone[0,3]=="000"
     begin
       pass = URI.escape("www.dface.cn20130709")
       info = RestClient.get "http://106.ihuyi.com/webservice/sms.php?method=Submit&account=cf_llh&password=#{pass}&mobile=#{phone}&content=#{URI.escape(text)}"
