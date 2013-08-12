558487a12d3a00a7cdd9c55a491ef5bd6acdd40b
pulsen <huang123qwe@126.com>
2013-08-12 11:41:51 +0800

    旅行社接口

--- app/controllers/xgroup_controller.rb
+++ app/controllers/xgroup_controller.rb
@@ -4,21 +4,28 @@ class XgroupController < ApplicationController
   def create
     errmsg = blank_check(:id, :data)
     return render :json => {error: errmsg} if errmsg
-    return render :json => {error: "id已有"} if Line.find_by_id(params[:id])
+    return render :json => {error: "id已有"} if Group.find_by_id(params[:id])
     data = params[:data]
     begin
       group = Group.new(name: data['name'], line_id: data['line_id'], fat: data['fat'], tat: data['tat'], code: data['tid'], users: data['users'],admin_sid: 21834762 )
       group.id = params[:id]
-      group.save
+      group.gen_shop
+      group.save!
     rescue
-      render :json => {error: 'data数据格式错误'}
+      return render :json => {error: 'data数据格式错误'}
     end
     render :json => {ok: group.id}
   end
 
 
   def find
-    
+    return render :json => {error: "id必填"} if params[:id].blank?
+    group = Group.find_by_id(params[:id])
+    if group
+      render :json => {data: group.attributes}
+    else
+      render :json => {error: "id不存在"}
+    end
   end
 
   
