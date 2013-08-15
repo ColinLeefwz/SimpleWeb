fa1a08aa861aabbf878b0133c4b15bd3a5ca7b34
web2 <web2@dface.cn>
2013-08-15 14:31:51 +0800

    语音聊天

--- app/controllers/sound2s_controller.rb
+++ app/controllers/sound2s_controller.rb
@@ -1,5 +1,5 @@
 class Sound2sController < ApplicationController
-  before_filter :user_login_filter
+  #before_filter :user_login_filter
   
   def uptoken
     token = Sound2.uptoken(params[:user_id])
@@ -7,12 +7,16 @@ class Sound2sController < ApplicationController
   end
   
   def callback
-    Sound2.callback
-    render :json => {sec:params[:sec]}.to_json
+    snd = Sound2.new
+    #snd._id = params[:key]
+    snd.sec = params[:sec]
+    snd.save!
+    snd.after_async_store
+    render :json => request.params.to_json
   end
 
   def show
-    redirect_to Sound2.img_url(params[:id])
+    redirect_to "http://sound.qiniudn.com/#{params[:id]}"
   end
 
 end
