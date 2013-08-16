6c082448c0ed19dfe3c9188dfe1a0b3fbfab0b89
tony <tony_doofy@163.com>
2013-08-16 14:31:38 +0800

    商家后台改版之签到管理

--- app/helpers/shop_checkins_helper.rb
+++ app/helpers/shop_checkins_helper.rb
@@ -10,4 +10,12 @@ module ShopCheckinsHelper
     end
   end
 
+  def shop3_ban_link(uid)
+    if session_shop.ban_user(uid)
+      link_to '解除屏蔽', "/shop3_checkins/unban?uid=#{uid}&page=#{params[:page]}&name=#{params[:name]}"
+    else
+      link_to '屏蔽', "/shop3_checkins/do_ban?uid=#{uid}&page=#{params[:page]}&name=#{params[:name]}"
+    end
+  end
+
 end
