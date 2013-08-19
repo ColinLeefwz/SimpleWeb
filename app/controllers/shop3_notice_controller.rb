5fe6ef60c449f5bc0bbb0422bbfad25a71fe46f7
pulsen <huang123qwe@126.com>
2013-08-19 14:50:23 +0800

    用户评分

--- app/controllers/shop3_notice_controller.rb
+++ app/controllers/shop3_notice_controller.rb
@@ -8,4 +8,11 @@ class Shop3NoticeController < ApplicationController
     @shop_notice = session_shop.notice
   end
 
+  def ajax_release
+    @shop_notice = ShopNotice.find_or_new(params[:id])
+    @shop_notice.title = params[:title]
+    @shop_notice.shop_id = session_shop.id
+    @shop_notice.save
+    render :json => {:title => @shop_notice.title, :id => @shop_notice.id }
+  end
 end
