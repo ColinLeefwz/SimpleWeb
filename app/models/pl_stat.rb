62a5566c0176174dd58fd200f095003332defe1a
tony <tony_doofy@163.com>
2013-08-19 10:04:35 +0800

    统计修改

--- app/models/pl_stat.rb
+++ app/models/pl_stat.rb
@@ -6,26 +6,26 @@ class PlStat
   field :pl_words_num, type:Array
 
   #计算一天内陪聊人员与多少人聊过
-  def chat_people_num
+  def chat_people_num(date)
     pl_people = ["51418836c90d8bc37b000567","513ed1e7c90d8b590100016f","50bec2c1c90d8bd12f000086",
                "51427b92c90d8b670c00027b","514190f8c90d8bc67b00054a"]
     num = []
     (0..pl_people.length-1).each do |x|
-      utnl = User.find_by_id(pl_people[x]).user_talk_new.length
+      utnl = User.find_by_id(pl_people[x]).user_talk_new(date).length
       num.push(utnl)
     end
     return num
   end
 
   #计算在一天内陪聊用户聊天话的数量
-  def chat_words_num
+  def chat_words_num(date)
     pl_people = ["51418836c90d8bc37b000567","513ed1e7c90d8b590100016f","50bec2c1c90d8bd12f000086",
                "51427b92c90d8b670c00027b","514190f8c90d8bc67b00054a"]
-    num = User.find_by_id(pl_people[n]).user_talk_new.length
+    # num = User.find_by_id(pl_people[n]).user_talk_new.length
     num = []
     sum = 0
     (0..pl_people.length-1).each do |y|
-      utnl = User.find_by_id(pl_people[y]).user_talk_new.length
+      utnl = User.find_by_id(pl_people[y]).user_talk_new(date).length
       (0..utnl-1).each do |x|
         sum2 = @pl1.human_chat(User.find_by_id(@pl1.chat[x][0]).id).length
         sum += sum2
@@ -35,20 +35,20 @@ class PlStat
   end
 
   #计算当前pl_stat, 并保存到mongo中
-  def do_count(data)
-  	user_talk_new(date)
-    self.data = data
-    self.pl_people_num = chat_people_num
-    self.pl_reply_num = chat_people_num
-    self.pl_words_num = chat_words_num
+  def do_count(date)
+  	# user_talk_new(date)
+    self.date = date
+    self.pl_people_num = chat_people_num(date)
+    self.pl_reply_num = chat_people_num(date)
+    self.pl_words_num = chat_words_num(date)
     self.save
   end
 
   #从缓存中获取当日陪聊信息
-  def self.user_talk_new(date)
-    content = $redis.smembers("PL#{date}#{self.id}")
-    return content
-  end
+  # def self.user_talk_new(date)
+  #   content = $redis.smembers("PL#{date}#{self.id}")
+  #   return content
+  # end
 
   #类方法，cron的调用接口，每天执行一次
   def self.do_count(date = nil)
