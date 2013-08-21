30d9bd01452b824c6b7ca36060c8a26d9149b53d
tony <tony_doofy@163.com>
2013-08-21 14:30:37 +0800

    陪聊统计需求修改

--- app/models/pl_stat.rb
+++ app/models/pl_stat.rb
@@ -17,11 +17,29 @@ class PlStat
     return num
   end
 
+  #计算一天内回复人数
+  def chat_reply_num(date)
+    pl_people = ["51418836c90d8bc37b000567","513ed1e7c90d8b590100016f","50bec2c1c90d8bd12f000086",
+               "51427b92c90d8b670c00027b","514190f8c90d8bc67b00054a"]
+    num = []
+    (0..pl_people.length-1).each do |y|
+      pl = User.find_by_id(pl_people[y])
+      utnl = pl.user_talk_new(date).length
+      (0..utnl-1).each do |x|
+        sum2 = pl.human_chat(User.find_by_id(pl.chat[x][0]).id).length
+        if sum2 < 2
+          utnl = utnl - 1
+        end
+      end
+      num.push(utnl)
+    end
+    num
+  end
+
   #计算在一天内陪聊用户聊天话的数量
   def chat_words_num(date)
     pl_people = ["51418836c90d8bc37b000567","513ed1e7c90d8b590100016f","50bec2c1c90d8bd12f000086",
                "51427b92c90d8b670c00027b","514190f8c90d8bc67b00054a"]
-    # num = User.find_by_id(pl_people[n]).user_talk_new.length
     num = []
     (0..pl_people.length-1).each do |y|
       sum = 0
@@ -38,24 +56,17 @@ class PlStat
 
   #计算当前pl_stat, 并保存到mongo中
   def do_count(date)
-  	# user_talk_new(date)
     self.date = date
     self.pl_people_num = chat_people_num(date)
-    self.pl_reply_num = chat_people_num(date)
+    self.pl_reply_num = chat_reply_num(date)
     self.pl_words_num = chat_words_num(date)
     self.save
   end
 
-  #从缓存中获取当日陪聊信息
-  # def self.user_talk_new(date)
-  #   content = $redis.smembers("PL#{date}#{self.id}")
-  #   return content
-  # end
-
   #类方法，cron的调用接口，每天执行一次
   def self.do_count(date = nil)
     if date.nil?
-      date = 1.days.ago.strftime("%Y-%m-%d")
+      date = 2.days.ago.strftime("%Y-%m-%d")
   	end
     ua = self.new
     ua.do_count(date)
