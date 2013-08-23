0e7dca98c952a108c824389337f8fca47a99ff7c
tony <tony_doofy@163.com>
2013-08-23 11:47:06 +0800

    图片上传预览js换成2，避免与原后台

--- public/javascripts/imgUploadPreview.js
+++ public/javascripts/imgUploadPreview.js
@@ -2,7 +2,7 @@
  *  图片上传预览， select文件选择框， divid： 图片要预览的id
  **/
 
-function imgUploadPreview(select, divid){
+function imgUploadPreview2(select, divid){
 
     $(select).change(function(){
         var picPath =  this.value
@@ -38,6 +38,45 @@ function imgUploadPreview(select, divid){
 
         }catch (e) {
             //alert("游览器不支持预览图片")
+        }
+    });
+}
+
+
+function imgUploadPreview(select, divid){
+    
+    $(select).change(function(){
+        var picPath =  this.value
+        var type = picPath.substring(picPath.lastIndexOf(".") + 1, picPath.length).toLowerCase();
+        if (type != "jpg" && type != "png") {
+            $(this).val('')
+            alert("请上传jpg/png格式图片");
+            return false;
+        }
+        
+        try{
+            var divObj = document.getElementById(divid)
+            if(navigator.userAgent.indexOf("MSIE") > -1&& (navigator.userAgent.indexOf("MSIE 7.0") > -1|| navigator.userAgent.indexOf("MSIE 9.0") > -1 || navigator.userAgent.indexOf("MSIE 8.0") > -1)  ){
+                this.select();
+                this.blur();
+                var path = document.selection.createRange().text;
+                document.selection.empty();
+                path = "file:///" + path.replace("\\",'/');
+                divObj.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod='scale',src='"+ path + "')";
+            }
+            else if(navigator.userAgent.indexOf("MSIE") > -1&& navigator.userAgent.indexOf("MSIE 6.0") > -1 ){
+                divObj.innerHTML = "<img id='image_view' style='height: 197px; width: 194px' />";
+                var imageView = document.getElementById("image_view");
+                imageView.src = $(this).val();
+            }
+            else{
+                divObj.innerHTML = "<img id='image_view' style='height: 197px; width: 194px' />";
+                imageView = document.getElementById("image_view");
+                imageView.src = window.URL.createObjectURL(this.files[0]);
+            }
+
+        }catch (e) {
+            alert("游览器不支持预览图片")
         }
     });
 }
