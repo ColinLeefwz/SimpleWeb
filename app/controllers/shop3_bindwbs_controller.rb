be0e92e238ab0d73fbe80f10d93ed7f5bbc2e2fd
pulsen <huang123qwe@126.com>
2013-08-28 14:07:05 +0800

    新版微博绑定

--- app/controllers/shop3_bindwbs_controller.rb
+++ app/controllers/shop3_bindwbs_controller.rb
@@ -5,17 +5,22 @@ class Shop3BindwbsController < ApplicationController
   layout 'shop3'
 
   def index
-    @bind_wb = BindWb.find2(session[:shop_id]) || BindWb.new
+    @bind_wb = BindWb.find_by_id(session[:shop_id]) || BindWb.new
   end
 
   def create
     @bind_wb = BindWb.new(params[:bind_wb])
     @bind_wb._id = session[:shop_id]
+
+    if (error = @bind_wb.check_wb)
+      flash.now[:notice] = error
+      return  render :action => 'index'
+    end
+    
     if @bind_wb.save
       redirect_to :action => "index"
     else
        flash.now[:notice] == '绑定失败,请检查微博uid和昵称的正确性.'
-       render :action => 'index'
     end
   end
 
--- app/models/bind_wb.rb
+++ app/models/bind_wb.rb
@@ -7,23 +7,22 @@ class BindWb
   field :wb_uid, type: Integer #微博uid
   field :val, type:Boolean, default:false #是否验证过
 
-  validates_presence_of :wb_uid, :message => "微博uid必填."
-  validates_presence_of :login_name, :message => "微博登录名必填."
-  validates_presence_of :name, :message => "微博昵称必填."
 
-
-
-  validate :check_wb
   def check_wb
+    return "微博登录名必填" if login_name.blank?
+    return "微博uid必填" if wb_uid.blank?
+    return "微博昵称必填" if name.blank?
+
     url = "https://api.weibo.com/2/users/show.json?access_token=#{$sina_token}&uid=#{self.wb_uid}"
     begin
       response = JSON.parse(RestClient.get(url))
-      self.errors.add(:wu_uid, '微博uid和微博昵称不匹配.')  if response["screen_name"] != self.name
+      return '微博uid和微博昵称不匹配'  if response["screen_name"] != self.name
       self.val = true
     rescue RestClient::BadRequest
-      self.errors.add(:wu_uid, '微博uid可能不存在.')
+      return  '微博uid可能不存在'
     rescue
     end
+    return nil
   end
   
   def shop
@@ -32,16 +31,6 @@ class BindWb
 
   def weibo_home
     "http://www.weibo.com/#{wb_uid}" if wb_uid
-  end
-
-
-
-  def self.find2(id)
-    begin
-      self.find(id)
-    rescue
-      nil
-    end
   end
   
   # 根据商家id获得其企业微博的名字，或者是主店的企业微博的名字
