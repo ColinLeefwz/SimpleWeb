8125d1d2942df6c85a73f9c47efd2184c23f0d03
aaa <aaa@aaa.(none)>
2013-08-28 13:47:21 +0800

    m

--- public/javascripts/imgUploadPreview.js
+++ public/javascripts/imgUploadPreview.js
@@ -81,3 +81,47 @@ function imgUploadPreview(select, divid){//优惠券
         }
     });
 }
+
+function imgUploadPreview3(select, divid){//商家设置
+    $(select).change(function(){
+        var picPath =  this.value
+        var type = picPath.substring(picPath.lastIndexOf(".") + 1, picPath.length).toLowerCase();
+        if (type != "jpg" && type != "png") {
+            $(this).val('')
+            alert("请上传jpg/png格式图片");
+            return false;
+        }
+        
+        try{
+            var divObj = document.getElementById(divid)
+            if(navigator.userAgent.indexOf("MSIE") > -1&& (navigator.userAgent.indexOf("MSIE 7.0") > -1|| navigator.userAgent.indexOf("MSIE 9.0") > -1 || navigator.userAgent.indexOf("MSIE 8.0") > -1)  ){
+				this.select();
+                this.blur();
+                var path = document.selection.createRange().text;
+                document.selection.empty();
+                path = "file:///" + path.replace("\\",'/');
+                document.getElementById("image_view").style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod='scale',src='"+ path + "')";
+				document.getElementById("image_view").src ="http://shop.dface.cn/images/clear.gif";
+				document.getElementById("image_view2").style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod='scale',src='"+ path + "')";
+				document.getElementById("image_view2").src ="http://shop.dface.cn/images/clear.gif";
+			}
+            else if(navigator.userAgent.indexOf("MSIE") > -1&& navigator.userAgent.indexOf("MSIE 6.0") > -1 ){
+                divObj.innerHTML = "<img id='image_view' class='imgstyle2'/><img id='image_view2' class='imgstyle3 ml15 mb75'/>";
+                var imageView = document.getElementById("image_view");
+				var imageView2 = document.getElementById("image_view2");
+                imageView.src = $(this).val();
+				imageView2.src = $(this).val();
+            }
+            else{
+                divObj.innerHTML = "<img id='image_view' class='imgstyle2'/><img id='image_view2' class='imgstyle3 ml15 mb75'/>";
+                imageView = document.getElementById("image_view");
+				imageView2 = document.getElementById("image_view2");
+                imageView.src = window.URL.createObjectURL(this.files[0]);
+				imageView2.src = window.URL.createObjectURL(this.files[0]);
+            }
+
+        }catch (e) {
+            //alert("游览器不支持预览图片")
+        }
+    });
+}
