8f084e91278e173315fae2612df1caee5717d2b9
JevanWu <jevanwu@gmail.com>
2013-08-30 17:07:07 +0800

    delete unnecessary files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery.min.js
 //= require jquery_ujs
 //= require turbolinks
+//= require scrolltopcontrol.js
 //= require masonry.pkgd.min.js
 //= require_tree .
--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -12,22 +12,6 @@ $(document).ready(function(){
 
   $(window).scroll(function()
   {
-//     alert(""+$(window).scrollTop());
-//     alert($(document).height());
-//     alert($(window).height());
+
   });
 });
-	
-  // $("img.lazy").lazyload({
-  //   effect : "fadeIn"
-  // });
-
-  // (function(d, s, id) {
-  //   var js, fjs = d.getElementsByTagName(s)[0];
-  //   if (d.getElementById(id)) return;
-  //   js = d.createElement(s); js.id = id;
-  //   js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
-  //   fjs.parentNode.insertBefore(js, fjs);
-  // }(document, 'script', 'facebook-jssdk'));
-
-
