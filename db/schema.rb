fa7c5db97990baf1b3d1e01f80cfafae551d7259
Gecko-Gecko <zjut.fmr@gmail.com>
2013-09-10 17:36:43 +0800

    Rollback TinyMCE editor

--- db/schema.rb
+++ db/schema.rb
@@ -84,8 +84,6 @@ ActiveRecord::Schema.define(version: 20130910044701) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.integer  "rolable_id"
     t.string   "rolable_type"
   end
