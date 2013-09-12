2fdd633f2d9b5a7ed24b9ae4af9c12333d29e985
web2 <web2@dface.cn>
2013-09-12 11:40:52 +0800

    m

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -55,18 +55,17 @@ class ShopController < ApplicationController
     lo = [params[:lat].to_f, params[:lng].to_f]
     lo = Shop.lob_to_lo(lo) if params[:baidu].to_i==1
     if params[:sname].length<3
-      radis = 0.002 + params[:sname].length*0.0005
-      shops = Shop.where({lo:{"$within" => {"$center" => [lo,radis]}}, name:/#{params[:sname]}/}).limit(10)
-      #TODO: 增加缓存，key为经纬度加查询关键字
+      radis = 0.01 + params[:sname].length*0.002
+      shops = Shop.where2({lo:{"$within" => {"$center" => [lo,radis]}}, name:/#{params[:sname]}/}, {limit:10})
       render :json =>  shops.map {|s| {id:s.id,name:s.name, visit:0}.merge!(s.group_hash(session[:user_id])) }.to_json
     else
       ret = []
-      shop = Shop.where({t:0, name:params[:sname]}).first #虚拟的活动地点名称完全匹配时可以进入
-      if shop
-        hash = {id:shop.id,name:shop.name, visit:0}.merge!(shop.group_hash(session[:user_id])) 
-        ret << hash
-      end
-      shop1s = Shop.where({lo:{"$within" => {"$center" => [lo,0.01]}}, name:/#{params[:sname]}/}).limit(10)
+      #shop = Shop.where({t:0, name:params[:sname]}).first #虚拟的活动地点名称完全匹配时可以进入
+      #if shop
+      #  hash = {id:shop.id,name:shop.name, visit:0}.merge!(shop.group_hash(session[:user_id])) 
+      #  ret << hash
+      #end
+      shop1s = Shop.where2({lo:{"$within" => {"$center" => [lo,0.015]}}, name:/#{params[:sname]}/},{limit:10})
       shop1s.each do |s| 
         hash = {id:s.id,name:s.name, visit:0}.merge!(s.group_hash(session[:user_id]))
         ret << hash
