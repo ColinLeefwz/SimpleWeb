1d075acdc9a360a8d4443301babb2b601f737628
Gecko-Gecko <zjut.fmr@gmail.com>
2013-09-16 17:30:06 +0800

    Customize Expert admin page (click on Expert's name to see details)

--- app/admin/expert.rb
+++ app/admin/expert.rb
@@ -1,4 +1,20 @@
 ActiveAdmin.register Expert do
+
+  index do
+    column :name do |expert|
+      link_to(expert.name, admin_expert_path(expert))
+    end
+    column "Avatar", :image_url do |expert|
+      image_tag(expert.image_url, size: "50x50")
+    end
+    column :title
+    column :company
+    column :location
+    column :email
+
+  end
+
+
   controller do
     def permitted_params
       params.permit expert: [:name, :title, :company, :location, :expertise, :favorite_quote, :career, :education, :web_site, :article_reports, :speeches, :additional, :testimonials]
