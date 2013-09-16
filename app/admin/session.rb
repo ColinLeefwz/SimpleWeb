45edd81be203e823cf6cd681687961ee5336e94b
Gecko-Gecko <zjut.fmr@gmail.com>
2013-09-16 18:06:30 +0800

    Customize session's admin page

--- app/admin/session.rb
+++ app/admin/session.rb
@@ -1,4 +1,24 @@
 ActiveAdmin.register Session do
+
+  index do
+    column :cover do |session|
+      image_tag(session.cover.url, size: "50x50")
+    end
+    column :title do |session|
+      link_to(session.title, admin_session_path(session))
+    end
+    column "Expert", :expert_id do |session|
+      link_to(session.expert.name, admin_expert_path(session.expert_id))
+    end
+    column :status
+    column :content_type
+    column :category
+    column :location
+    column :price, sortable: :price do |session|
+      number_to_currency(session.price, unit: "USD: ")
+    end
+  end
+
   controller do
     def permitted_params
       params.permit session: [:title, :expert_id, :created_date, :description]
