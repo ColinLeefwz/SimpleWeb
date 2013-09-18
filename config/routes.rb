52747e645db58c0fbf029459eab01634b1f54f21
JevanWu <jevanwu@gmail.com>
2013-09-18 17:28:27 +0800

    add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -15,12 +15,13 @@ Prodygia::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root to: "welcome#index"
 
-  get "/about_us", to: "welcome#about_us"
-  get "/faq", to: "welcome#faq"
-  get "/for_experts", to: "welcome#for_experts"
-  get "/for_members", to: "welcome#for_members"
-  get "/privacy", to: "welcome#privacy"
-  get "/terms", to: "welcome#terms"
+  get "/:page", to: 'static_pages#static'
+#  get "/about_us", to: "welcome#about_us"
+#  get "/faq", to: "welcome#faq"
+#  get "/for_experts", to: "welcome#for_experts"
+#  get "/for_members", to: "welcome#for_members"
+#  get "/privacy", to: "welcome#privacy"
+#  get "/terms", to: "welcome#terms"
   get "video_page/:id", to: "welcome#video_page", as: 'video_page'
   get "text_page/:id", to: "welcome#text_page", as: 'text_page'
  end
