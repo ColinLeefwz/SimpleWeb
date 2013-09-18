27fb92cdc0ff547b526d3737e447c62df9468075
peterzd.work <zdsunshine0640@126.com>
2013-09-18 19:10:52 +0800

    working on admin/expert update function

--- app/admin/expert.rb
+++ app/admin/expert.rb
@@ -1,35 +1,35 @@
 ActiveAdmin.register Expert do
 
-  index do
+	index do
 		column :name do |expert|
 			link_to(expert.name, admin_expert_path(expert))
 		end
-    # column "Avatar", :image_url do |expert|
-    #   link_to image_tag(expert.avatar.url, width: "50"), admin_expert_path(expert)
-    # end
-    column :title do |e|
+		# column "Avatar", :image_url do |expert|
+		#   link_to image_tag(expert.avatar.url, width: "50"), admin_expert_path(expert)
+		# end
+		column :title do |e|
 			e.profile.title
 		end
 
-    column :company do |e|
+		column :company do |e|
 			e.profile.company
 		end
 
-    column :location do |e|
+		column :location do |e|
 			e.profile.location
 		end
 
-    column :email
-    default_actions
-  end
+		column :email
+		default_actions
+	end
 
 
-  form do |f|
-    f.inputs do
-      f.input :first_name
+	form do |f|
+		f.inputs do
+			f.input :first_name
 			f.input :last_name
-      # f.input :avatar, as: :file
-      f.input :email
+			# f.input :avatar, as: :file
+			f.input :email
 
 			f.inputs name: "Profiles", for: :profile do |profile_form|
 				profile_form.input :title
@@ -40,52 +40,66 @@ ActiveAdmin.register Expert do
 				profile_form.input :testimonials
 				profile_form.input :additional
 			end
-      f.actions
-    end
-  end
+			f.actions
+		end
+	end
 
 
-  show do |expert|
-    attributes_table do
-      row :name
-      # row :avatar do
-      #   image_tag expert.avatar.url, width: "70"
-      # end
-      row :email
-      row :title do |expert|
+	show do |expert|
+		attributes_table do
+			row :name
+			# row :avatar do
+			#   image_tag expert.avatar.url, width: "70"
+			# end
+			row :email
+			row :title do |expert|
 				expert.profile.title
 			end
 
-      row :company do |expert|
+			row :company do |expert|
 				expert.profile.company
 			end
 
-      row :location do |expert|
+			row :location do |expert|
 				expert.profile.location
 			end
 
-      row :expertise do |expert|
+			row :expertise do |expert|
 				expert.profile.location
 			end
 
-      row :web_site do |expert|
+			row :web_site do |expert|
 				expert.profile.location
 			end
 
-      row :testimonials do |expert|
+			row :testimonials do |expert|
 				expert.profile.location
 			end
 
-      row :additional do |expert|
+			row :additional do |expert|
 				expert.profile.location
 			end
-    end
-  end
+		end
+	end
 
 
-  controller do
-    def permitted_params
-      params.permit expert: [:name, :avatar, :title, :company, :location, :expertise, :favorite_quote, :career, :education, :web_site, :article_reports, :speeches, :additional, :testimonials]
-    end
-  end
+	controller do
+		def update
+			@expert = Expert.find params[:id]
+			@profile = ExpertProfile.find params[:expert][:profile_attributes][:id]
+
+			profile_attributes = params[:expert][:profile_attributes]
+			profile_attributes.delete :id
+			logger.info "profile attr is: #{profile_attributes}"
+			@profile.update_attributes title: profile_attributes[:title], company: profile_attributes[:company], location: profile_attributes[:location], expertise: profile_attributes[:expertise], web_site: profile_attributes[:web_site], testimonials: profile_attributes[:testimonials], additional: profile_attributes[:additional]
+			# @profile.update_attributes(profile_attributes)
+
+			redirect_to admin_expert_path @expert
+		end
+
+		def permitted_params
+			# params.permit expert: [:name, :avatar, :title, :company, :location, :expertise, :favorite_quote, :career, :education, :web_site, :article_reports, :speeches, :additional, :testimonials]
+			params.permit expert: [:name, :profile]
+		end
+	end
 end
