9571ba85a1fb01a99528a259cfe7080a78e6de64
Gecko-Gecko <zjut.fmr@gmail.com>
2013-09-23 17:08:00 +0800

    Customize CKEditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,3 +1,19 @@
-config.fontSize_style = {
-  element: 'span'
-}
+
+$(document).ready(function(){
+  CKEDITOR.editorConfig = function(config){
+    config.toolbar = 'Basic';
+
+    config.toolbar_Basic =
+    [
+      { name: 'document', items: ['Source', '-']},
+      { name: 'clipboard', items: ['Undo', 'Redo']},
+      { name: 'styles', items : [ 'Format' ] },
+      { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Superscript','-','RemoveFormat' ]},
+      { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','-','Blockquote','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock']},
+      { name: 'links', items : [ 'Link','Unlink'] },
+      { name: 'insert', items : [ 'Image','Table' ] },
+      { name: 'tools', items : [ 'Maximize', 'Templates','Scayt' ] }
+    ];
+  };
+
+});
