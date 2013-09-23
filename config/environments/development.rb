6f744d55900f3f8e45273c05825cb85511011b5b
peterzd.work <zdsunshine0640@126.com>
2013-09-23 16:20:16 +0800

    make devise work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,7 +23,7 @@ Prodygia::Application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   #config default url
-  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+	config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
