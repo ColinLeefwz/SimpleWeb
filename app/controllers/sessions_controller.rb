e4cf8837b3c2a61d422d590f8b8dcbaf440ed460
peterzd.work <zdsunshine0640@126.com>
2013-10-02 14:46:24 +0800

    extract Payment to Model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,3 @@
-require 'paypal-sdk-rest'
-include PayPal::SDK::REST
-include PayPal::SDK::Core::Logging
-
 class SessionsController < ApplicationController
 
 	before_action :set_session, only: [:show, :edit, :update, :destroy]
@@ -69,10 +65,10 @@ class SessionsController < ApplicationController
 		@order.user = current_user
 
 		if @order.save
-			@order.approve_url = create_payment_with_paypal(@session)
+			Paypal.create_payment_with_paypal(@session, @order, order_execute_url(@order.id))
 
 			if @order.approve_url
-				redirect_to @redirect_url
+				redirect_to @order.approve_url
 			else
 				redirect_to session_path(@session)
 			end
@@ -82,51 +78,5 @@ class SessionsController < ApplicationController
 
 	end
 
-	def create_payment_with_paypal(paid_session)
-		# price = paid_session.price.to_s
-		@payment = Payment.new({
-			:intent =>  "sale",
-
-			# ###Payer
-			# A resource representing a Payer that funds a payment
-			# Payment Method as 'paypal'
-			:payer =>  {
-				:payment_method =>  "paypal" },
-
-			# ###Redirect URLs
-			:redirect_urls => {
-				:return_url => order_execute_url(@order.id),
-				:cancel_url => "http://localhost:3000/" },
-
-			# ###Transaction
-			# A transaction defines the contract of a
-			# payment - what is the payment for and who
-			# is fulfilling it.
-			:transactions =>  [{
-
-				# Item List
-				:item_list => {
-					:items => [{
-						:name => paid_session.title,
-						# :sku => "item",
-						:price => '%.2f' % paid_session.price,
-						:currency => "USD",
-						:quantity => 1 }]},
-
-				# ###Amount
-				# Let's you specify a payment amount.
-				:amount =>  {
-					:total =>  '%.2f' % paid_session.price,
-					:currency =>  "USD" },
-					:description =>  "This is the payment transaction description." }]})
-
-
-		if @payment.create
-			@order.update_attributes payment_id: @payment.id
-
-			@redirect_url = @payment.links.find{|v| v.method == "REDIRECT" }.href
-		else
-		end
-	end
 end
 
