c8ea1cdcfe24e58824007019969c97bc9f5b75b0
peterzd.work <zdsunshine0640@126.com>
2013-10-02 14:02:31 +0800

    remove unuseful methods in welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,22 +3,5 @@ class WelcomeController < ApplicationController
   def index
     @sessions = Session.all
   end
-  
-  def about_us
-  end
-  
-  def faq
-  end
 
-  def for_experts
-  end
-
-  def for_members
-  end
-
-  def privacy
-  end
-
-  def terms
-  end 
 end
