8e814b1130c9239053f6393728cb09a590ac0ce3
peterzd.work <zdsunshine0640@126.com>
2013-10-02 14:09:38 +0800

    remove actions from experts controller

--- app/controllers/experts_controller.rb
+++ app/controllers/experts_controller.rb
@@ -1,59 +1,12 @@
 class ExpertsController < ApplicationController
   before_action :set_expert, only: [:show, :edit, :destroy, :update]
 
-  # GET /experts
-  # GET /experts.json
-  def index
-    @experts = Expert.all
-  end
-
-  # GET /experts/1
-  # GET /experts/1.json
-  def show
-  end
-
-  # GET /experts/new
-  def new
-    @expert = Expert.new
-  end
-
-  # GET /experts/1/edit
-  def edit
-  end
-
-  # POST /experts
-  # POST /experts.json
-  def create
-    @expert = Expert.new(expert_params)
-
-    if @expert.save
-      redirect_to @expert, notice: 'Expert was successfully created.'
-    else
-      render action: 'new'
-    end
-  end
-
-  # PATCH/PUT /experts/1
-  # PATCH/PUT /experts/1.json
-  def update
-    @expert.save
-    redirect_to @expert
-  end
-
-  # DELETE /experts/1
-  # DELETE /experts/1.json
-  def destroy
-    @expert.destroy
-    redirect_to experts_url
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_expert
       @expert = Expert.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def expert_params
       params.require(:expert).permit(:name, :avatar, :title, :company, :location, :expertise, :favorite_quote, :career, :education, :web_site, :article_reports, :speeches, :additional, :testimonials)
     end
