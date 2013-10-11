809e3c82ec1c08f9c72601f9d2ba202c5123b3cc
peterzd.work <zdsunshine0640@126.com>
2013-10-11 14:01:06 +0800

    make the sign-up email work.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 require 'paypal'
 require 'mandrill_api'
+require 'active_support'
 
 class SessionsController < ApplicationController
 
@@ -92,8 +93,15 @@ class SessionsController < ApplicationController
 	end
 
 	def send_mail
+    domain_url = request.base_url
+    if domain_url == "http://localhost:3000"
+      domain_url = "http://www.prodygia.com"
+    end
+
+    session_image_url = domain_url + @session.cover.url
     mandrill = MandrillApi.new
-    mandrill.template_send(current_user, @session, "enroll_comfirm")
+    mandrill.template_send(current_user, @session, "enroll_comfirm", session_image_url)
+
 	end
 
 end
--- lib/mandrill_api.rb
+++ lib/mandrill_api.rb
@@ -1,12 +1,17 @@
 require 'mandrill'
+require 'base64'
+
 class MandrillApi
   def initialize
     @mandrill = Mandrill::API.new ENV['MANDRILL_API']
   end
 
-  def template_send(user, session, template_name)
+  def template_send(user, session, template_name, session_image_url)
     template_content = [{"name" => "first-name", "content" => user.first_name}, {"name" => "session-title", "content" => session.title }, {"name" => "expert-name", "content" => session.expert.name}, {"name" => "start-date", "content" => session.start_date }]
+
     message = {
+      "merge_vars"=>
+      [{"rcpt"=>user.email, "vars"=>[{"name"=>"SESSIONIMAGE", "content"=>session_image_url}]}],
       "bcc_address"=>"message.bcc_address@example.com",
       "subject"=>"Session enrolled confirmation",
       "text"=>"Example text content",
