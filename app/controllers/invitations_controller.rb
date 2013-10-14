072fd9bc119b4d588ecfac45d9007b8740a10148
JevanWu <jevanwu@gmail.com>
2013-10-14 15:05:21 +0800

    generate an invitation_token instead by sending an email

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -3,10 +3,15 @@ class InvitationsController < Devise::InvitationsController
  #before_filter :is_admin
  
  def create 
-   self.resource = resource_class.invite!(invite_params, current_inviter)
+   self.resource = resource_class.invite!(invite_params, current_inviter) do |u|
+     u.skip_invitation = true
+   end
+
    if resource.errors.empty?
      set_flash_message :notice, :send_instructions, :email => self.resource.email if self.resource.invitation_sent_at
-     redirect_to admin_dashboard_url
+     @invitation_token = resource.invitation_token
+     render 'generated'
+     #redirect_to admin_dashboard_url
    else
      respond_with_navigational(resource) { render :new }
    end
@@ -21,7 +26,10 @@ class InvitationsController < Devise::InvitationsController
    expert.create_profile
    super
  end
- 
+
+ def generated
+ end 
+
  def is_admin
    if !current_user.is_a?Member
      redirect_to admin_experts_url
