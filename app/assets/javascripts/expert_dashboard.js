8579e3d3ba9f736400573d846921d6a2b97f2cb7
JevanWu <jevanwu@gmail.com>
2013-10-17 17:50:18 +0800

    refine expert dashboard side menu

--- app/assets/javascripts/expert_dashboard.js
+++ app/assets/javascripts/expert_dashboard.js
@@ -1,11 +1,12 @@
-document().read
-$(".item-text > a").click(function() {
-  $(".item").find(".item-icon").css("display", "inline-block");
-  $(this).parents(".item").find(".item-icon").css("display", "none");
-  $(".item").find(".item-icon-selected").css("display", "none");
-  $(this).parents(".item").find(".item-icon-selected").css("display", "inline-block");
-  $(".item").find(".item-text > a").css("color", "");
-  $(this).parents(".item").find(".item-text > a").css("color", "#880848");
+$(document).ready(function(){
+  $(".item-text > a").click(function() {
+    $(".item").find(".item-icon").css("display", "inline-block");
+    $(this).parents(".item").find(".item-icon").css("display", "none");
+    $(".item").find(".item-icon-selected").css("display", "none");
+    $(this).parents(".item").find(".item-icon-selected").css("display", "inline-block");
+    $(".item").find(".item-text > a").css("color", "");
+    $(this).parents(".item").find(".item-text > a").css("color", "#880848");
+  });
 });
 
 
