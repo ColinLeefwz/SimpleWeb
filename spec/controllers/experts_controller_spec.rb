41e0aac5a62c10b7a0a4c9c20ceab5c11c0ee550
Gecko-Gecko <zjut.fmr@gmail.com>
2013-10-21 20:13:53 +0800

    refact experts_controller_sprc.rb

--- spec/controllers/experts_controller_spec.rb
+++ spec/controllers/experts_controller_spec.rb
@@ -3,75 +3,80 @@ require 'spec_helper'
 describe ExpertsController do
   helper_objects
 
+
+  context "not logged in" do
+    it "can not access all dashboard page" do
+      expect{ get :dashboard, id: sameer.id }.to raise_error(CanCan::AccessDenied)
+      expect{ get :new_post_content, id: sameer.id }.to raise_error(CanCan::AccessDenied)
+      expect{ get :new_session, id: sameer.id}.to raise_error(CanCan::AccessDenied)
+    end
+  end
+  
+
+  context "logged in as member" do
+    before :each do
+      sign_in peter
+    end
+    it "can not access all dashboard page" do
+      expect{ get :dashboard, id: sameer.id }.to raise_error(CanCan::AccessDenied)
+      expect{ get :new_post_content, id: sameer.id }.to raise_error(CanCan::AccessDenied)
+      expect{ get :new_session, id: sameer.id}.to raise_error(CanCan::AccessDenied)
+    end
+  end
+
+
+  describe "expert can access all his dashboard functionalities" do
+    before :each do
+      sign_in sameer
+    end
+    it "can access his own dashboard main page" do
+      get :dashboard, id: sameer.id
+      expect(response).to render_template "dashboard"
+    end
+    it "can access new session page" do
+      get :new_session, id: sameer.id
+      expect(response).to render_template "new_session"
+    end
+  end
+  
+
+  describe "expert can not access other expert's dashboard" do
+    before :each do
+      sign_in sameer
+    end
+    it "can not access other expert's dashboard main page" do
+      expect{ get :dashboard, id: alex.id }.to raise_error(CanCan::AccessDenied)
+    end
+    it "can not access other expect's new session page" do
+      expect{ get :new_session, id: alex.id }.to raise_error(CanCan::AccessDenied)
+    end
+  end
+  
+
   describe "GET dashboard" do
-    context "not logged in" do
-      it "can not access the dashboard page" do
-        expect{ get :dashboard, id: sameer.id }.to raise_error(CanCan::AccessDenied)
-      end
-    end
-
-    context "logged in as member" do
-      before :each do
-        sign_in peter
-      end
-
-      it "can not access the dashboard page" do
-        expect{ get :dashboard, id: sameer.id }.to raise_error(CanCan::AccessDenied)
-      end
-    end
-
     context "logged in as expert" do
       before :each do
         sign_in sameer
       end
-
-      it "access his own dashboard page" do
-        get :dashboard, id: sameer.id
-        expect(response).to render_template "dashboard"
-      end
-
       it "assigns sessions" do
         get :dashboard, id: sameer.id
         expect(assigns[:sessions]).to eq sameer.sessions
       end
-
-      it "can not access other's dashboard page" do
-        expect{ get :dashboard, id: alex.id }.to raise_error(CanCan::AccessDenied)
-      end
     end
-
   end
 
+
+
   describe "GET new session" do
-
-    context "not logged in" do
-
-      it "can not access new session page" do
-        expect{ get :new_session, id: sameer.id }.to raise_error(CanCan::AccessDenied)
-        
-      end
-    end
-
     context "logged in as expert" do
       before :each do 
         sign_in sameer
       end
-
-      it "access new session page" do
-        get :new_session, id: sameer.id
-        expect(response).to render_template "new_session"
-      end
-
-      it "can not access other expert's new session page" do
-        expect{ get :new_session, id: alex.id }.to raise_error(CanCan::AccessDenied)
-      end
-
       it "assigns new session" do
         get :new_session, id: sameer.id
-        expect(assigns[:session]).to be_new_record
+        expect(assigns[:live_session]).to be_new_record
       end
     end
-    
   end
 
 end
