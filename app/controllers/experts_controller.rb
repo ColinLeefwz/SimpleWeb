ba5096db87c5049681fd9baee213fdc416fce618
Gecko-Gecko <zjut.fmr@gmail.com>
2013-10-22 16:48:47 +0800

    [dashboard] enable live_session create and save_as_draft

--- app/controllers/experts_controller.rb
+++ app/controllers/experts_controller.rb
@@ -2,15 +2,15 @@ class ExpertsController < ApplicationController
   load_and_authorize_resource
 
   def dashboard
-    @sessions = @expert.sessions
+    @sessions = @expert.sessions.order("draft desc")
   end
 
   def new_post_content
-    @article_session = ArticleSession.new
+    @article_session = Session.new  # use Session.new so that form params are wrapped in :session
   end
 
   def create_post_content
-    @article_session = ArticleSession.new(article_params)
+    @article_session = ArticleSession.new(session_params)
     @article_session.expert = current_user
 
     if @article_session.save
@@ -20,13 +20,35 @@ class ExpertsController < ApplicationController
     end
   end
   
-  def new_session
+  def new_live_session
     @live_session = Session.new
   end
+
+
+  def create_live_session
+    @live_session = LiveSession.new(session_params)
+    @live_session.expert = current_user
+
+    case params[:commit]
+    when "Save Draft"
+      @live_session.draft = true
+      notice = "Draft Saved"
+    when "Preview"
+    when "Submit"
+      notice = "successful"
+    end
+
+    if @live_session.save
+      redirect_to dashboard_expert_path(current_user), notice: notice
+    else
+      redirect_to dashboard_expert_path(current_user), notice: 'failed'
+    end
+  end
+
   
   private
-    def article_params
-      params.require(:article_session).permit(:title, :description, :cover, {categories:[]} )
+    def session_params
+      params.require(:session).permit(:title, :description, :cover, :video, {categories:[]}, :location, :price, :language, :start_date ) 
     end
 
     def expert_params
