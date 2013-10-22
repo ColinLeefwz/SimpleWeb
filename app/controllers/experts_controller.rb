c78ec5a51cc5e80a4bb27d77aed05d8e90489411
JevanWu <jevanwu@gmail.com>
2013-10-22 17:31:58 +0800

    redirect to session detail page when click 'preview'

--- app/controllers/experts_controller.rb
+++ app/controllers/experts_controller.rb
@@ -12,12 +12,19 @@ class ExpertsController < ApplicationController
   def create_post_content
     @article_session = ArticleSession.new(article_params)
     @article_session.expert = current_user
-
-    if @article_session.save
-      redirect_to dashboard_expert_path(current_user), notice: 'successful'
-    else
-      redirect_to dashboard_expert_path(current_user), notice: 'failed'
+    case params[:commit]
+    when "Save draft"
+      @article_session.draft = true
+      save_session(@article_session)
+    when "Preview"
+      @article_session.draft = true   
+      if @article_session.save
+        redirect_to session_path(@article_session)
+      end
+    when "Createe post content" 
+      save_session(@article_session)
     end
+
   end
   
   def new_session
@@ -33,4 +40,11 @@ class ExpertsController < ApplicationController
       params.require(:expert).permit(:name, :avatar, :title, :company, :location, :expertise, :favorite_quote, :career, :education, :web_site, :article_reports, :speeches, :additional, :testimonials)
     end
 
+    def save_session(session)
+      if session.save
+        redirect_to dashboard_expert_path(current_user), notice: 'successful'
+      else
+        redirect_to dashboard_expert_path(current_user), notice: 'failed'
+      end
+    end
 end
