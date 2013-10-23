9609d9282f4d20e622eeab1f41414d80df6f48d4
peterzd.work <zdsunshine0640@126.com>
2013-10-23 17:22:24 +0800

    add message content to refer message

--- app/controllers/experts_controller.rb
+++ app/controllers/experts_controller.rb
@@ -23,20 +23,34 @@ class ExpertsController < ApplicationController
 
   def refer_new_expert
     @expert = current_user
-    @email_message = current_user.email_messages.build(subject: "Invite you to be expert at Prodygia", message: "good day", from_name: "#{current_user.first_name} #{current_user.last_name}", from_address: "no-reply@prodygia", reply_to: "#{current_user.email}")
+    message_content = <<CONTENT
+    <p>Dear [FIRST NAME_RECIPIENT EXPERT],</p>
+
+    <p>I’m using a new site to read and display quality content on China and thought I’d share with you.</p>
+
+    <p>It’s called Prodygia. What’s different with their model is that they start with experts, like you and me, and give us a toolkit to promote our knowledge on China and get paid. You can record on-demand video sessions and sell them to your network or run live sessions online. It’s quite unique and innovative in the market. They are carving out a niche for expertise on business, entrepreneurship, technology and culture as they relate to China.</p>
+
+    <p>I encourage you to learn more about the platform and see how you can capitalize on it to build your online presence.</p>
+    <p>Click below to sign up and enter your profile. It’s a first step. You can actually save time by signing up from your LinkedIn or Facebook accounts. Feel free to email specific questions to the Prodygia team on<a href="mailto:experts@prodygia.com"> experts@prodygia.com. </a>
+    <p>Sincerely,</p>
+
+    <p>[FIRST NAME_SENDER EXPERT]</p>
+CONTENT
+
+    @email_message = current_user.email_messages.build(subject: "Invite you to be expert at Prodygia", message: message_content, from_name: "#{current_user.first_name} #{current_user.last_name}", from_address: "no-reply@prodygia", reply_to: "#{current_user.email}")
   end
 
   private
-    def set_expert
-      @expert = Expert.find(params[:id])
-    end
+  def set_expert
+    @expert = Expert.find(params[:id])
+  end
 
-    def article_params
-      params.require(:article_session).permit(:title, :description, :cover, {categories:[]} )
-    end
+  def article_params
+    params.require(:article_session).permit(:title, :description, :cover, {categories:[]} )
+  end
 
-    def expert_params
-      params.require(:expert).permit(:name, :avatar, :title, :company, :location, :expertise, :favorite_quote, :career, :education, :web_site, :article_reports, :speeches, :additional, :testimonials)
-    end
+  def expert_params
+    params.require(:expert).permit(:name, :avatar, :title, :company, :location, :expertise, :favorite_quote, :career, :education, :web_site, :article_reports, :speeches, :additional, :testimonials)
+  end
 
 end
