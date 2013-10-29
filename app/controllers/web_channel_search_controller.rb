1b80e57a822d1acb0ff78bccfdc11101ef31d15e
tony <tony_doofy@163.com>
2013-10-29 15:55:41 +0800

    2脸脸渠道查询web_channel_search

--- app/controllers/web_channel_search_controller.rb
+++ app/controllers/web_channel_search_controller.rb
@@ -7,11 +7,9 @@ class WebChannelSearchController < ApplicationController
     sort = {_id: -1}
 
     hash.merge!({v: params[:v]}) unless params[:v].blank?
-
-    # @channel =  paginate3("Channel", params[:page], hash, sort, 3).group_by{|g| g.v}
-
-    @channel_ip = Channel.where(hash).group_by{|g| g.ip}.map{|k,v| [k,v.count]}
-    @channel_date = Channel.where(hash).group_by{|g| g.time.strftime("%Y-%m-%d")}.map{|k,v| [k,v.count]}
+    @channel_dip = Channel.where(hash).group_by{|g| [g.time.strftime("%Y-%m-%d"),g.ip]}.map{|k,v| [k,v.count]}.sort
+    @channel_ip = Channel.where(hash).group_by{|g| g.ip}.map{|k,v| [k,v.count]}.sort
+    @channel_date = Channel.where(hash).group_by{|g| g.time.strftime("%Y-%m-%d")}.map{|k,v| [k,v.count]}.sort
     respond_to do |format|
       format.html
     end
