de22b82e7e99a20b7577f76f8133f08d4493cba0
tony <tony_doofy@163.com>
2013-10-29 16:17:49 +0800

    4脸脸渠道查询web_channel_search

--- app/controllers/web_channel_search_controller.rb
+++ app/controllers/web_channel_search_controller.rb
@@ -8,8 +8,8 @@ class WebChannelSearchController < ApplicationController
 
     hash.merge!({v: params[:v]}) unless params[:v].blank?
     @channel_dip = Channel.where(hash).group_by{|g| [g.time.strftime("%Y-%m-%d"),g.ip]}.map{|k,v| [k,v.count]}
-    @channel_ip = Channel.where(hash).group_by{|g| g.ip}.map{|k,v| [k,v.count]}.sort
-    @channel_date = Channel.where(hash).group_by{|g| g.time.strftime("%Y-%m-%d")}.map{|k,v| [k,v.count]}.sort
+    @channel_ip = Channel.where(hash).group_by{|g| g.ip}.map{|k,v| [k,v.count]}
+    @channel_date = Channel.where(hash).group_by{|g| g.time.strftime("%Y-%m-%d")}.map{|k,v| [k,v.count]}
     respond_to do |format|
       format.html
     end
