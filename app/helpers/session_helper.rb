650249b6d06f6ebd6e5c8bd65c53d5c95a5002da
Gecko-Gecko <zjut.fmr@gmail.com>
2013-10-31 20:39:53 +0800

    fix timezone_helper

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -27,9 +27,11 @@ module SessionHelper
 
   def determine_time_with_zone(zone=nil, time, fallback)
 
-    user_timezone = current_user.time_zone if user_signed_in?
-    session_timezone = fallback
-    zone = user_timezone || session_timezone || "UTC"
+    if zone.nil?   # if zone is not explicitly specified
+      user_timezone = current_user.time_zone if user_signed_in?
+      session_timezone = fallback
+      zone = user_timezone || session_timezone || "UTC"
+    end
 
     time.in_time_zone(zone).to_formatted_s(:short) + "   " + zone
   end
