b4243f86e7c52a4862c11d4094ac92940b04f24d
peterzd <peter@originatechina.com>
2013-10-31 16:09:36 +0800

    working on post/draft new post

--- spec/features/expert_dashboard_spec.rb
+++ spec/features/expert_dashboard_spec.rb
@@ -10,6 +10,15 @@ def login_expert
 	login_as(sameer, scope: :user)
 end
 
+def fill_post_new_content
+	within("form#new_session") do
+		fill_in("Title", with: "a new acticle session")
+		# attach_file("session_cover", File.join( Rails.root.join('spec', 'fixtures', 'about_us.jpg') ))
+		check("session_categories_macro")
+	end
+
+end
+
 feature "Expert Dashboard" do
 	helper_objects
 
@@ -30,18 +39,31 @@ feature "Expert Dashboard" do
 		expect(page).to have_css("form#new_session")
 
 		## fill in the post new content form
-		within("form#new_session") do
-			fill_in("Title", with: "a new acticle session")
-			attach_file("session_cover", File.join( Rails.root.join('spec', 'fixtures', 'about_us.jpg') ))
-			check("session_categories_macro")
-		end
+		fill_post_new_content
 
 		## Create post content
-		click_button("Create post content")
+		click_button("Submit")
 
 		expect(current_path).to eq dashboard_expert_path(sameer.id)
+		expect(page).to have_css(".session-items .item", count: 1)
 
-		expect(Session.count).to eq 1
+		expect(Session).to have(1).instance
+
+	end
+
+	scenario "draft a new content", js: true do
+		click_link("Post new content")
+		expect(page).to have_css("form#new_session")
+
+		## fill in the post new content form
+		fill_post_new_content
+
+		## Draft it
+		click_button "Save Draft"
+		expect(current_path).to eq dashboard_expert_path(sameer.id)
+		expect(page).to have_css(".session-items .item", count: 1)
+		expect(Session).to have(1).instance
+		expect(Session.last).to be_draft
 	end
 
 end
