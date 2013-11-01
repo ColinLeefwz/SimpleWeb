68d9dfcc7575475ad458e3ac058fd322e32fab31
pulsen <huang123qwe@126.com>
2013-11-01 10:54:44 +0800

    图片剪裁

--- app/controllers/crop_photo_controller.rb
+++ app/controllers/crop_photo_controller.rb
@@ -1,14 +1,21 @@
 # coding: utf-8
 class CropPhotoController < ApplicationController
 
+  def upload
+    path = "/uploads/tmp/#{Time.now.strftime("%y%m%d%H%M%S")}coupon#{rand(99)}.jpg"
+    FileUtils.mv(params[:photo].tempfile.path, "public"+ path)
+    #    puts "----------" +path
+
+    render :text =>  path
+  end
+
   def crop
-    path = params[:photo].tempfile.path
-    img = MiniMagick::Image.open(path)
+    path =  params[:path]
+    img = MiniMagick::Image.open("public" + path)
     sc = img['width'].to_f/400
     img.crop "#{(params[:w].to_i*sc).to_i}x#{(params[:h].to_i*sc).to_i}+#{(params[:x].to_i*sc).to_i}+#{(params[:y].to_i*sc).to_i}"
-    rpath = "/uploads/tmp/#{Time.now.strftime("%y%m%d%H%M%S")}coupon#{rand(99)}.jpg"
-    img.write("public" + rpath)
-    render :json => {url: rpath}
+    img.write("public" + path)
+    render :json => {url: path+"?time=#{Time.now}"}
   end
     
 end
