14f3a85f8b1fae5ff4201a72c500dd603a5f0494
Gecko-Gecko <zjut.fmr@gmail.com>
2013-11-06 16:52:02 +0800

    template authorize solution

--- app/controllers/experts_controller.rb
+++ app/controllers/experts_controller.rb
@@ -1,5 +1,6 @@
 class ExpertsController < ApplicationController
-  load_and_authorize_resource
+  # load_and_authorize_resource
+  before_filter :set_expert
 
   def dashboard
     @sessions = @expert.sessions.order("draft desc")
@@ -22,7 +23,17 @@ class ExpertsController < ApplicationController
     end
   end
 
+
+  def profile
+    @sessions = @expert.sessions
+  end
+
   private
+  
+  def set_expert
+    @expert = Expert.find params[:id]
+  end
+
   def session_params
     params.require(:session).permit(:title, :description, :cover, :video, {categories:[]}, :location, :price, :language, :start_date, :time_zone )
   end
