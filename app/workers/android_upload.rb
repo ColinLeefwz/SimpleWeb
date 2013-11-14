e369d67ab16559f36f93d4a7a15514059a587bcc
pulsen <huang123qwe@126.com>
2013-11-14 15:05:30 +0800

    文件上传阿里云

--- app/workers/android_upload.rb
+++ app/workers/android_upload.rb
@@ -2,8 +2,9 @@
 class AndroidUpload
   @queue = :normal
 
-  def self.perform(path)
-    `scp web2:#{path} web1:/mnt/lianlian/public/`
+  def self.perform(version)
+#    `scp web2:#{path} web1:/mnt/lianlian/public/`
+    `ssh web2 "/mnt/Oss/oss2/osscmd put /mnt/lianlian/public/dface#{version}.apk oss://dface/dface${version}.apk --content-type=application/octet-stream"`
   end
   
 end
