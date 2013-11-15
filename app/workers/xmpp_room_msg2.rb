31c7f4243caee7ddbe4b6d3cdca2aafbcf735425
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-11-15 09:33:57 +0800

    m

--- app/workers/xmpp_room_msg2.rb
+++ app/workers/xmpp_room_msg2.rb
@@ -6,6 +6,10 @@ class XmppRoomMsg2
   def self.perform(room, uid, msg, mid=nil, log=0)
     mid = $uuid.generate if mid.nil?
     Xmpp.post("api/room", :roomid  => room , :message=> msg , :uid => uid, :mid => mid, :log => log)
+    if log==1
+      gchat = Gchat.new(sid: room.to_i, uid: uid, mid: mid, txt: msg)
+      gchat.save!
+    end
   end
   
 end
