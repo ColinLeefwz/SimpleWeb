29523b39c446a6cc79ca64f3ae9f68217894afcb
peterzd <peterzd@gmail.com>
2013-11-18 23:34:28 +0800

    if the user is logged in, show his name and email when tell a friend

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -111,7 +111,7 @@ describe SessionsController do
 		context "not logged in user"
 		context "logged in user" do
 			it "construct the email content" do
-				post :email_friend, id: session_intro, email_friend: {"your_name"=>"peter", "your_address"=>"zhao", "to_name"=>"Tina", "to_address"=>"tina@oc.com", "content"=>"some content"}
+				post :email_friend, id: session_intro, email_friend: {"your_name"=>"peter", "your_address"=>"peter@originate.com", "to_name"=>"Tina", "to_address"=>"tina@oc.com", "content"=>"some content"}
 				expect(assigns[:email]).not_to be_nil
 			end
 		end
