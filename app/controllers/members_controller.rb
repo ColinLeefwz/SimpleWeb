3a98976910508f6f8753304b4d0ec8d131860145
JevanWu <jevanwu@gmail.com>
2013-11-21 16:07:57 +0800

    create 'refer a friend' page

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,4 +2,13 @@ class MembersController < ApplicationController
   def dashboard
     @member = Member.where(id: params[:id]).first
   end
+
+  def refer_a_friend
+    @member = current_user
+    @email_message = current_user.build_refer_message
+    @from = "refer_a_friend"
+    respond_to do |format|
+      format.js {render "update"}
+    end
+  end
 end
