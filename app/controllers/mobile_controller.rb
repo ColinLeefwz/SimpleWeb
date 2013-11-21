55b75b6b773d0892b85ada92e0563d73b7063b4e
tony <tony_doofy@163.com>
2013-11-21 10:36:23 +0800

    移动网站修改2

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -10,8 +10,9 @@ class MobileController < ApplicationController
 
   def index2
     if params[:sid]
-      @mobile_articles = MobileArticle.where({sid: params[:sid]})
+      @mobile_articles = MobileArticle.where({sid: params[:sid]}).sort({_id:-1})
       @shop = Shop.find_by_id(params[:sid])
+      @mobile_banners = MobileBanner.where({sid: params[:sid]}).sort({_id:-1})
     end
     render :layout => false
   end
