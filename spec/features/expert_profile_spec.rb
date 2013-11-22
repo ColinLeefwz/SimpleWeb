dc20d7fa8e614cd51a0ac574e623184375f700c2
peterzd <peter@originatechina.com>
2013-11-22 17:31:26 +0800

    integration test for follow

--- spec/features/expert_profile_spec.rb
+++ spec/features/expert_profile_spec.rb
@@ -6,10 +6,28 @@ feature "Expert Profile Page" do
 	background do
 		[session_intro, session_communication]
 		[sameer_profile, alex_profile]
+		login_as peter
 		visit profile_expert_path(sameer)
 	end
 
 	scenario "lists all my sessions", js: true do
 		expect(page).to have_css("div.box-header", count: 2)
+	end
+
+	context "follow the expert", js: true do
+		scenario "follow successfully" do
+			page.find("i.fa-star").click
+			expect(page).to have_content("Unfollow")
+			expect(sameer.reload.followers).to include peter
+		end
+	end
+
+	context "unfollow the expert", js: true do
+		scenario "unfollow successfully" do
+			peter.follow sameer
+			page.find("i.fa-star").click
+			expect(page).to have_content("Follow")
+			expect(sameer.reload.followers).not_to include peter
+		end
 	end
 end
