a07036b16a4cd1277fcce38936eca6f05180bc6d
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-11-22 18:14:04 +0800

    首次开通填充内容

--- app/models/mobile_article.rb
+++ app/models/mobile_article.rb
@@ -20,5 +20,36 @@ class MobileArticle
       "http://oss.aliyuncs.com/dface/#{id}/0.jpg"
     end
   end
+  
+  def self.create_stub(sid,seq)
+    ma = MobileArticle.new
+    ma.id = "[#{sid}]#{seq}"
+    ma.save
+  end
+  
+  $fake_articles = ["",""]
+  
+  class << self
+    alias_method :find_by_id_old, :find_by_id unless method_defined?(:find_by_id_old)
+    alias_method :find_old, :find unless method_defined?(:find_old)
+  end
+  
+  def self.find_by_id(aid)
+    return nil if aid.nil?
+    if aid.class==String && aid[0]=="["
+      find_by_id_old($fake_articles[0])
+    else
+      find_by_id_old(aid)
+    end
+  end
+  
+  def self.find(aid)
+    return nil if aid.nil?
+    if aid.class==String && aid[0]=="["
+      find_old($fake_articles[0])
+    else
+      find_old(aid)
+    end
+  end
 
 end
