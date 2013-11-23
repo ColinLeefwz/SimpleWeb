6df28d357cd5936981c7a449f87679e877220c21
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-11-23 18:22:33 +0800

    玄武短信接口

--- app/workers/sms_sender.rb
+++ app/workers/sms_sender.rb
@@ -29,4 +29,21 @@ class SmsSender
     
   end
   
+  def self.send_sms_xuanwu(phone, text)
+    return true if ENV["RAILS_ENV"] != "production"
+    begin
+      str = URI.encode(text.encode('gbk','utf-8'))
+      info = RestClient.get "http://211.147.239.62:9050/cgi-bin/sendsms?username=test&password=11111&to=#{phone}&text=#{str}&msgtype=1"
+      unless info=="0"
+        Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{info}")
+        raise info
+      end
+    rescue Exception => e
+      puts e
+      puts e.backtrace
+      return nil
+    end
+  end
+  
+  
 end
