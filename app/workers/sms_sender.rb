a7a17552e8e003af08e3cff0ec1952ddf3b247d7
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-11-24 14:51:04 +0800

    短信错误修正

--- app/workers/sms_sender.rb
+++ app/workers/sms_sender.rb
@@ -12,13 +12,12 @@ class SmsSender
     begin
       pass = URI.escape("www.dface.cn20130709")
       info = RestClient.get "http://106.ihuyi.com/webservice/sms.php?method=Submit&account=cf_llh&password=#{pass}&mobile=#{phone}&content=#{URI.escape(text)}"
-      unless info.index("<code>2</code>")>0
-        Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{info}")
-        raise info
-      end
+      match = info.index("<code>2</code>")
+      return true if match && match>0
+      Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{info}")
+      return nil
     rescue Exception => e
-      puts e
-      puts e.backtrace
+      Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{e}")
       return nil
     end
   end
@@ -34,13 +33,12 @@ class SmsSender
     begin
       str = URI.encode(text.encode('gbk','utf-8'))
       info = RestClient.get "http://211.147.239.62:9050/cgi-bin/sendsms?username=test&password=11111&to=#{phone}&text=#{str}&msgtype=1"
-      unless info=="0"
-        Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{info}")
-        raise info
+      return if info=="0"
+      Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{info}")
+      return nil
       end
     rescue Exception => e
-      puts e
-      puts e.backtrace
+      Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{info}")
       return nil
     end
   end
