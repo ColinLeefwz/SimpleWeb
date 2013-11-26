e285422b78774ae36cc291bc12f1ab58e2abb952
JevanWu <jevanwu@gmail.com>
2013-11-26 15:25:29 +0800

    modify session query order

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @sessions = Session.where("draft=false")
+    @sessions = Session.where("draft=false").order("always_show desc, created_at desc")
   end
 
 end
