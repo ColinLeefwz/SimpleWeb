282084b87f95e32e182b46e2fd367101f7c79405
peterzd <peter@originatechina.com>
2013-11-27 18:06:58 +0800

    can use same email to sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ActiveRecord::Base
 
 	validates_presence_of   :email, :if => :email_required?
 	validates_format_of     :email, :with  => Devise.email_regexp, :allow_blank => true, :if => :email_changed?
+	validates_uniqueness_of :email, scope: [:provider]
 
 	validates_presence_of     :password, :if => :password_required?
 	validates_confirmation_of :password, :if => :password_required?
