22de77a176f4858221ae16c26089a741a96eec35
peterzd <peter@originatechina.com>
2013-11-28 15:02:49 +0800

    commit schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131126053755) do
+ActiveRecord::Schema.define(version: 20131127080747) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -253,7 +256,7 @@ ActiveRecord::Schema.define(version: 20131126053755) do
     t.string   "time_zone",              default: "UTC"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["email", "provider"], name: "index_users_on_email_and_provider", unique: true, using: :btree
   add_index "users", ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
   add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
