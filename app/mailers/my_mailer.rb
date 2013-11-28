efebbdd09da5e349f356545dc8b9ebf4aa15ede7
peterzd <peter@originatechina.com>
2013-11-28 11:46:42 +0800

    fix bug about use reset_password_token by Devise

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -4,7 +4,6 @@ class MyMailer < Devise::Mailer
 	helper :application
 
 	def reset_password_instructions(record, token, opts=[])
-		token = record.reset_password_token
 		reset_link = edit_user_password_url(record, reset_password_token: token)
 		mandrill = MandrillApi.new
 		mandrill.reset_password(record, reset_link)
