f3f5829a422b1212b588b107e614a6c07fae5f81
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-11-28 13:36:47 +0800

    m

--- app/workers/sms_sender.rb
+++ app/workers/sms_sender.rb
@@ -14,10 +14,10 @@ class SmsSender
       info = RestClient.get "http://106.ihuyi.com/webservice/sms.php?method=Submit&account=cf_llh&password=#{pass}&mobile=#{phone}&content=#{URI.escape(text)}"
       match = info.index("<code>2</code>")
       return true if match && match>0
-      Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{info}")
+      Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{Time.now},#{info}")
       return nil
     rescue Exception => e
-      Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{e}")
+      Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{Time.now},#{e}")
       return nil
     end
   end
@@ -32,12 +32,12 @@ class SmsSender
     return true if ENV["RAILS_ENV"] != "production"
     begin
       str = URI.encode(text.encode('gbk','utf-8'))
-      info = RestClient.get "http://211.147.239.62:9050/cgi-bin/sendsms?username=test&password=11111&to=#{phone}&text=#{str}&msgtype=1"
+      info = RestClient.get "http://211.147.239.62:9050/cgi-bin/sendsms?username=admin@zjll&password=123456&to=#{phone}&text=#{str}&msgtype=1"
       return if info=="0"
-      Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{info}")
+      Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{Time.now},#{info}")
       return nil
     rescue Exception => e
-      Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{info}")
+      Xmpp.send_chat($gfuid, $yuanid, "短信错误：#{Time.now},#{info}")
       return nil
     end
   end
