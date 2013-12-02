037d56ecdb6e66f81346dddae8c4480b624e20d5
Gecko-Gecko <zjut.fmr@gmail.com>
2013-12-02 20:01:12 +0800

    configuration for AWS

---
+++ config/initializers/s3_direct_upload.rb
@@ -0,0 +1,8 @@
+
+S3DirectUpload.config do |c|
+  Rails.configuration.aws = YAML.load_file("#{Rails.root}/config/aws.yml")[Rails.env].symbolize_keys!
+
+  c.access_key_id = Rails.configuration.aws[:access_key_id]
+  c.secret_access_key = Rails.configuration.aws[:secret_access_key]
+  c.bucket = Rails.configuration.aws[:bucket]
+end
