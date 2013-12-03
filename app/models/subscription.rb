5874a56142285471b63cd35d496dedab16e8469e
JevanWu <jevanwu@gmail.com>
2013-12-03 19:49:56 +0800

    add validates to subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,6 @@
 class Subscription < ActiveRecord::Base
   belongs_to :subscriber, class_name: "User"
+  validates :subscriber, presence: true
   belongs_to :subscribed_session, class_name: "Session"
+  validates :subscribed_session, presence: true
 end
