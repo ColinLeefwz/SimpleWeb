116aeb07d781171098997758d030368c761029e7
Gecko-Gecko <zjut.fmr@gmail.com>
2013-12-04 13:17:21 +0800

    modify "courses_users" table

--- db/migrate/20131204041317_create_courses_users.rb
+++ db/migrate/20131204041317_create_courses_users.rb
@@ -1,8 +1,8 @@
 class CreateCoursesUsers < ActiveRecord::Migration
   def change
     create_table :courses_users do |t|
-      t.references :courses, index: true
-      t.references :experts, index: true
+      t.references :course, index: true
+      t.references :expert, index: true
     end
   end
 end
