0293cdd826a871428303a8fbfdc6ad2d81256be5
Gecko-Gecko <zjut.fmr@gmail.com>
2013-12-04 10:20:15 +0800

    courses_controller & views application_helper for "add_and_remove fields"

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,12 @@
+class CoursesController < ApplicationController
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,35 @@
 module ApplicationHelper
-	def flash_class
-		if flash[:alert]
-			"alert-error"
-		elsif flash[:error]
-			"alert-something"
-		elsif flash[:notice]
-			"alert-success"
-		end
-	end
+  def flash_class
+    if flash[:alert]
+      "alert-error"
+    elsif flash[:error]
+      "alert-something"
+    elsif flash[:notice]
+      "alert-success"
+    end
+  end
 
-	def flash_message
-		if flash[:alert]
-			flash[:alert]
-		elsif flash[:error]
-			flash[:error]
-		elsif flash[:notice]
-			flash[:notice]
-		end
-		
-	end
+  def flash_message
+    if flash[:alert]
+      flash[:alert]
+    elsif flash[:error]
+      flash[:error]
+    elsif flash[:notice]
+      flash[:notice]
+    end
+  end
+
+
+  def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to(name, "#", class: "remove_fields")
+  end
+
+  def link_to_add_fields(name, f, associaton)
+    new_object = f.object.send(associaton.to_s).build
+    fields = f.fields_for(associaton, new_object, child_index: "new_#{associaton}") do |builder|
+      render(associaton.to_s.singularize + "_fields", f: builder)
+    end
+
+    link_to(name, "#", class: "add_fields", data: {associaton: associaton.to_s, fields: fields.gsub("\n", "") } )
+  end
 end
