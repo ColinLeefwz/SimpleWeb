c8b5a5a661087814ce0f2afcbf1db9f1d70d249c
tony <tony_doofy@163.com>
2013-12-05 16:18:15 +0800

    123

--- app/controllers/mobile_articles_controller.rb
+++ app/controllers/mobile_articles_controller.rb
@@ -19,9 +19,10 @@ class MobileArticlesController < ApplicationController
     @mobile_article.sid = session[:shop_id]
     @mobile_article.category = params[:c]
 
-    # path =  FileUtils.mkdir_p('public/mobile_article/' + @mobile_article.id.to_s).first
+    pre = params[:mobile_article][:img2]
+    path =  FileUtils.mkdir_p('public/mobile_article/' + @mobile_article.id.to_s).first
     # FileUtils.mv("public#{pre}", path+"/0.jpg")
-    # @mobile_article.img2_filename = "0.jpg"
+    @mobile_article.img2_filename = "0.jpg"
 
     if @mobile_article.save
       redirect_to URI::escape("/mobile_articles/index?c=#{params[:c]}")
@@ -68,6 +69,11 @@ class MobileArticlesController < ApplicationController
 
   def update
     @mobile_article = MobileArticle.find_by_id(params[:id])
+      unless  (pre = params[:mobile_article][:img2]).blank?
+        path = 'public/mobile_article/' + @mobile_article.id.to_s
+        FileUtils.rm(path+"/0.jpg")
+        FileUtils.mv("public#{pre}", path+"/0.jpg")
+      end
     if @mobile_article.update_attributes(params[:mobile_article])
       redirect_to URI::escape("/mobile_articles/index?c=#{params[:c]}")
     else
