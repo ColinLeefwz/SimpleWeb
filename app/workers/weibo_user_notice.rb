96c6e87bfb2e20bc2b37ac8f1d1fad05956eef4d
pulsen <huang123qwe@126.com>
2013-12-05 11:03:08 +0800

    用户注册给微博关系发送消息

--- app/workers/weibo_user_notice.rb
+++ app/workers/weibo_user_notice.rb
@@ -6,6 +6,7 @@
 class WeiboUserNotice
   @queue = :normal
 
+  PAGECOUNT=500
   
   def self.perform(uid)
     return if (user = User.find_by_id(uid)).nil?
@@ -20,6 +21,7 @@ class WeiboUserNotice
     api = "https://api.weibo.com/2/friendships/followers/ids.json"
     get_friendships(api, user, token).each do |u|
       Xmpp.send_chat(user.id, u.id, "我是你微博关注的人‘#{user.wb_name}’,也在使用脸脸，要来关注我哦！")
+      # puts "我是你微博关注的人‘#{user.wb_name}’,也在使用脸脸，要来关注我哦！"
     end
 
     #TODO 
@@ -29,29 +31,31 @@ class WeiboUserNotice
   def self.send_friend_notice(user, token)
     api = "https://api.weibo.com/2/friendships/friends/ids.json"
     get_friendships(api, user, token).each do |u|
-      Xmpp.send_chat(u.id, user.id, "我是你微博关注的人‘#{user.wb_name}’也在使用脸脸，要来关注我哦！")
+      # puts "我是你微博关注的人‘#{u.id}’,也在使用脸脸，要来关注我哦！"
+      Xmpp.send_chat(u.id, user.id, "我是你微博关注的人‘#{u.wb_name}’也在使用脸脸，要来关注我哦！")
     end
   end
 
   def self.get_friendships(api, user, token )
     response = res_api(api, user, token)
-    return if respone.nil?
+    return if response.nil?
     ids = response['ids']
     total_number = response['total_number']
-    (total_number/500).times do |t|
+    (total_number/PAGECOUNT).times do |t|
       begin
-        response = res_api(api, user, token,(t+1)*500 )
+        response = res_api(api, user, token,(t+1)*PAGECOUNT )
         ids = ids+response['ids']
       rescue
         next
       end
     end
-    User.where({wb_uid: {"$in" => ids}})
+    User.where({wb_uid: {"$in" => ids.map{|m| m.to_s}}})
   end
 
   def self.res_api(api, user, token, cursor=0, err=0)
+    puts '----------------'
     begin
-      JSON.parse(RestClient.get("#{api}?uid=#{user.wb_uid}&access_token=#{token}&count=#{500}&cursor=#{cursor}"))
+      JSON.parse(RestClient.get("#{api}?uid=#{user.wb_uid}&access_token=#{token}&count=#{PAGECOUNT}&cursor=#{cursor}"))
     rescue RestClient::BadRequest
       return nil
     rescue
