e9bc911e6fdcc8e8b15787cf56dc9a95988060a4
Gecko-Gecko <zjut.fmr@gmail.com>
2013-12-06 11:46:03 +0800

    add default_order :asc to Chapters & Sections

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -4,4 +4,6 @@ class Chapter < ActiveRecord::Base
   belongs_to :course
   has_many :sections, dependent: :destroy
   accepts_nested_attributes_for :sections, reject_if: lambda{|s| s[:title].blank?}, allow_destroy: true
+
+  default_scope {order(order: :asc)}
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -2,4 +2,9 @@ class Section < ActiveRecord::Base
   validates :title, presence: true
 
   belongs_to :chapter
+  has_many :resources, dependent: :destroy
+
+  accepts_nested_attributes_for :resources, allow_destroy: true  #todo add "reject_if" block
+
+  default_scope {order(order: :asc)}
 end
