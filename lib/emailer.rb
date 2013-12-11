236cba555b8f82c043b7865d14125516c615e3ac
pulsen <huang123qwe@126.com>
2013-12-11 10:10:26 +0800

    恢复sina_user

--- lib/emailer.rb
+++ lib/emailer.rb
@@ -29,37 +29,6 @@ class Emailer < ActionMailer::Base
 
   end
 
-  
-  class SendWibo
-    def self.count_weibo
-      sus = SinaUser.where({}).count
-      ius = SinaUser.where({is_I:true}).count
-      sps = SinaPoi.where({}).count
-      pps = SinaPoi.where({mtype:{"$exists" => true}}).count
-      hls = HeatLoc.where({fetched: true}).count
-
-      hangzhou = SinaPoi.where({city: '0571'})
-      hpi = hangzhou.count
-      hiu = hangzhou.inject(0) do |f, s|
-        if s.respond_to?(:datas)
-          f + s.datas.select{|data| data[2].to_s.match(/iphone|ipad/i)}.to_a.count
-        else
-          f
-        end
-      end
-
-      #      photo_num = SinaPoiPhoto.count
-      sf_num = SinaFriend.count
-
-      "用户数：#{sus};  其中ios系列用户：#{ius}; 杭州iso用户: #{hiu} \n 商家数：#{sps};  其中能匹配的商家数：#{pps}; 杭州商家: #{hpi} \n 已抓取的热门地点数： #{hls}; 完成度：#{(hls/110.92).round(3)}% \n \n 已抓微博好友个数: #{sf_num} "
-    end
-
-    def self.send_mail
-      Emailer.send_weibo_mail('微博抓取情况', count_weibo.to_s).deliver
-    end
-
-  end
-
   module SendDayStat
 
     def self.day_stat
