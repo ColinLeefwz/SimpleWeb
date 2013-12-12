b26fab4abd162c58eebcbb361d2ea079440bf23b
pulsen <huang123qwe@126.com>
2013-12-12 16:37:55 +0800

    m

--- app/controllers/admin_versions_controller.rb
+++ app/controllers/admin_versions_controller.rb
@@ -43,6 +43,17 @@ class AdminVersionsController < ApplicationController
     $redis.set("android_version", Version.last.id)
     render :json => ''
   end
+
+  def upload_test
+    if request.post?
+     path = "public/a/" + params[:file].original_filename
+     File.delete(path) if File.exists?(path)
+     FileUtils.mv(params[:file].tempfile.path, path)
+     render :text => '成功'
+    end
+
+  end
+
     
 
 end
