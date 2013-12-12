b3c15bfd4c99bc0aa595cf8077a39a084dc2db0a
peterzd <peter@originatechina.com>
2013-12-12 16:45:18 +0800

    short video_interviews index page

--- app/admin/video_interview.rb
+++ app/admin/video_interview.rb
@@ -1,6 +1,18 @@
 ActiveAdmin.register VideoInterview do
 	form partial: "form"
 
+	index do
+		column :title
+		column :categories
+		column :description
+		column :attached_video_hd_file_name
+		column :attached_video_sd_file_name
+		column :created_at
+		column :updated_at
+
+		default_actions
+	end
+
 	permit_params :title, :description, :expert_id, {categories:[]}, :attached_video_hd_file_name, :attached_video_hd_content_type, :attached_video_hd_file_size, :hd_url, :attached_video_sd_file_name, :attached_video_sd_content_type, :attached_video_sd_file_size, :sd_url
   
 end
