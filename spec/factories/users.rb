d5cdf5158b8e6c0f5fcc4db8d4853205f0684e10
Gecko-Gecko <zjut.fmr@gmail.com>
2013-12-13 22:40:06 +0800

    [test] add factories for admin_user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,17 +2,21 @@
 
 FactoryGirl.define do
   factory :user do
-		email 'email@test.com'
-		password 'password'
-		first_name 'first_name'
-		last_name 'last_name'
+    email 'email@test.com'
+    password 'password'
+    first_name 'first_name'
+    last_name 'last_name'
 
-		factory :member, class: 'Member' do
+    factory :member, class: 'Member' do
 
-			factory :expert, class: 'Expert' do
-				first_name 'expert@test.com'
-			end
-		end
+      factory :expert, class: 'Expert' do
+        first_name 'expert@test.com'
+      end
+    end
+
+    factory :admin_user, class: 'AdminUser' do
+      first_name 'admin@test.com'
+    end
 
   end
 end
