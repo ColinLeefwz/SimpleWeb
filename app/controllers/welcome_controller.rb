e31a03c4b711d43e5af5b24ed082e84fdcaceac9
JevanWu <jevanwu@gmail.com>
2013-12-16 16:20:46 +0800

    shows category only on landing page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     @sessions = Session.where("draft=false").order("always_show desc, created_at desc")
+    @show_category = true
   end
 
 end
