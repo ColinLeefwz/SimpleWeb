904816b6d1dc9f179bf07f97c06b9a4118d90cff
JevanWu <jevanwu@gmail.com>
2013-12-18 11:55:57 +0800

    delete unnecessary static action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,3 @@
 class StaticPagesController < ApplicationController
  
- # def static
- #   requested_page = params[:page].sub "_"," "
-
- #   @static_page = StaticPages.where("lower(title) = ?", requested_page.downcase).first
- #   render 'static_pages'
- # end
 end
