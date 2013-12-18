fd62f1c6930510ad3422b5e240955d259b4c266c
pulsen <huang123qwe@126.com>
2013-12-18 15:58:37 +0800

    紫薇原点消息

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -74,7 +74,7 @@ class Photo
       url = "http://www.dface.cn/zw#{self.id}.jpg"
       attrs = " NOLOG='1'  url='#{url}' "
       ext = "<x xmlns='dface.url'>#{url}</x>"
-      Xmpp.send_chat("s#{self.id.to_i}", self.user_id, text, "zw#{self.id}#{self.user_id}#{Time.now.to_i}", attrs, ext)
+      Xmpp.send_chat("s#{self.room.to_i}", self.user_id, text, "zw#{self.id}#{self.user_id}#{Time.now.to_i}", attrs, ext)
   end
   
   def gen_zwyd
