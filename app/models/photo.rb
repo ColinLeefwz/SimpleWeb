78ac860d08d2eb1441d18dbf15885608b4a5af92
pulsen <huang123qwe@126.com>
2013-12-19 18:11:06 +0800

    紫薇

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -69,7 +69,6 @@ class Photo
   end
 
   def zwyd_send_link
-      return if  room=="21828958"
       text = "😜他们都在制作专属的紫薇原点电子贺卡，还能集祝福抽红包哦～快来试试吧！"
       url = "http://www.dface.cn/zwyd_wish?id=#{self.id}"
       Xmpp.send_link_gchat($gfuid, self.room.to_i, self.user_id, text,url, "zw#{self.id}#{self.user_id}#{Time.now.to_i}")
