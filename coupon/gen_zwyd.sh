fae5f866fdf09b6453bff90691b4be995fcc88e9
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2013-12-19 19:22:28 +0800

    m

--- coupon/gen_zwyd.sh
+++ coupon/gen_zwyd.sh
@@ -10,6 +10,10 @@ convert -resize 640x640 $1 $5
 ruby ./crop2.rb $5 $2 $3 $4
 mogrify -resize 320x320 $5
 mogrify  -matte -draw 'image Dst_In 10,10 0,0 "circle.gif"'  $5
-convert -quality 70 -draw "image Over 573,231 0,0 \"$5\"" zwyd.jpg $6
+convert -quality 85 -draw "image Over 573,231 0,0 \"$5\"" zwyd.jpg $6
+rm $5
+convert -resize 275x275 $6 $5
+jpegoptim $5
+jpegoptim $6
+mv $5 ../public/t$6
 mv $6 ../public/
-#rm $5
