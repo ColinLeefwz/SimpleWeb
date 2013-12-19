27f6be8aa87a368e248dfffc0fa12032709d1823
Gecko-Gecko <zjut.fmr@gmail.com>
2013-12-19 21:16:17 +0800

    [refactor] move paypal and email method to application_controller
    - so that they can be used in multiple place

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -54,6 +54,37 @@ class ApplicationController < ActionController::Base
     current_user
   end
 
+  ## payments and enrollments
+  # paypal
+  def paypal_pay(item)
+    @order = Order.new(user: current_user, enrollable: item)
+
+    if @order.save
+      Paypal.create_payment_with_paypal(item, @order, order_execute_url(@order.id))
+      if @order.approve_url
+        redirect_to @order.approve_url
+      else
+        redirect_to send("#{item.class.name.downcase}_path", item.id), flash: {error: "Opps, something went wrong"}
+      end
+    else
+      redirect_to send("#{item.class.name.downcase}_path", item.id), flash: {error: "Opps, something went wrong"}
+    end
+  end
+
+  # send mail after user successfully enrolled 
+  def send_enrolled_mail(item)
+    domain_url = request.base_url
+    if domain_url == "http://localhost:3000"
+      domain_url = "http://www.prodygia.com"
+    end
+
+    item_image_url = domain_url + item.cover.url
+    mandrill = MandrillApi.new
+    mandrill.enroll_comfirm(current_user, item, item_image_url)
+  end
+  
+
+
   protected
   ## Override devise-invitable before_filter
   def authenticate_inviter!
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -194,38 +194,10 @@ class SessionsController < ApplicationController
     params.require(:article_session).permit(:title, {categories:[]}, :cover, :description, :language)
   end
 
-  def paypal_pay
-    @order = @session.orders.build
-    @order.user = current_user
-
-    if @order.save
-      Paypal.create_payment_with_paypal(@session, @order, order_execute_url(@order.id))
-
-      if @order.approve_url
-        redirect_to @order.approve_url
-      else
-        redirect_to session_path(@session)
-      end
-    else
-      render :create, alert: @order.errors.to_a.join(", ")
-    end
-  end
-
   def enroll_redirect
     current_user.enroll_session @session
     send_mail
     redirect_to session_path(@session), flash: { success: "Enrolled Successful !" }
-  end
-
-  def send_mail
-    domain_url = request.base_url
-    if domain_url == "http://localhost:3000"
-      domain_url = "http://www.prodygia.com"
-    end
-
-    session_image_url = domain_url + @session.cover.url
-    mandrill = MandrillApi.new
-    mandrill.enroll_comfirm(current_user, @session, session_image_url)
   end
 end
 
