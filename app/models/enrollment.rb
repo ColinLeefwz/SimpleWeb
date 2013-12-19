f729e95f7402d6b821f72643f7c6e3e8c9f004a7
Gecko-Gecko <zjut.fmr@gmail.com>
2013-12-19 16:24:07 +0800

    [db] add polymorphic to orders table
    - add “enrollable_id”, “enrollable_type” to orders table
    - add validations to Order model
    - add validations to Enrollment model

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,7 @@
 class Enrollment< ActiveRecord::Base
   belongs_to :enrollable, polymorphic: true  # belongs to courses and sessions
   belongs_to :user
+
+  validates :user_id, :enrollable_id, :enrollable_type, presence: true
+  validates :user_id, uniqueness: { scope: [:enrollable_type, :enrollable_id], message: "can not enroll twice" }
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,9 @@
 class Order < ActiveRecord::Base
+  belongs_to :enrollable, polymorphic: true  # belongs to courses and sessions
   belongs_to :user
-  belongs_to :session
 
-	attr_accessor :return_url, :cancel_url, :payment_method, :approve_url
+  validates :user_id, :enrollable_id, :enrollable_type, presence: true
+  validates :user_id, uniqueness: { scope: [:enrollable_type, :enrollable_id], message: "already purchased" }
 
+  attr_accessor :return_url, :cancel_url, :payment_method, :approve_url
 end
