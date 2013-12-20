53b422c5af0cf796fe4b9671dea16d05d48125fd
peterzd <peter@originatechina.com>
2013-12-20 15:17:48 +0800

    [fix]upload covers to Alex's s3 account
    1. Add s3_host_name: "" to models for paperclip
    2. Add AWS_REGION to ENV file

--- config/initializers/s3_direct_upload.rb
+++ config/initializers/s3_direct_upload.rb
@@ -4,4 +4,5 @@ S3DirectUpload.config do |c|
   c.access_key_id = ENV["AWS_ACCESS_KEY_ID"]
   c.secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
   c.bucket = ENV["AWS_BUCKET"]
+	c.region = ENV["AWS_REGION"]
 end
