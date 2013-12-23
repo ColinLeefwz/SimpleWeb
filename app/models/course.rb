ba114cef7d6eb0808f664714c57c28db317789db
peterzd <zdsunshine0640@gmail.com>
2013-12-23 22:57:14 +0800

    [dev]try to add intro video to course/new page
    this url https://github.com/plataformatec/simple_form/wiki/Nested-Models
    is for simple_form nested form
    we use it to create intro_video when creating a new course

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,8 +3,10 @@ class Course < ActiveRecord::Base
 
   has_and_belongs_to_many :experts
   has_many :chapters, dependent: :destroy
-	has_one :intro_video
   accepts_nested_attributes_for :chapters, reject_if: lambda{|c| c[:title].blank?}, allow_destroy: true
+
+	has_one :intro_video
+	accepts_nested_attributes_for :intro_video
 
   has_attached_file :cover,
     storage: :s3,
