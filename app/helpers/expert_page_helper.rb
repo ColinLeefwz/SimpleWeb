ce0fab68ad76e1e86fb7b5ade7779fe2da13c7b8
peterzd <peter@originatechina.com>
2013-12-27 16:56:00 +0800

    [chore]remove follow_link helper method

--- app/helpers/expert_page_helper.rb
+++ app/helpers/expert_page_helper.rb
@@ -1,20 +1,10 @@
 module ExpertPageHelper
-	def favorite_class(expert)
+	def favorite_class(expert, extra="")
 		if current_user.try(:follow?, expert)
-			".favorite.solid-star "
+			"favorite-#{extra} solid-star "
 		else
-			".favorite.hollow-star"
+			"favorite-#{extra} hollow-star"
 		end
 	end
 
-	def follow_link(expert)
-		if current_user.try(:follow?, expert)
-			link_to image_tag("favorite.png", class: "favorite-icon solid-star"), relationship_path(expert), remote: true
-		else
-			link_to image_tag("favorite.png", class: "favorite-icon hollow-star"), relationship_path(expert), remote: true
-		end
-
-
-	end
-	
 end
