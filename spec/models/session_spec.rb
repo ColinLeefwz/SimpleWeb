6111a00ab203887d23f206d0cccda1e9016d013d
Gecko-Gecko <zjut.fmr@gmail.com>
2014-01-01 19:12:45 +0800

    [test] add a simple shoulda test

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,24 +1,27 @@
 require 'spec_helper'
 
 describe Session do
-	helper_objects
+  helper_objects
 
-	describe ".free?" do
-		it "true if the price equals 0.00" do
-			session_intro.price = 0.00
-			expect(session_intro.free?).to be_true
-		end
+  # it{ should have_and_belong_to_many :enroll_users }
+  it { should belong_to(:expert) }
 
-		it "true if the price is less than 0.00" do
-			session_intro.price = -3.00
-			expect(session_intro.free?).to be_true
-		end
+  describe ".free?" do
+    it "true if the price equals 0.00" do
+      session_intro.price = 0.00
+      expect(session_intro.free?).to be_true
+    end
 
-		it "false if the price is bigger than 0.00" do
-			session_intro.price = 5.00
-			expect(session_intro.free?).to be_false
-		end
-	end
+    it "true if the price is less than 0.00" do
+      session_intro.price = -3.00
+      expect(session_intro.free?).to be_true
+    end
+
+    it "false if the price is bigger than 0.00" do
+      session_intro.price = 5.00
+      expect(session_intro.free?).to be_false
+    end
+  end
 
   describe ".set_default" do
     it "set default price to 0.00 for the session" do
@@ -27,12 +30,12 @@ describe Session do
     end
   end
 
-	describe "canceled" do
-		it "can be canceled" do
+  describe "canceled" do
+    it "can be canceled" do
       session = create(:session, title: "test", expert: sameer)
-			session.update_attributes canceled: true
-			expect(session.reload).to be_canceled
-		end
-	end
-	
+      session.update_attributes canceled: true
+      expect(session.reload).to be_canceled
+    end
+  end
+
 end
