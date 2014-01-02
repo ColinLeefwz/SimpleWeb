be992b74276c4d45bac55184e3140f9097e0216c
Gecko-Gecko <zjut.fmr@gmail.com>
2014-01-02 10:53:38 +0800

    [db] add `twitter` column to `profiles` table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131226031951) do
+ActiveRecord::Schema.define(version: 20140102023300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -210,6 +210,7 @@ ActiveRecord::Schema.define(version: 20131226031951) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "twitter"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
