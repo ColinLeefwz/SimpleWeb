950b9a7277ef25e9a0d6c470f039b6d9323973a3
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2014-01-03 18:38:38 +0800

    图片默认公开，但最多只有2页／10张

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -242,6 +242,7 @@ class PhotosController < ApplicationController
     page = 1 if page==0
     pcount = 5 if pcount==0
     skip = (page-1)*pcount
+    return :json => [].to_json if skip>1
     photos = user_photo_cache(params[:uid], skip, pcount)
     render :json => photos.map {|p| p.output_hash_with_shopname }.to_json
   end
@@ -263,7 +264,7 @@ class PhotosController < ApplicationController
     arr = $redis.smembers("UnBroadcast")
     arr.push(nil)
     arr.push("")
-    Photo.where({user_id: uid, "$or" => [ { weibo: true } , { qq: true } ], room:{"$nin" => arr} }).
+    Photo.where({user_id: uid, room:{"$nin" => arr} }).
       sort({updated_at: -1}).skip(skip).limit(pcount).to_a
   end
 
