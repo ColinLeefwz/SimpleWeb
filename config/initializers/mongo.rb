36b1ba87314b16ffcadf61d06d975e9d9d6cee34
web2 <web2@dface.cn>
2014-01-06 22:17:13 +0800

    恢复采用mongoid的node自动发现机制

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -174,38 +174,3 @@ module Mongoid
   end
 end
 
-module Moped
-
-  # The cluster represents a cluster of MongoDB server nodes, either a single
-  # node, a replica set, or a mongos server.
-  class Cluster
-
-    def nodes(opts = {})
-	    #puts "disable Node Discovery."
-      @nodes
-    end
-
-    def refresh(nodes_to_refresh = @nodes)
-      @nodes.each {|node| node.refresh}
-      @nodes
-    end
-
-  end
-
-  class Node
-    
-    alias :refresh_old :refresh
-    
-    def refresh
-      if ip_address.to_s == "127.0.0.1"
-        @primary = true
-      else
-        refresh_old
-      end
-    end
-    
-  end
-  
-end
-
-
