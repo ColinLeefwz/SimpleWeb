37e0f47301a49fd1d6b54b435fbbfce935750419
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2014-01-06 11:59:03 +0800

    m

--- app/controllers/a_controller.rb
+++ app/controllers/a_controller.rb
@@ -25,7 +25,7 @@ class AController < ApplicationController
     #Mozilla/5.0 (iPhone; CPU iPhone OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Mobile/9B206 MicroMessenger/5.0.3
     #Mozilla/5.0 (Linux; U; Android 2.3.4; zh-cn; WT19i Build/4.0.2.A.0.62) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1 MicroMessenger/4.5.255
 
-    if params[:x]
+    if params[:x].nil?  #不带x则尝试直接下载
       if c.agent.index("Android")
         if c.agent.index("MicroMessenger")
           render :file => "~/lianlian/public/mini.html", :use_full_path => true
