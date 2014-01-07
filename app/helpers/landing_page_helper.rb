357b38483136bf6b67a87b34944ff1bfe1c36b62
jevanwu <jevanwu@gmail.com>
2014-01-07 15:20:34 +0800

    [feature] change the ariticle_session url 'session/...' to 'article/...'

--- app/helpers/landing_page_helper.rb
+++ app/helpers/landing_page_helper.rb
@@ -1,7 +1,7 @@
 module LandingPageHelper
   def currect_path(session)
     if session.is_a? Session
-      session_path(session)
+      article_path(session)
     elsif session.is_a? VideoInterview
       video_interview_path(session)
     elsif session.is_a? Announcement
