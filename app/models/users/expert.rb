c10cb6e58b78854c5fb81a7ac8f918dc7cd7811b
peterzd <peter@originatechina.com>
2014-01-09 16:56:15 +0800

    [feature]make subdomain for expert's profile page

--- app/models/users/expert.rb
+++ app/models/users/expert.rb
@@ -8,9 +8,8 @@ class Expert < Member
 
   accepts_nested_attributes_for :profile
   accepts_nested_attributes_for :intro_video
-  # alias_method :profile=, :profile_attributes=   # NOTE add this line for active admin working properly
 
-  # after_create :build_profile
+	before_create :set_subdomain
 
   def name
     "#{first_name} #{last_name}"
@@ -32,10 +31,9 @@ class Expert < Member
     self.sessions.where("content_type = 'LiveSession'").order("draft desc")
   end
 
-  # private
-  # def build_profile
-		# logger.info "after create expert"
-  #   self.profile || self.create_profile
-  # end
+	private
+	def set_subdomain
+		self.subdomain = self.name.downcase.delete(" ").delete("'")
+	end
 
 end
