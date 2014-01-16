07c7816511a9f39ceef91e6085ea6fea539ad9e6
jevanwu <jevanwu@gmail.com>
2014-01-16 14:49:44 +0800

    [feature] justify whether the user is signed in inside the controller, rather than using js

--- app/assets/javascripts/text_page.js.coffee
+++ app/assets/javascripts/text_page.js.coffee
@@ -1,40 +1,8 @@
-get_cookie = (name) ->
-	parts = document.cookie.split(name + "=")
-	if parts.length == 2
-		return parts.pop().split(";").shift()
+change_star_class = ->
+  if $(".favorite-icon").hasClass("solid-star-icon")
+    $(".favorite-icon").attr("src", "/assets/favorite.png")
+  else
+    $(".favorite-icon").attr("src", "/assets/favorite_unselected.png")
 
-detect_login = ->
-	signed_in = get_cookie("signed_in")
-	if signed_in == "1"
-		return true
-	else
-		alert "not logged in"
-		return false
-
-favorite_icon_img = ->
-	favorite_imgs = $("img.favorite-icon")
-	for img_icon in favorite_imgs
-		if $(img_icon).hasClass("solid-star")
-		  $(img_icon).attr("src", "/assets/favorite.png")
-		else
-			$(img_icon).attr("src", "/assets/favorite_unselected.png")
-
-favorite_click = ->
-	$("img.favorite-icon").unbind "click"
-	$("img.favorite-icon").on "click", ->
-		if $(this).hasClass("solid-star")
-			$(this).removeClass("solid-star").addClass("hollow-star")
-		else
-			$(this).removeClass("hollow-star").addClass("solid-star")
-	favorite_icon_img()
-
-$(document).ready ->
-	# favorite_click()
-	favorite_icon_img()
-
-$(document).on 'page:load', ->
-	# favorite_click()
-	favorite_icon_img()
-
-$(document).on 'ajax:success', ->
-	favorite_click()
+$(document).ready(change_star_class)
+$(document).on 'page:load', change_star_class
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,14 +31,14 @@ class UsersController < ApplicationController
         
         if current_user.blank?
           render js: "window.location='#{new_user_session_path}'"
-          flash[:alert] = "Sorry! You have to sign in to follow an expert"
-        elsif
+          flash[:alert] = "Sorry! You have to sign in to follow an Expert"
+        else
           if current_user.try(:follow?, followed_user)
             current_user.unfollow(followed_user)
           else
             current_user.follow(followed_user)
           end
-          render nothing: true
+          render "shared/update_favorite_star"
         end
       }
     end
@@ -54,12 +54,17 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.js{
         current_session = Session.where(id: params[:session_id]).first
-        if current_user.has_subscribed?(current_session)
-          current_user.unsubscribe(current_session)
+        if current_user.blank?
+          render js: "window.location='#{new_user_session_path}'"
+          flash[:alert] = "Sorry! You have to sign in to follow an Article"
         else
-          current_user.subscribe(current_session)
+          if current_user.has_subscribed?(current_session)
+            current_user.unsubscribe(current_session)
+          else
+            current_user.subscribe(current_session)
+          end
+          render "shared/update_favorite_star"
         end
-        render nothing: true
      } 
     end
   end
@@ -68,12 +73,17 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.js{
         current_course = Course.where(id: params[:course_id]).first
-        if current_user.has_subscribed?(current_course)
-          current_user.unsubscribe(current_course)
+        if current_user.blank?
+          render js: "window.location='#{new_user_session_path}'"
+          flash[:alert] = "Sorry! You have to sign in to follow a Course"
         else
-          current_user.subscribe(current_course)
+          if current_user.has_subscribed?(current_course)
+            current_user.unsubscribe(current_course)
+          else
+            current_user.subscribe(current_course)
+          end
+          render "shared/update_favorite_star"
         end
-        render nothing: true
      } 
     end
   end
@@ -82,12 +92,17 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.js{
         video_interview = VideoInterview.where(id: params[:video_interview_id]).first
-        if current_user.has_subscribed?(video_interview)
-          current_user.unsubscribe(video_interview)
+        if current_user.blank?
+          render js: "window.location='#{new_user_session_path}'"
+          flash[:alert] = "Sorry! You have to sign in to follow a Video Interview"
         else
-          current_user.subscribe(video_interview)
+          if current_user.has_subscribed?(video_interview)
+            current_user.unsubscribe(video_interview)
+          else
+            current_user.subscribe(video_interview)
+          end
+          render "shared/update_favorite_star"
         end
-        render nothing: true
      } 
     end
   end
