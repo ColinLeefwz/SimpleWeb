47ad89f411384c2acda7e0ad7f488931195d424c
Gecko-Gecko <zjut.fmr@gmail.com>
2014-01-17 20:58:34 +0800

    [chore] indentation

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -4,37 +4,40 @@ class Resource < ActiveRecord::Base
   belongs_to :expert
   has_attached_file :attached_file,
     storage: :s3,
-		s3_credentials: {
-			bucket: ENV["AWS_BUCKET"],
-			access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-			secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
-		},
+    s3_credentials: {
+      bucket: ENV["AWS_BUCKET"],
+      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+    },
+    s3_host_name: "s3-us-west-1.amazonaws.com",
     path: ":class/:attachment/:id/:style/:filename"
 
-  scope :video, lambda{ where("attached_file_content_type like ?", "__d_o%" )}
+    scope :video, lambda{ where("attached_file_content_type like ?", "__d_o%" )}
 
-  after_save :copy_and_delete
+    after_save :copy_and_delete
 
-  private
+    private
 
-  def copy_and_delete
-    
-    if self.attached_file_file_name.present?
-      paperclip_file_path = "resources/attached_files/#{self.id}/original/#{self.attached_file_file_name}"
-      raw_source = self.attached_file_file_path
+    def copy_and_delete
 
-      s3 = AWS::S3.new
-      bucket_name = ENV["AWS_BUCKET"]
+      if self.attached_file_file_name.present?
+        paperclip_file_path = "resources/attached_files/#{self.id}/original/#{self.attached_file_file_name}"
+        raw_source = self.attached_file_file_path
 
-      destination = s3.buckets[bucket_name].objects[paperclip_file_path]
-      sub_source = CGI.unescape(raw_source)
-      sub_source = sub_source.split("/")
-      2.times{ sub_source.shift() }
-      sub_source = sub_source.join("/")
+        s3 = AWS::S3.new
+        bucket_name = ENV["AWS_BUCKET"]
 
-      source = s3.buckets[bucket_name].objects["#{sub_source}"]
-      source.copy_to(destination)
-      source.delete
+        destination = s3.buckets[bucket_name].objects[paperclip_file_path]
+
+        sub_source = CGI.unescape(raw_source)
+        sub_source = sub_source.split("/")
+        2.times{ sub_source.shift() }
+        sub_source = sub_source.join("/")
+
+        source = s3.buckets[bucket_name].objects["#{sub_source}"]
+
+        source.copy_to(destination)
+        source.delete
+      end
     end
-  end
 end
