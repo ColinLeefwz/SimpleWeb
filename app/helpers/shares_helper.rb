4d111ab55c44e9a4ba22e05d8c158c68b57cc0a2
jevanwu <jevanwu@gmail.com>
2014-01-20 15:17:37 +0800

    [fix] if the shares is nil, show 0

--- app/helpers/shares_helper.rb
+++ app/helpers/shares_helper.rb
@@ -5,18 +5,18 @@ module SharesHelper
   def get_fb_shares(url)
     json_content = open('http://graph.facebook.com/?id=' + url)
     shares = JSON.parse json_content.read
-    shares['shares']
+    shares['shares'] || 0
   end
 
   def get_li_shares(url)
     json_content = open('http://www.linkedin.com/countserv/count/share?format=json&url=' + url)
     shares = JSON.parse json_content.read
-    shares['count']
+    shares['count'] || 0
   end
 
   def get_tw_shares(url)
     json_content = open('http://urls.api.twitter.com/1/urls/count.json?url='+ url)
     shares = JSON.parse json_content.read
-    shares['count']
+    shares['count'] || 0
   end
 end
