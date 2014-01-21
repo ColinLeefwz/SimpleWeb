2c03af6330cb9c9fb3175026a96d9f1a5c9769d9
Gecko-Gecko <zjut.fmr@gmail.com>
2014-01-21 18:30:24 +0800

    [fix] undefined method `web_sit`
    - spelling error

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,7 +7,7 @@ class Profile < ActiveRecord::Base
   # make sure the URL starts with `http` protocol, so that link_to works properly
   def check_web_site
     if self.web_site.present? and (/^http/.match(self.web_site) == nil)
-      self.web_site = "http://" + self.web_sit
+      self.web_site = "http://" + self.web_site
     end
   end
 
