2a6841993998f806c8b0090546c71ea4e1d3bd73
Gecko-Gecko <zjut.fmr@gmail.com>
2014-01-22 16:19:37 +0800

    [feature] calculate duration according to the input
    - put the logic in model so that you don’t have to inspect through
    `course => chapter => section`

--- app/models/section.rb
+++ app/models/section.rb
@@ -7,6 +7,7 @@ class Section < ActiveRecord::Base
   accepts_nested_attributes_for :resources, allow_destroy: true  #todo add "reject_if" block
 
   default_scope {order(order: :asc)}
+  before_save :convert_duration
 
   def has_video?
     self.resources.inject(false) { |memo, obj|  memo or obj.attached_file_file_name.present? }
@@ -19,4 +20,19 @@ class Section < ActiveRecord::Base
   def hd_url
     self.resources.where(video_definition: "HD").first.attached_file.url  || " "
   end
+
+  private
+  def convert_duration
+    matcher = /(?<hour>\d*):(?<minute>[0-5]?[0-9]):(?<second>[0-5]?[0-9])/
+    result = matcher.match(self.duration)
+
+    if result
+      hour = result[:hour].to_i
+      minute = result[:minute].to_i
+      second = result[:second].to_i
+
+      self.duration = (second + minute*60 + hour*3600).to_s
+    end
+  end
+
 end
