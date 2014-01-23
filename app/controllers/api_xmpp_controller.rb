ee42ea8bade6ef844d59da6bf2ddf606eecf2bb2
ibluetime <ibluetime@gmail.com>
2014-01-23 15:12:05 +0800

    change message push

--- app/controllers/api_xmpp_controller.rb
+++ app/controllers/api_xmpp_controller.rb
@@ -3,9 +3,9 @@ class ApiXmppController < ApplicationController
   def push_message
     return if to.blank? || params[:msg].blank?
     if to.is_a? Array
-      to.each { |t| XmppMsg.perform(from, t, params[:msg]) }
+      to.each { |t| Resque.enqueue(XmppMsg, from, t, params[:msg])}
     else
-      XmppMsg.perform(from, to, params[:msg])
+      Resque.enqueue(XmppMsg, from, to, params[:msg])
     end 
     render json: { error: 0 }
   end
