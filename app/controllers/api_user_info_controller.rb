b51c08096f500c497ec67d82eeadf02562bc8ea7
ibluetime <ibluetime@gmail.com>
2014-01-25 16:31:21 +0800

    change to use session

--- app/controllers/api_user_info_controller.rb
+++ app/controllers/api_user_info_controller.rb
@@ -1,7 +1,13 @@
 class ApiUserInfoController < ApplicationController
 
   def basic
-    user = User.find_by_id(params[:id])
+    if params[:session]
+      cache = $redis.get(params[:session])
+      user_id = Marshal.load(cache)["user_id"] unless cache.blank?
+    else
+      user_id = params[:id]
+    end
+    user = User.find_by_id(user_id)
     render json: user.safe_output.to_json
   end
 end
