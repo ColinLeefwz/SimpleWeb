45646da35588ab111f42c563b71232c231de009f
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2014-01-28 16:23:37 +0800

    m

--- app/controllers/a_controller.rb
+++ app/controllers/a_controller.rb
@@ -28,6 +28,7 @@ class AController < ApplicationController
     if params[:x].nil?  #不带x则尝试直接下载
       if c.agent.index("Android")
         if c.agent.index("MicroMessenger")
+          return redirect_to "http://3g.qq.com.dface.cn/dface2.7.1.apk"
           render :file => "~/lianlian/public/wx_down.html", :use_full_path => true
           return
         else
@@ -69,6 +70,7 @@ class AController < ApplicationController
     c.save
     if false && c.agent.index("Android")
       if c.agent.index("MicroMessenger")
+        return redirect_to "http://3g.qq.com.dface.cn/dface2.7.1.apk"
         render :file => "~/lianlian/public/mini.html", :use_full_path => true
         return
       end
