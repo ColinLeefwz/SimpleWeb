ce5ac18b03a73478cba47e6f5fcdd893a7b194e4
Gecko-Gecko <zjut.fmr@gmail.com>
2014-02-07 15:02:59 +0800

    [refactor] solve n+1 problem of index using `:includes`

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-    video_interviews = VideoInterview.all
-    annoucements = Announcement.all
-    articles = Session.where(content_type: "ArticleSession").where(draft: false)
+    video_interviews = VideoInterview.includes(:expert).all
+    annoucements = Announcement.includes(:expert).all
+    articles = ArticleSession.includes(:expert).where(draft: false)
     collection = video_interviews + annoucements + articles
     @sessions = collection.sort{|x, y| y.updated_at <=> x.updated_at}
     @show_category = true
