199150f8a1dc91f11ef182c7db50c10f073093e3
Gecko-Gecko <zjut.fmr@gmail.com>
2014-02-07 20:54:25 +0800

    [refactor] DEPRECATION WARNING: Relation#all is deprecated for eager loading
    - no need for `.all` to eager loading

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
-    video_interviews = VideoInterview.includes(:expert).all
-    annoucements = Announcement.includes(:expert).all
+    video_interviews = VideoInterview.includes(:expert)
+    annoucements = Announcement.includes(:expert)
     articles = ArticleSession.includes(:expert).where(draft: false)
     collection = video_interviews + annoucements + articles
     @sessions = collection.sort{|x, y| y.updated_at <=> x.updated_at}
