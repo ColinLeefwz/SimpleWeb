b1d91c3c8809bd3ff32b84c06806cf8b3532e73c
peterzd <zdsunshine0640@gmail.com>
2014-02-11 15:37:00 +0800

    [chore]forget the code in Course for recommended_courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -36,6 +36,26 @@ class Course < ActiveRecord::Base
 
     }
 
+  class << self
+    def recommend_courses(current_user)
+      show_courses = []
+      if current_user.is_a? Expert
+        own_courses = current_user.courses
+        staff_courses = Expert.staff.courses.take(3)
+        show_courses = staff_courses
+        if staff_courses.count <= 3
+          other_courses = Course.all.sample(3 - staff_courses.count)
+          other_courses = (Course.all - own_courses - staff_courses).sample(3 - staff_courses.count)
+          show_courses << other_courses unless other_courses.empty?
+        end
+      elsif current_user.is_a? Member
+        show_courses = (Course.all - Expert.staff.courses).sample(3)
+      end
+      show_courses
+    end
+  end
+
+
   def producers
     "by " + self.experts.map(&:name).join(" and ")
   end
