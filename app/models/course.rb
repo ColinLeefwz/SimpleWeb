2e83c242aa8a23fb29958e97f5402fa6455daa64
Gecko-Gecko <zjut.fmr@gmail.com>
2014-02-12 21:19:20 +0800

    [chore] indentation

--- app/models/course.rb
+++ app/models/course.rb
@@ -13,9 +13,9 @@ class Course < ActiveRecord::Base
   has_many :chapters, -> {order(order: :asc)}, dependent: :destroy
   accepts_nested_attributes_for :chapters, reject_if: lambda{|c| c[:title].blank?}, allow_destroy: true
 
-	has_one :intro_video, as: :introable, dependent: :destroy
+  has_one :intro_video, as: :introable, dependent: :destroy
 
-	accepts_nested_attributes_for :intro_video
+  accepts_nested_attributes_for :intro_video
 
   has_many :subscriptions, as: :subscribable
   has_many :subscribers, through: :subscriptions
@@ -36,29 +36,32 @@ class Course < ActiveRecord::Base
 
     }
 
-  class << self
-    def recommend_courses(current_user)
-      staff_courses = Expert.staff.courses.take(3)
-      if current_user.is_a? Expert
-        own_courses = current_user.courses
-        show_courses = staff_courses
-        if staff_courses.count <= 3
-          other_courses = (Course.all - own_courses - staff_courses).sample(3 - staff_courses.count)
-          show_courses << other_courses unless other_courses.empty?
+    class << self
+      def recommend_courses(current_user)
+        staff_courses = Expert.staff.courses.take(3)
+
+        if current_user.is_a? Expert
+          own_courses = current_user.courses
+          show_courses = staff_courses
+          if staff_courses.count <= 3
+            other_courses = (Course.all - own_courses - staff_courses).sample(3 - staff_courses.count)
+            show_courses << other_courses unless other_courses.empty?
+          end
+
+        elsif current_user.is_a? Member
+          show_courses = (Course.all - staff_courses).sample(3)
         end
-      elsif current_user.is_a? Member
-        show_courses = (Course.all - staff_courses).sample(3)
+
+        show_courses
       end
-      show_courses
     end
-  end
 
 
-  def producers
-    "by " + self.experts.map(&:name).join(" and ")
-  end
+    def producers
+      "by " + self.experts.map(&:name).join(" and ")
+    end
 
-  def free?
-    self.price == 0
-  end
+    def free?
+      self.price == 0
+    end
 end
