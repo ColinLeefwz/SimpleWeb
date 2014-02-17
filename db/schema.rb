8c90876f6abf52fabe60ccc3eee11eba55af5835
Gecko-Gecko <zjut.fmr@gmail.com>
2014-02-17 17:43:30 +0800

    [db] add default to page_views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212063008) do
+ActiveRecord::Schema.define(version: 20140217093716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,16 +30,6 @@ ActiveRecord::Schema.define(version: 20140212063008) do
   add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
   add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
-
-  create_table "activity_streams", force: true do |t|
-    t.integer  "activity_streamable_id"
-    t.string   "activity_streamable_type"
-    t.string   "action"
-    t.integer  "operation_id"
-    t.string   "operation_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "announcements", force: true do |t|
     t.string   "title"
@@ -87,6 +77,7 @@ ActiveRecord::Schema.define(version: 20140212063008) do
     t.boolean  "draft",              default: false
     t.string   "time_zone",          default: "UTC"
     t.boolean  "canceled",           default: false
+    t.string   "content_type"
   end
 
   add_index "articles", ["categories"], name: "index_articles_on_categories", using: :gin
@@ -343,6 +334,7 @@ ActiveRecord::Schema.define(version: 20140212063008) do
     t.integer  "rolable_id"
     t.string   "rolable_type"
     t.string   "type"
+    t.string   "name"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "avatar_file_name"
@@ -359,8 +351,6 @@ ActiveRecord::Schema.define(version: 20140212063008) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.string   "time_zone",              default: "UTC"
-    t.string   "subdomain"
-    t.string   "user_name"
   end
 
   add_index "users", ["email", "provider"], name: "index_users_on_email_and_provider", unique: true, using: :btree
@@ -399,7 +389,7 @@ ActiveRecord::Schema.define(version: 20140212063008) do
   create_table "visits", force: true do |t|
     t.integer "visitable_id"
     t.string  "visitable_type"
-    t.integer "page_views"
+    t.integer "page_views",     default: 0
   end
 
   add_index "visits", ["visitable_id", "visitable_type"], name: "index_visits_on_visitable_id_and_visitable_type", using: :btree
