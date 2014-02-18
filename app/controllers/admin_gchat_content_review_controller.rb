1a357786a1979965650085acc892d91c1bb551a6
tony <tony_doofy@163.com>
2014-02-18 17:27:54 +0800

    聊天室内容审核2

--- app/controllers/admin_gchat_content_review_controller.rb
+++ app/controllers/admin_gchat_content_review_controller.rb
@@ -5,10 +5,16 @@ class AdminGchatContentReviewController < ApplicationController
   layout 'admin'
   
   def index
-    # hash = {$where:"this.txt.length > 30 && (this.txt[0] !='[') "}
+    # hash = "this.txt.length > 30 && this.txt[0] !='[' && this.del != true"
     # sort = {_id: -1}
     # @gchat_review =  paginate3("Gchat", params[:page], hash, sort)
-    @gchat_review = Gchat.where("this.txt.length > 30 && (this.txt[0] !='[') ").limit(50)
+    @gchat_review = Gchat.where("this.txt.length > 30 && this.txt[0] !='[' && this.del != true").limit(50)
+  end
+
+  def ajax_pb
+    gchat = Gchat.find_by_id(params[:id])
+    gchat.set(:del, true)
+    render :json => {}
   end
 
 end
