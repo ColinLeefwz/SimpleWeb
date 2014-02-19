f9a880623ad28db4014a8a65ee89279bf01ca777
tony <tony_doofy@163.com>
2014-02-19 11:52:17 +0800

    图片配文字，文字大于30字数，屏蔽图片和文字

--- app/controllers/admin_gchat_content_review_controller.rb
+++ app/controllers/admin_gchat_content_review_controller.rb
@@ -5,7 +5,7 @@ class AdminGchatContentReviewController < ApplicationController
   layout 'admin'
   
   def index
-    hash = "(this.txt.length > 30 && this.txt[0] !='[' && this.del != true) || (this.txt[0] == '[' && this.txt[1] == 'i')"
+    hash = "(this.txt.length > 30 && this.txt[0] !='[' && this.del != true) || (this.txt[0] == '[' && this.txt[1] == 'i' && this.txt.length > 60)"
     sort = {_id: -1}
     @gchat_review =  paginate3("Gchat", params[:page], hash, sort)
     # @gchat_review = Gchat.where("(this.txt.length > 30 && this.txt[0] !='[' && this.del != true) || this.txt[0] == '['").limit(50)
@@ -14,6 +14,15 @@ class AdminGchatContentReviewController < ApplicationController
   def ajax_pb
     gchat = Gchat.find_by_id(params[:id])
     gchat.set(:del, true)
+    render :json => {}
+  end
+
+  def ajax_pb_and_photo
+    gchat = Gchat.find_by_id(params[:id])
+    pid = gchat.txt.slice(5,24)
+    gchat.set(:del, true)
+    photo = Photo.find_by_id(pid)
+    photo.set(:hide, true) if photo
     render :json => {}
   end
 
