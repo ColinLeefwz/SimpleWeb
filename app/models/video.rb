7539892541cd16733d6cf8b192e60dfb2dc0b4ff
Gecko-Gecko <zjut.fmr@gmail.com>
2014-02-21 14:57:23 +0800

    [fix] Video Model: only copy_and_delete if file has been changed

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,5 @@
 class Video < ActiveRecord::Base
-  belongs_to :videoable
+  belongs_to :videoable, polymorphic: true
 
   has_attached_file :SD, path: ":class/:attachment/:id/:filename"
   has_attached_file :HD, path: ":class/:attachment/:id/:filename"
@@ -10,32 +10,39 @@ class Video < ActiveRecord::Base
   private
   #todo: add Exception Handle
   def paperclip_path
+    bucket = get_bucket
 
-    #SD
-    expect_path = "videos/sds/#{self.id}/#{self.SD_file_name}"
-    actual_path = get_actual_path(self.SD_file_path)
-    copy_and_delete(actual_path, expect_path) if (actual_path != expect_path)
+    %w(SD HD).each do |definition|
+      raw_path = get_raw_path(self.send(definition+"_file_path"))
+      object = bucket.objects[raw_path]
 
-    #HD
-    expect_path = "videos/hds/#{self.id}/#{self.HD_file_name}"
-    actual_path = get_actual_path(self.HD_file_path)
-    copy_and_delete(actual_path, expect_path) if (actual_path != expect_path)
+      if object.exists?
+        object.copy_to get_destination(definition)
+        object.delete
+      end
+    end
   end
 
-  def copy_and_delete(actual_path, expect_path)
+
+  def get_bucket
+    return @bucket if defined?(@bucket)
     s3 = AWS::S3.new
-    bucket = s3.buckets[ENV["AWS_BUCKET"]]
-
-    destination = bucket.objects[expect_path]
-    source = bucket.objects[actual_path]
-
-    source.copy_to(destination)
-    source.delete
+    @bucket = s3.buckets[ENV["AWS_BUCKET"]]
   end
 
-  def get_actual_path(path)
+
+  def get_raw_path(path)
     path = (CGI.unescape path).split("/")
     2.times{ path.shift() }
     path = path.join("/")
   end
+
+
+  def get_destination(definition)
+    expect_path = "videos/#{definition.downcase.pluralize}/#{self.id}/#{self.send(definition+'_file_name')}"
+    bucket = get_bucket
+    destination = bucket.objects[expect_path]
+  end
+
 end
+
