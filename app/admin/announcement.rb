45d1205e4836b04186675d8a68320fa668c2324d
Gecko-Gecko <zjut.fmr@gmail.com>
2014-02-22 17:50:32 +0800

    [refactor] announcement use `video`

--- app/admin/announcement.rb
+++ app/admin/announcement.rb
@@ -13,6 +13,11 @@ ActiveAdmin.register Announcement do
     default_actions
   end
 
-  permit_params :title, :language, :cover, :description, :always_show, :expert_id, {categories:[]}, :attached_video_hd_file_name, :attached_video_hd_content_type, :attached_video_hd_file_size, :hd_url, :attached_video_sd_file_name, :attached_video_sd_content_type, :attached_video_sd_file_size, :sd_url, :id
+  controller do
+    def permitted_params
+      params.permit :id, announcement: [:id, :title, :language, :cover, :description, :always_show, :expert_id, {categories:[]}, 
+                                        video_attributes: [:id, :cover, :SD_file_name, :SD_content_type, :SD_file_size, :SD_temp_path,  :HD_file_name, :HD_content_type, :HD_file_size, :HD_temp_path]]
+    end
+  end
 
 end
--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -4,10 +4,12 @@ class Announcement < ActiveRecord::Base
   belongs_to :expert
   validates :expert, presence: true
 
+  has_one :video, as: :videoable
+  accepts_nested_attributes_for :video, allow_destroy: true
+
   has_attached_file :cover
 
   def has_video_to_present?
-    self.attached_video_sd_file_name.present? || self.attached_video_hd_file_name.present?
+    self.video.try(:SD_file_name) || self.video.try(:HD_file_name)
   end
-
 end
