edc2692c0676d4dd036c6b7acfd7a5dc89b8038d
ColinLLL <colin@originatechina.com>
2014-02-24 14:34:54 +0800

    [feature] add notice and follow links

--- app/helpers/shares_helper.rb
+++ app/helpers/shares_helper.rb
@@ -19,4 +19,18 @@ module SharesHelper
     shares = JSON.parse json_content.read
     shares['count'] || 0
   end
+
+  def copyright_notice_year_range(start_year)
+    start_year = start_year.to_i
+
+    current_year = Time.new.year
+
+    if current_year > start_year && start_year > 2000
+      "#{start_year} - #{current_year}"
+    elsif start_year > 2000
+      "#{start_year}"
+    else
+      "#{current_year}"
+    end
+  end
 end
