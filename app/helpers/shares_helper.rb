7bd18a2f7a01147b0fb18fb7dd1ac25bb6c92764
peterzd <zdsunshine0640@gmail.com>
2014-02-25 10:27:43 +0800

    [chore]little change for copyriht helper

--- app/helpers/shares_helper.rb
+++ app/helpers/shares_helper.rb
@@ -25,10 +25,8 @@ module SharesHelper
 
     current_year = Time.new.year
 
-    if current_year > start_year && start_year > 2000
+    if current_year > start_year
       "#{start_year} - #{current_year}"
-    elsif start_year > 2000
-      "#{start_year}"
     else
       "#{current_year}"
     end
