b14e8b5e6665547d3e9057369ac9eae9c2cef001
peterzd <zdsunshine0640@gmail.com>
2014-02-27 20:57:39 +0800

    [feature]add association for consultations and users

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,4 +1,4 @@
 class Consultation < ActiveRecord::Base
-  belongs_to :requester
-  belongs_to :consultant
+  belongs_to :requester, class_name: "User"
+  belongs_to :consultant, class_name: "User"
 end
--- spec/models/consultation_spec.rb
+++ spec/models/consultation_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Consultation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  helper_objects
+
+  describe "relationship with users" do
+    before :each do
+      @consultation = create(:consultation,  consultant: sameer,requester: jevan, description: "jevan's consultation to sameer")
+    end
+
+    context "member sends consultation" do
+      it "requester is the member" do
+        expect(@consultation.requester).to eq jevan
+      end
+
+      it "the member has the consultation" do
+        expect(jevan.sent_consultations).to include @consultation
+      end
+
+    end
+
+    context "expert receives consultation" do
+      it "consultant is the expert" do
+        expect(@consultation.consultant).to eq sameer
+      end
+
+      it "the expert has the consultation" do
+        expect(sameer.received_consultations).to include @consultation
+      end
+    end
+  end
 end
