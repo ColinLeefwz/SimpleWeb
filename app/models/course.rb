4f30e93564b2d05d9778bec17f68dc57b86b673d
peterzd <zdsunshine0640@gmail.com>
2014-02-27 15:52:39 +0800

    [fix]expert enroll his own courses after create the course

--- app/models/course.rb
+++ app/models/course.rb
@@ -24,7 +24,7 @@ class Course < ActiveRecord::Base
 
   has_attached_file :cover
 
-  after_create :create_a_video
+  after_create :create_a_video, :expert_enrolled_own
 
   class << self
     def recommend_courses(current_user)
@@ -63,5 +63,11 @@ class Course < ActiveRecord::Base
   private
   def create_a_video
     self.create_video
+  end
+
+  def expert_enrolled_own
+    self.experts.each do |exp|
+      exp.enroll self
+    end
   end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -5,6 +5,7 @@ describe Course do
 
   describe "#recommended_courses" do
     before :each do
+      Expert.delete_all
       @courses = create_list(:course, 5, title: "course", experts: [sameer], categories: ["culture"])
       @staff_course = create(:course, title: "staff course", experts: [staff], categories: ["culture"])
     end
@@ -38,6 +39,13 @@ describe Course do
       it "excludes his own courses" do
         expect(Course.recommend_courses(sameer)).not_to include @courses
       end
+    end
+  end
+
+  describe "expert should auto enroll his own courses" do
+    it "enrolled his own course after create the course" do
+      first_course
+      expect(sameer.enrolled_courses).to include first_course
     end
   end
 
