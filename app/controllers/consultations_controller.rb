5de3394fb39bf92e5447a5f1a132075fa0e17b2b
peterzd <zdsunshine0640@gmail.com>
2014-03-03 19:51:30 +0800

    [feature]user can send consultation to expert
    Note: we use AJAX to create the consultation form

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,7 +1,22 @@
 class ConsultationsController < ApplicationController
   def new
+    @consultation = Consultation.new(consultant: Expert.find(params[:expert]), requester: current_user)
+    respond_to do |format|
+      format.js { render 'new_consultation' }
+    end
+
   end
 
   def create
+    @consultation = Consultation.new consultation_params
+    if @consultation.save
+      #TODO: Peter at 2014-03-02: redirect or show something
+      logger.info "created a new consultation"
+    end
+  end
+
+  private
+  def consultation_params
+    params.require(:consultation).permit(:id, :requester_id, :consultant_id, :description)
   end
 end
