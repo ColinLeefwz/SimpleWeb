7ab810084c00098121345737c4ec2e953dec4bfa
Gecko-Gecko <zjut.fmr@gmail.com>
2014-03-04 14:17:41 +0800

    [refactor] use conditional callback
    - separately for SD and HD video

--- app/models/video.rb
+++ app/models/video.rb
@@ -10,7 +10,8 @@ class Video < ActiveRecord::Base
   has_attached_file :HD, path: ":class/:id/:attachment/:filename", default_url: ""
 
   after_initialize :get_current_path
-  after_save :paperclip_path
+  after_save :sd_paperclip_path, if: ->{@SD_temp_path.present?}
+  after_save :hd_paperclip_path, if: ->{@HD_temp_path.present?}
 
 
   def available?
@@ -24,7 +25,35 @@ class Video < ActiveRecord::Base
     @HD_current_path = /videos\/\d+\/hds\/.+/.match CGI.unescape(self.HD.url)
   end
 
+  
+  #todo: add Exception Handle
+  def sd_paperclip_path
+    @bucket ||= AWS::S3.new.buckets[ENV["AWS_BUCKET"]]
+    @bucket.objects[@SD_current_path].delete if @SD_current_path.present?
 
+    expect_path = "videos/#{self.id}/sds/#{self.SD_file_name}"
+    copy_and_delete(@SD_temp_path, expect_path)
+  end
+
+
+  def hd_paperclip_path
+    @bucket ||= AWS::S3.new.buckets[ENV["AWS_BUCKET"]]
+    @bucket.objects[@HD_current_path].delete if @HD_current_path.present?
+    
+    expect_path = "videos/#{self.id}/hds/#{self.HD_file_name}"
+    copy_and_delete(@HD_temp_path, expect_path)
+  end
+
+
+  # helper methods
+  def copy_and_delete(temp_path, expect_path)
+    temp_path = chop_bucket CGI.unescape(temp_path)
+
+    temp = @bucket.objects[temp_path]
+    expect = @bucket.objects[expect_path]
+
+    temp.copy_to(expect)
+    temp.delete
   end
 
   def chop_bucket(path)
@@ -33,37 +62,6 @@ class Video < ActiveRecord::Base
     path = path.split("/")
     2.times{ path.shift() }
     path = path.join("/")
-  end
-
-
-
-  #todo: add Exception Handle
-  def paperclip_path
-    @bucket = AWS::S3.new.buckets[ENV["AWS_BUCKET"]]
-    #SD
-    if @SD_temp_path.present?
-      @bucket.objects[@SD_current_path].delete if @SD_current_path.present?
-
-      expect_path = "videos/#{self.id}/sds/#{self.SD_file_name}"
-      copy_and_delete(@SD_temp_path, expect_path)
-    end
-
-    #HD
-    if @HD_temp_path.present?
-      @bucket.objects[@HD_current_path].delete if @HD_current_path.present?
-
-      expect_path = "videos/#{self.id}/hds/#{self.HD_file_name}"
-      copy_and_delete(@HD_temp_path, expect_path)
-    end
-  end
-
-
-  def copy_and_delete(temp_path, expect_path)
-    temp = @bucket.objects[temp_path]
-    expect = @bucket.objects[expect_path]
-
-    temp.copy_to(expect)
-    temp.delete
   end
 end
 
