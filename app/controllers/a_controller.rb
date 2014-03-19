2aa251e6f6ea0f73aa3596cd922a38d97b2918f6
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2014-03-19 14:58:16 +0800

    m

--- app/controllers/a_controller.rb
+++ app/controllers/a_controller.rb
@@ -5,7 +5,7 @@ class AController < ApplicationController
   before_filter :weixin_filter, :only => [:index, :webdown]
   
   #$apk_url = "http://dd.myapp.com/16891/external_EC325DF2C79795CCE3725D873B97B775.apk"  #二次扫描不行  
-  $apk_url = "http://android.myapp.com/android/down.jsp?appid=1064735&pkgid=17623821&icfa=-1&lmid=1022&g_f=990976&actiondetail=0&downtype=1&softname=%E8%84%B8%E8%84%B8"
+  $apk_url = "http://android.myapp.com/android/down.jsp?appid=1064735&pkgid=17697967&icfa=-1&lmid=1022&g_f=990976&actiondetail=0&downtype=1&transactionid=1395212192718252&topicid=-1&softname=%E8%84%B8%E8%84%B8"
   
   $ios_url = "https://itunes.apple.com/cn/app/lianlian/id577710538"
   
