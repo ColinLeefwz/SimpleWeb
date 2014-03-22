2cca51e2bb77e5b86ac9188b378b9ec549aab0c9
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2014-03-22 20:53:10 +0800

    短信http请求失败重发一次

--- app/workers/sms_sender.rb
+++ app/workers/sms_sender.rb
@@ -23,16 +23,23 @@ class SmsSender
     end
   end
   
-  def self.send_sms_ihuiyi(phone, text)
+  def self.send_sms_ihuiyi(phone, text, rtry=1)
       return true if ENV["RAILS_ENV"] != "production"
       pass = URI.escape("www.dface.cn20130709")
-      info = RestClient.get "http://106.ihuyi.com/webservice/sms.php?method=Submit&account=cf_llh&password=#{pass}&mobile=#{phone}&content=#{URI.escape(text)}"
-      match = info.index("<code>2</code>")
-      return true if match && match>0
-      Xmpp.error_notify("ihuiyi短信错误：#{Time.now},#{phone_operator(phone)} #{phone}")
-      Xmpp.error_notify(info.encode('utf-8','gbk',{:invalid => :replace}))
-      Xmpp.error_notify(info)
-      return nil
+      begin
+        info = RestClient.get "http://106.ihuyi.com/webservice/sms.php?method=Submit&account=cf_llh&password=#{pass}&mobile=#{phone}&content=#{URI.escape(text)}"
+        match = info.index("<code>2</code>")
+        return true if match && match>0
+      rescue
+      end
+      if rtry>0
+        return send_sms_ihuiyi(phone, text,0)
+      else
+        Xmpp.error_notify("ihuiyi短信错误：#{Time.now},#{phone_operator(phone)} #{phone}")
+        Xmpp.error_notify(info.encode('utf-8','gbk',{:invalid => :replace}))
+        Xmpp.error_notify(info)
+        return nil
+      end
   end
   
   def self.ihuiyi_remain
@@ -41,24 +48,38 @@ class SmsSender
     
   end
   
-  def self.send_sms_xuanwu(phone, text)
+  def self.send_sms_xuanwu(phone, text, rtry=1)
       return true if ENV["RAILS_ENV"] != "production"
       text += "“回复TD退订”" if is_dianxin(phone)
       str = URI.encode(text.encode('gbk','utf-8'))
-      info = RestClient.get "http://211.147.239.62:9050/cgi-bin/sendsms?username=cadmin@zjll&password=Dface.cn1234&to=#{phone}&text=#{str}&msgtype=1"
-      return true if info=="0"
-      Xmpp.error_notify("玄武短信错误：#{Time.now},#{info}")
-      return nil
+      begin
+        info = RestClient.get "http://211.147.239.62:9050/cgi-bin/sendsms?username=cadmin@zjll&password=Dface.cn1234&to=#{phone}&text=#{str}&msgtype=1"
+        return true if info=="0"
+      rescue
+      end
+      if rtry>0
+        return send_sms_xuanwu(phone, text,0)
+      else
+        Xmpp.error_notify("玄武短信错误：#{Time.now},#{info}")
+        return nil
+      end
   end
     
-  def self.send_sms_mandao(phone, text)
+  def self.send_sms_mandao(phone, text, rtry=1)
       return true if ENV["RAILS_ENV"] != "production"
       str = text.encode('gbk','utf-8')
-      info = RestClient.post "http://sdk2.entinfo.cn:8060/webservice.asmx/SendSMS", 
-        :sn => "SDK-NSF-010-00034", :pwd => "DD07e9-8", :mobile => phone, :content => str
-      return true if info.index(">0 ")
-      Xmpp.error_notify("漫道短信错误：#{Time.now},#{info}")
-      return nil
+      begin
+        info = RestClient.post "http://sdk2.entinfo.cn:8060/webservice.asmx/SendSMS", 
+          :sn => "SDK-NSF-010-00034", :pwd => "DD07e9-8", :mobile => phone, :content => str
+        return true if info.index(">0 ")
+      rescue
+      end
+      if rtry>0
+        return send_sms_mandao(phone, text,0)
+      else
+        Xmpp.error_notify("漫道短信错误：#{Time.now},#{info}")
+        return nil
+      end
   end
   
   #sleep 30;["18665313145","18679426779","15158000625","15397888138","18910803153"].each {|phone| SmsSender.send_sms_xuanwu(phone,"测试#{Time.now.to_s}")}
