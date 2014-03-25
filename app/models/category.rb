6d926905a1fa375f5c2c0fb919670cd2dedcf5ba
Gecko-Gecko <zjut.fmr@gmail.com>
2014-03-25 14:57:37 +0800

    [fix] category.count
    - delegate to categorisation to get count of items in a particular
    category

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,6 @@ class Category < ActiveRecord::Base
   has_many :video_interviews, through: :categorization, source: :categoriable, source_type: "VideoInterview"
   has_many :announcements, through: :categorization, source: :categoriable, source_type: "Announcement"
   has_many :courses, through: :categorization, source: :categoriable, source_type: "Course"
+
+  delegate :count, to: :categorization
 end
