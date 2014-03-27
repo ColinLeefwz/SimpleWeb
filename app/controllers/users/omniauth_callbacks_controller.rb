bd4dbadcc11da90af8b8f53d50a621be12b26596
Gecko-Gecko <zjut.fmr@gmail.com>
2014-03-27 16:02:08 +0800

    [chore] indentation

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,28 +1,29 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-	def facebook
-		@user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+  def facebook
+    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
 
-		if @user.persisted?
-			sign_in @user
+    if @user.persisted?
+      sign_in @user
       @user.update_attributes type: "Member"
-			redirect_to cookies[:previous_path]
+      redirect_to cookies[:previous_path]
 
-		else
-			session["devise.facebook_data"] = request.env["omniauth.auth"]
-			redirect_to new_user_registration_url
-		end
-	end
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
 
   def linkedin
     @user = User.find_for_linkedin(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
-			sign_in @user
+      sign_in @user
       @user.update_attributes type: "Member"
-			redirect_to cookies[:previous_path]
+      redirect_to cookies[:previous_path]
     else
       session["devise.linkedin.data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
+
 end
