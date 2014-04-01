4058fe76c262651896811afb01a0a838c04d2ad3
Gecko-Gecko <zjut.fmr@gmail.com>
2014-04-01 23:00:08 +0800

    [refactor] UserSubscription use `toggle` instead of separate `create` and `destroy`
    - also take the responsibility of update user status from
    MailChimp_Controller

--- app/models/users/user_subscription.rb
+++ app/models/users/user_subscription.rb
@@ -6,29 +6,35 @@ class UserSubscription
 
   def initialize(user, list_id)
     @user = user
-    @mc = Mailchimp::API.new ENV["MAILCHIMP_API_KEY"]
+    @mail_chimp = Mailchimp::API.new ENV["MAILCHIMP_API_KEY"]
     @list_id = list_id
   end
 
-  def create
+
+  def toggle
     begin
-      @mc.lists.subscribe(@list_id, {email: @user.email}, {FNAME: @user.first_name, LNAME: @user.last_name})
-      @message = "High Fives! Subscribed successfully! Please confirm the email sending to you"
+      @user.subscribed ? destroy : create
     rescue Mailchimp::ListAlreadySubscribedError
       @message = "You are already subscribed."
-    rescue Mailchimp::Error
-      @message = "We are sorry, but something went wrong."
-    end
-  end
-
-  def destroy
-    begin
-      @mc.lists.unsubscribe(@list_id, {email: @user.email})
-      @message = "Unsubscribed."
     rescue Mailchimp::EmailAlreadyUnsubscribedError
       @message = "You have already unsubscribed."
     rescue Mailchimp::Error
       @message = "We are sorry, but something went wrong."
     end
   end
+
+
+  private
+  def create
+    @mail_chimp.lists.subscribe(@list_id, {email: @user.email}, {FNAME: @user.first_name, LNAME: @user.last_name})
+    @user.update_attributes(subscribe_newsletter: true) # guest return nil because of method_missing
+    @message = "High Fives! Subscribed successfully! Please confirm the email sending to you"
+  end
+
+  def destroy
+    @mail_chimp.lists.unsubscribe(@list_id, {email: @user.email})
+    @user.update_attributes(subscribe_newsletter: false)
+    @message = "Unsubscribed."
+  end
+
 end
