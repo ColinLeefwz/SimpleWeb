6fe7c200ae7c3a0904082ebb2e62687f99f7532c
peterzd <zdsunshine0640@gmail.com>
2014-04-03 15:28:57 +0800

    [chore]remove i18n from config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,11 +13,8 @@ module Prodygia
     config.autoload_paths += Dir[Rails.root.join('app', 'controllers', '{**}')]
     config.autoload_paths += Dir[Rails.root.join('lib', '{**}')]
 
-    config.i18n.enforce_available_locales = true
-    config.i18n.default_locale = :de
-
-		config.assets.precompile += Ckeditor.assets
-		config.assets.precompile += %w(ckeditor/*)
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w(ckeditor/*)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
