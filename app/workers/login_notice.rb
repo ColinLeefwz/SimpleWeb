d506336ead31eeb14c3217b484c6302f9cc95532
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2014-04-06 16:55:10 +0800

    m

--- app/workers/login_notice.rb
+++ app/workers/login_notice.rb
@@ -13,7 +13,7 @@ class LoginNotice
   def self.save_session(uid, session_id)
     begin
       old = SessionUser.find(session_id)
-      if old.uid == uid
+      if old.uid.to_s == uid.to_s
         Xmpp.error_notify("用户：#{old.user.name} 重复登录. 注册时间#{old.user.cat},上次登录时间#{old.cats},当前时间 #{Time.now}")
       else
         Xmpp.error_notify("同一个session：#{session_id},不同的用户：#{old.user.name} , #{User.find_by_id(uid).name}")
