ab6c956a394276e2f38722d734ae57f34b4cbc50
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2014-04-06 15:49:30 +0800

    m

--- app/workers/login_notice.rb
+++ app/workers/login_notice.rb
@@ -4,14 +4,26 @@ class LoginNotice
   @queue = :normal
 
   def self.perform(uid, session_id, reg=false)
+    save_session(uid,session_id)
+    #user = User.find_by_id(uid)
+    #cpd = CouponDown.where({uid:user.id, :uat => {"$exists" => false} }).last
+    #cpd.xmpp_send if cpd
+  end
+  
+  def self.save_session(uid, session_id)
+    begin
+      old = SessionUser.find(session_id)
+      if old.uid != uid
+        Xmpp.error_notify("同一个session：#{session_id},不同的用户：#{old.user.name} , #{User.find_by_id(uid).name}")
+      end
+      return
+    rescue
+    end
     su = SessionUser.new
     su._id = session_id
     su.uid = uid
     su.cat = Time.now.to_i
     su.save!
-    #user = User.find_by_id(uid)
-    #cpd = CouponDown.where({uid:user.id, :uat => {"$exists" => false} }).last
-    #cpd.xmpp_send if cpd
   end
   
 end
