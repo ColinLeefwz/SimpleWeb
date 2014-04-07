0aefc201798de7ba2749f4dade919d13f313da57
peterzd <zdsunshine0640@gmail.com>
2014-04-07 19:45:13 +0800

    [chore]add courses to landing page.Hide not used code for load_more

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,29 +11,31 @@ class WelcomeController < ApplicationController
     video_interviews = VideoInterview.includes(:expert)
     annoucements = Announcement.includes(:expert)
     articles = Article.includes(:expert).where(draft: false)
-    collection = video_interviews + annoucements + articles
+    courses = Course.all
+    collection = video_interviews + annoucements + articles + courses
     @items = collection.sort{|x, y| y.updated_at <=> x.updated_at}
     @show_category = true
   end
 
-  def load_more
-    point = cookies[:landing_batch_point].to_i
-    @items = Landingitem.all_index_items(point)
-    respond_to do |format|
-      if Landingitem.next(point + 1)
-        cookies[:no_more_load] = false
-      else
-        cookies[:no_more_load] = true
-      end
-      increase_cookie
-      format.js { render "load_more" }
-    end
-  end
+  # Peter at 2014-04-07: not used when we hide the load_more function
+  # def load_more
+  #   point = cookies[:landing_batch_point].to_i
+  #   @items = Landingitem.all_index_items(point)
+  #   respond_to do |format|
+  #     if Landingitem.next(point + 1)
+  #       cookies[:no_more_load] = false
+  #     else
+  #       cookies[:no_more_load] = true
+  #     end
+  #     increase_cookie
+  #     format.js { render "load_more" }
+  #   end
+  # end
 
-  private
-  def increase_cookie
-    new_val = cookies[:landing_batch_point].to_i + 1
-    cookies[:landing_batch_point] = new_val
-  end
+  # private
+  # def increase_cookie
+  #   new_val = cookies[:landing_batch_point].to_i + 1
+  #   cookies[:landing_batch_point] = new_val
+  # end
 
 end
