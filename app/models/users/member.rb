b06d99d0a99ad2e02e346b60c4d4619232905657
peterzd <zdsunshine0640@gmail.com>
2014-04-18 18:15:54 +0800

    [chore]move logic code to model/member.rb

--- app/models/users/member.rb
+++ app/models/users/member.rb
@@ -2,4 +2,21 @@ class Member < User
   def delete(object)
     object.destroy
   end
+
+  def tutorial_video_sd_url
+    if self.is_a? Expert
+      return "https://s3-us-west-1.amazonaws.com/prodygia-videos/videos/62/sds/tutorial_expert_dashboard_kbps600.mp4"
+    else
+      return "https://s3-us-west-1.amazonaws.com/prodygia-videos/videos/61/sds/tutorial_member_dashboard_kbps600.mp4"
+    end
+  end
+
+  def tutorial_video_hd_url
+    if self.is_a? Expert
+      return "https://s3-us-west-1.amazonaws.com/prodygia-videos/videos/62/hds/tutorial_expert_dashboard.mp4"
+    else
+      return "https://s3-us-west-1.amazonaws.com/prodygia-videos/videos/61/hds/tutorial_member_dashboard.mp4"
+    end
+  end
+
 end
