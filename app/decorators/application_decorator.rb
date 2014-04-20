18395848216127ef5e54e121e9fd4550e83abd32
peterzd <zdsunshine0640@gmail.com>
2014-04-20 20:04:54 +0800

    [refactor]move "get_box_class" to decorator

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -9,4 +9,12 @@ class ApplicationDecorator < Draper::Decorator
     images = { "LiveSession" => "livestreaming.png", "Article" => "text.png", "VideoInterview" => "video_interview_icon.png", "Announcement" => "announcement.png", "Course" => "video_course_icon.png" }
     images[object.class.name]
   end
+
+  def get_box_class
+    box_class = " item " + object.categories.pluck(:name).join(" ") + " "
+    box_class += object.class.name
+    box_class += " always_show" if object.try(:always_show)
+
+    box_class.downcase()
+  end
 end
