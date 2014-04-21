10c7ee73e263e7435d5174be2d4bb7abb016c43a
ColinLLL <colin@originatechina.com>
2014-04-21 15:27:10 +0800

    [refactor] take the course method into decorate

--- app/decorators/course_decorator.rb
+++ app/decorators/course_decorator.rb
@@ -1,3 +1,11 @@
 class CourseDecorator < ApplicationDecorator
   delegate_all
+
+  def producer_links
+    expert_links = []
+    object.experts.each do |expert|
+      expert_links << link_to(expert.name, profile_expert_path(expert))
+    end
+    raw "by #{expert_links.join(" and ")}"
+  end
 end
--- app/helpers/course_helper.rb
+++ app/helpers/course_helper.rb
@@ -1,9 +1,2 @@
 module CourseHelper
-  def producer_links(course)
-    expert_links = []
-    course.experts.each do |expert|
-      expert_links << link_to(expert.name, profile_expert_path(expert))
-    end
-    raw "by #{expert_links.join(" and ")}"
-  end
 end
