284f42146c12707e834d265f9d18993bae23592b
glh1991 <ai_haige@126.com>
2014-04-21 16:39:47 +0800

    添加取消删除方法

--- app/controllers/admin_user_reports_controller.rb
+++ app/controllers/admin_user_reports_controller.rb
@@ -2,10 +2,29 @@
 class AdminUserReportsController < ApplicationController
   include Paginate
   before_filter :admin_authorize
-  layout "admin"
+  layout "report"
 
   def index
-    hash, sort ={:flag => nil} , {}
+    hash, sort = {flag: nil}, {}
+    case params[:type]
+    when '1'
+      hash.merge!({type: "地点位置错误"})
+    when '2'
+      hash.merge!({type: "地点信息错误"})
+    when '3'
+      hash.merge!({type: "地点不存在"})
+    when '4'
+      hash.merge!({type: "地点重复"})
+    end
+
+    case params[:flag]
+    when '0'
+      hash.merge!({flag: {"$exists" => false}})
+    when '1'
+      hash.merge!({flag: 1})
+    when '2'
+      hash.merge!({flag: 2})
+    end
 
     @shop_reports = paginate3('shop_report', params[:page], hash, sort)
   end
@@ -26,7 +45,7 @@ class AdminUserReportsController < ApplicationController
       @shop.lob = lobs.inject([]){|f,s| f << s.split(/[,，]/).map { |m| m.to_f  }.reverse}
       @shop.lo = @shop.lob.map{|m| Shop.lob_to_lo(m)}
       @shop.unset(:lob)
-      
+
     end
 
     unless params[:shop][:addr].blank?
@@ -50,18 +69,55 @@ class AdminUserReportsController < ApplicationController
 
   end
 
+  def update_corrdinate
+    @shop_report = ShopReport.find(params[:id])
+    @shop = @shop_report.shop
+    if @shop.update_attribute(:lo, params[:lo][1..-2].split(",").map{|i| i.to_f})
+      @shop_report.update_attribute(:flag, 1)
+      render json: {"success" => true, "lo" => @shop.lo}
+    end
+  end
+
+  def ignore
+    @shop_report = ShopReport.find(params[:report_id])
+    if @shop_report.update_attribute(:flag, 2)
+      render json: {"success" => true}
+    end
+  end
+
+  def repot_del
+    @shop_report = ShopReport.find(params[:id])
+    @shop = @shop_report.shop
+    if @shop_report.update_attribute(:flag, 1)
+      @shop.shop_del
+      @shop.update_attribute(:i, true)
+      redirect_to action: "index"
+    end
+  end
+
+  def modify_location
+    @report = ShopReport.find(params[:id])
+  end
+
+  def modify_infor
+
+  end
+
+  def repeat
+    @shop_report = ShopReport.find(params[:id])
+    @shop = @shop_report.shop
+    @simaliar_shops = Shop.similar_shops(@shop)
+  end
+
+  def confirm
+    @shop_report = ShopReport.find(params[:id])
+    @shop_report.update_attribute(:flag, 1)
+    redirect_to action: "index"
+  end
+
   def post_chat
     Xmpp.send_chat($dduid, params[:to_uid], params[:text])
     render :text => "消息已发送"
-  end
-
-  def ajax_del
-    shop = Shop.find(params[:sid])
-    shop.shop_del
-    ShopReport.where({:sid => params[:sid].to_i, :flag => nil}).each do |report|
-      report.update_attribute(:flag, 1)
-    end
-    render :json => ''
   end
 
   def ajax_distort
@@ -71,5 +127,5 @@ class AdminUserReportsController < ApplicationController
     render :json => ''
   end
 
-  
+
 end
