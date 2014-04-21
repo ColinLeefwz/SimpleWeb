a54465dae79a0aaf79a44056e0f80163544a3b9c
袁新宇 <yuanxinyu.hangzhou@gmail.com>
2014-04-21 22:23:29 +0800

    我的地点的文档

--- app/controllers/my_loc_controller.rb
+++ app/controllers/my_loc_controller.rb
@@ -24,14 +24,14 @@ class MyLocController < ApplicationController
        loc =  MyLoc.new
        loc._id = session_user._id
     end
-    if parmas[:type]=="home"
+    if params[:type]=="home"
       loc.sid1 = params[:sid].to_i
-    elsif parmas[:type]=="school"
+    elsif params[:type]=="school"
       loc.sid2 = params[:sid].to_i
-    elsif parmas[:type]=="building"
+    elsif params[:type]=="building"
       loc.sid3 = params[:sid].to_i
     else
-      return render :json => {error:"错误的地点类型:#{parmas[:type]}"}.to_json
+      return render :json => {error:"错误的地点类型:#{params[:type]}"}.to_json
     end
     loc.save!
     render :json => {save:"ok"}.to_json
@@ -40,14 +40,14 @@ class MyLocController < ApplicationController
   def select_loc
     lo = [params[:lat].to_f , params[:lng].to_f]
     hash = {lo:{'$near' => lo,'$maxDistance' => 0.1}, del:{"$exists" => false}}
-    if parmas[:type]=="home"
+    if params[:type]=="home"
       hash.merge!( {t:11} )
-    elsif parmas[:type]=="school"
+    elsif params[:type]=="school"
       hash.merge!( {t:12} )
-    elsif parmas[:type]=="building"
+    elsif params[:type]=="building"
       hash.merge!( {t:10} )
     else
-      return render :json => {error:"错误的地点类型:#{parmas[:type]}"}.to_json
+      return render :json => {error:"错误的地点类型:#{params[:type]}"}.to_json
     end
     shops = Shop.where2(hash, {limit:100})
     ret = shops.map {|s| s.safe_output_with_users}
