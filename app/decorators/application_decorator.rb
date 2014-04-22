7ed4b80b7cb6b54c97540eea695018ecc85704a9
ColinLLL <colin@originatechina.com>
2014-04-22 17:11:08 +0800

    [refactor] take the method from share helpers

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,3 +1,37 @@
 class ApplicationDecorator < Draper::Decorator
   include BoxableDecorator
+
+  def self.copyright_notice_year_range(start_year)
+    start_year = start_year.to_i
+
+    current_year = Time.new.year
+
+    if current_year > start_year
+      "#{start_year} - #{current_year}"
+    else
+      "#{current_year}"
+    end
+  end
+
+  def self.model_count(klazz)
+    if klazz == "Course"
+      if h.current_user.is_a? Expert
+        Course.all.count
+      else
+        Course.all_without_staff.count
+      end
+    elsif klazz == "Article"
+      Article.non_draft.count
+    else
+      klazz.constantize.count
+    end
+  end
+
+  def self.all_count
+    all_count = 0
+    %w{Article VideoInterview Announcement Course}.each do |klazz|
+      all_count += model_count(klazz)
+    end
+    all_count
+  end
 end
