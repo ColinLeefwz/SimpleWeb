dd5b9fc6ebfe340e056cfd988ba8eb6c184c464a
Gecko-Gecko <zjut.fmr@gmail.com>
2014-04-23 17:16:00 +0800

    [chore] last_read_time for activity_stream

--- app/models/activity_stream/activity_stream.rb
+++ app/models/activity_stream/activity_stream.rb
@@ -2,6 +2,7 @@ class ActivityStream
   include Mongoid::Document
   include Mongoid::Timestamps
   field :user_id, type: Integer
+  field :last_read_time, type: DateTime
   embeds_many :activities
 
   validates :user_id, presence: true
