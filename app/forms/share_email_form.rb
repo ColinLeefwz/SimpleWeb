290e570396ad749244cba98059a73eecfd337729
peterzd <zdsunshine0640@gmail.com>
2014-04-23 17:11:43 +0800

    [feature]show new_share_email_form after click email icon

--- app/forms/share_email_form.rb
+++ app/forms/share_email_form.rb
@@ -3,12 +3,18 @@ class ShareEmailForm
 
   validates :to, presence: true
 
+  delegate :to, :subject, :message, to: :email_message
+
   def initialize(params = {})
     item_params = params[:item]
     item = item_params[:type].classify.constantize.find item_params[:id]
 
     @email_message = EmailMessage.new(subject: "share this to friend",
                                      message: "share this #{item.title}")
+  end
+
+  def email_message
+    @email_message ||= EmailMessage.new
   end
 
 
