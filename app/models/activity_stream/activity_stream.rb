36e8ae3921cc5444e8e03334b65841fa99ddf014
Gecko-Gecko <zjut.fmr@gmail.com>
2014-04-23 22:12:52 +0800

    Override ".find" in ActivityStream
    - so that it can also find by user_id

--- app/models/activity_stream/activity_stream.rb
+++ app/models/activity_stream/activity_stream.rb
@@ -3,9 +3,16 @@ class ActivityStream
   include Mongoid::Timestamps
   field :user_id, type: Integer
   field :last_read_time, type: DateTime
-  embeds_many :activities
+  embeds_many :activities, order: :created_at.desc
 
   validates :user_id, presence: true
 
+  def self.find(input)
+    begin
+      find_by(user_id: input)
+    rescue
+      super
+    end
+  end
 end
 
