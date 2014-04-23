7ad09f530198092e34452b0fb6abdcd2b3151f4e
Gecko-Gecko <zjut.fmr@gmail.com>
2014-04-23 22:12:19 +0800

    `#subject`, `#object` for Activity

--- app/models/activity_stream/activity.rb
+++ app/models/activity_stream/activity.rb
@@ -1,12 +1,20 @@
 class Activity
   include Mongoid::Document
   include Mongoid::Timestamps
-  field :subject_name, type: String
+  field :subject_type, type: String
   field :subject_id, type: Integer
   field :object_type, type: String
   field :object_id, type: Integer
   field :action, type: String
 
   embedded_in :activity_stream
+
+  def subject
+    subject_type.constantize.find(subject_id)
+  end
+
+  def object
+    object_type.constantize.find(object_id)
+  end
 end
 
