f72ae0b52f120d7430b64b990466939476705c4a
pulsen <huang123qwe@126.com>
2014-04-24 18:50:02 +0800

    微博匹配及测试

--- lib/weibo_api_test.rb
+++ lib/weibo_api_test.rb
@@ -13,14 +13,15 @@ end
 
 
 def relation
-    # wb_users = remote_users(token)
-    wb_users = remote_users($redis.get("wbtoken#{session[:user_id]}"))
+    wb_users = remote_users(token)
+    # wb_users = remote_users($redis.get("wbtoken#{session[:user_id]}"))
     users = User.where({wb_uid: {'$in' => wb_users.keys}})
+    return {error: 'token过期'} if wb_users['error']
     user_ids = users.distinct(:wb_uid)
     follow_ids = session_user.follow_ids
     to_invite = wb_users.keep_if{|k, v| !k.in?(user_ids)}.values
-    friend = users.select{|user| user.id.to_s.in?(follow_ids)}
-    to_add = users - friend
+    friend = users.select{|user| user.id.to_s.in?(follow_ids)}.map{|user| user.safe_output}
+    to_add = (users - friend).map{|user| user.safe_output}
     {to_add: to_add, friend: friend, to_invite: to_invite}
   end 
 
@@ -30,7 +31,10 @@ def relation
     url = "https://api.weibo.com/2/friendships/friends.json"
     hash= {access_token: token, uid: session_user.wb_uid, count: 200, cursor: cursor}
     begin
+      puts url+ "?" + hash.to_param
       JSON.parse RestClient.get(url+ "?" + hash.to_param)
+    rescue RestClient::BadRequest
+       {'error' => true}
     rescue 
       visit_remote(token, cursor, time+1)
     end
@@ -42,6 +46,7 @@ def relation
     cursor = 0
     while true
       wb_users = visit_remote(token, cursor)
+      return {'error' => true} if wb_users['error']
       wb_column = ['id', 'name', 'description', 'profile_image_url', 'gender','avatar_hd']
       wb_users['users'].each{|wb_user| users[wb_user['id'].to_s] = wb_user.select{|k,v| k.in?(wb_column)} }
       return users if (cursor = wb_users['next_cursor'].to_i) == 0 
