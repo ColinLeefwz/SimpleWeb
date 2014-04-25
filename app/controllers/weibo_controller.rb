32c53158a26d39ce316c5f9053d3d316df259e16
pulsen <huang123qwe@126.com>
2014-04-25 15:05:56 +0800

    微博接口

--- app/controllers/weibo_controller.rb
+++ app/controllers/weibo_controller.rb
@@ -7,12 +7,11 @@ class WeiboController < ApplicationController
     wb_users = remote_users($redis.get("wbtoken#{session[:user_id]}"))
     return render :json => {error: 'token过期'} if wb_users['error']
     users = User.where({wb_uid: {'$in' => wb_users.keys}})
-    user_ids = users.distinct(:wb_uid)
-    follow_ids = session_user.follow_ids
-    to_invite = wb_users.keep_if{|k, v| !k.in?(user_ids)}.values
-    friend = users.select{|user| user.id.to_s.in?(follow_ids)}.map{|user| user.safe_output}
-    to_add = (users - friend).map{|user| user.safe_output}
-    render :json => {to_add: to_add, friend: friend, to_invite: to_invite}.to_json
+    wb_uids = users.distinct(:wb_uid)
+    to_invite = wb_users.keep_if{|k, v| !k.in?(wb_uids) && v['follow_me']}.values
+    friend, to_add =[], []
+    users.each{|user| (user.id.to_s.in?(session_user.follow_ids) ? friend : to_add ) << user.safe_output}
+    render :json => {to_add: to_add, friend: friend, to_invite: to_invite}
   end 
 
   private
@@ -37,12 +36,11 @@ class WeiboController < ApplicationController
     while true
       wb_users = visit_remote(token, cursor)
       return {'error' => true} if wb_users['error']
-      wb_column = ['id', 'name', 'description', 'profile_image_url', 'gender','avatar_hd']
+      wb_column = ['id', 'name', 'description', 'profile_image_url', 'gender','avatar_hd', 'follow_me']
       wb_users['users'].each{|wb_user| users[wb_user['id'].to_s] = wb_user.select{|k,v| k.in?(wb_column)} }
       return users if (cursor = wb_users['next_cursor'].to_i) == 0 
     end
   end 
-
 
   def bind_weibo_filter
     render :json => {:error => "未绑定新浪微博"}.to_json if session_user.wb_uid.blank?
