13b747d8d473bad33b7667f3b84d269f830c2a90
peterzd <zdsunshine0640@gmail.com>
2014-04-25 15:28:06 +0800

    [chore]bring devise_error_messages back to DeivseHelper

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,2 +1,21 @@
 module DeviseHelper
+
+  ## overridden from Devise source code
+  # https://github.com/plataformatec/devise/blob/master/app/helpers/devise_helper.rb
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = "Oops! Your changes could not be saved for the following reason(s):"
+
+    html = <<-HTML
+    <div class="alert alert-danger alert-block">
+      <button type="button" class="close" data-dismiss="alert">x</button>
+      <h4>#{sentence}</h4>
+      #{messages}
+    </div>
+    HTML
+
+    html.html_safe
+  end
 end
