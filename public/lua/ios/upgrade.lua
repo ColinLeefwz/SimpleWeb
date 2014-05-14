d8630bcd2b812ade0dd8663eb01dd4039b7fb567
web1 <web1@dface.cn>
2014-05-15 00:16:42 +0800

    ios升级

--- public/lua/ios/upgrade.lua
+++ public/lua/ios/upgrade.lua
@@ -1,17 +1,18 @@
 waxClass{"MyClass", NSObject, protocols = {"UIAlertViewDelegate"}}
 
-function alertView_clickedButtonAtIndex(view, buttonIndex)
+function alertView_clickedButtonAtIndex(self, view, buttonIndex)
 	print("click")
-	
+if buttonIndex==0 then
+  return
+end;
+url = NSURL:URLWithString("http://itunes.apple.com/app/id577710538?ls=1&mt=8")
+app = UIApplication:sharedApplication()
+app:openURL(url)	
 end
 
 local delegate = MyClass:init()
 
-local x = wax.alert("更新提示","发现有新版本","跳过","确定")
+x = wax.alert("更新提示","发现有新版本","跳过","确定")
 x:setDelegate(delegate)
---local x = UIAlertView:initWithTitle_message_delegate_cancelButtonTitle_otherButtonTitles("title", "message", delegate, "取消", nil)
---x:show()
-puts("hello test")
-puts(wax.guid())
 
 
