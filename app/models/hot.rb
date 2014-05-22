424faf02b3dce69ef5431658b3d1368eed2000fd
leelolo <lilonglongfwz@gmail.com>
2014-05-22 09:24:57 +0800

    [模型]添加商家名称和显示范围调用方法

--- app/models/hot.rb
+++ app/models/hot.rb
@@ -7,4 +7,23 @@ class Hot
   field :date_array, type: Date
   field :shop_rank, type: Integer
 	field :display_range
+
+	def show_shop_name
+		if self.shop
+      self.shop.name
+		else
+			"商家名称未找到"
+		end
+	end
+
+
+	def show_display_range
+		if self.display_range == "全城"
+      range = self.display_range + "(" + self.shop.city + ")"
+		else
+			range = self.display_range
+		end
+
+		range
+	end
 end
